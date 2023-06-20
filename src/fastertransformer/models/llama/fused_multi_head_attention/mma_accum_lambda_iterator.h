5671dfea91b5464906c7af2cea65105a44a1f740
grimoire <streetyao@live.com>
2023-06-20 22:21:40 +0800

    clang-format

--- src/fastertransformer/models/llama/fused_multi_head_attention/mma_accum_lambda_iterator.h
+++ src/fastertransformer/models/llama/fused_multi_head_attention/mma_accum_lambda_iterator.h
@@ -1,12 +1,12 @@
 /***************************************************************************************************
- * Copyright (c) 2017 - 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
- * SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2017 - 2023 NVIDIA CORPORATION & AFFILIATES. All rights
+ *reserved. SPDX-License-Identifier: BSD-3-Clause
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
- * 1. Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  * this list of conditions and the following disclaimer in the documentation
@@ -18,14 +18,15 @@
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ *LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *POSSIBILITY OF SUCH DAMAGE.
  *
  **************************************************************************************************/
 
@@ -45,10 +46,9 @@ i-th element with the corresponding matrix row/col.
 
 template <typename T, typename accum_t, int kWarpSize>
 struct AccumLambdaIteratorSm80 {
-  static_assert(
-      cutlass::platform::
-          is_same<typename T::Layout, cutlass::layout::RowMajor>::value,
-      "only RowMajor is supported");
+  static_assert(cutlass::platform::is_same<typename T::Layout,
+                                           cutlass::layout::RowMajor>::value,
+                "only RowMajor is supported");
 
   using Policy = typename T::Policy;
   using InstructionShape = typename T::InstructionShape;
@@ -58,41 +58,36 @@ struct AccumLambdaIteratorSm80 {
   static int const kRowsPerTile = 8;
   static int const kAccumulatorRows = InstructionShape::kM / kRowsPerTile;
 
-  static cutlass::MatrixCoord CUTLASS_DEVICE get_lane_offset(
-      int8_t lane_id,
-      int8_t warp_id,
-      typename T::TensorCoord const& tile_offset) {
+  static cutlass::MatrixCoord CUTLASS_DEVICE
+  get_lane_offset(int8_t lane_id, int8_t warp_id,
+                  typename T::TensorCoord const& tile_offset) {
     int quad = (lane_id >> 2);
     int lane_in_quad = (lane_id & 3);
-    return cutlass::MatrixCoord(
-        quad + tile_offset.row() * Shape::kRow,
-        lane_in_quad * kElementsPerAccess +
-            tile_offset.column() * Shape::kColumn);
+    return cutlass::MatrixCoord(quad + tile_offset.row() * Shape::kRow,
+                                lane_in_quad * kElementsPerAccess +
+                                    tile_offset.column() * Shape::kColumn);
   }
 
   template <typename FA, typename FB, typename FC>
-  CUTLASS_DEVICE static void iterateRows(
-      cutlass::MatrixCoord& lane_offset,
-      FA beginRow,
-      FB op,
-      FC endRow) {
+  CUTLASS_DEVICE static void iterateRows(cutlass::MatrixCoord& lane_offset,
+                                         FA beginRow, FB op, FC endRow) {
     // See cutlass/gemm/warp/mma_tensor_op_tile_iterator.h
     CUTLASS_PRAGMA_UNROLL
     for (int mma_m = 0; mma_m < Policy::MmaIterations::kRow; ++mma_m) {
       CUTLASS_PRAGMA_UNROLL
       for (int row = 0; row < kAccumulatorRows; ++row) {
         int accum_m = mma_m * InstructionShape::kM * OpDelta::kRow +
-            row * kRowsPerTile + lane_offset.row();
+                      row * kRowsPerTile + lane_offset.row();
         beginRow(accum_m);
 
         CUTLASS_PRAGMA_UNROLL
         for (int mma_n = 0; mma_n < Policy::MmaIterations::kColumn; ++mma_n) {
           int mma_accum_start = kAccumulatorRows * kElementsPerAccess *
-              (mma_n * Policy::MmaIterations::kRow + mma_m);
+                                (mma_n * Policy::MmaIterations::kRow + mma_m);
           CUTLASS_PRAGMA_UNROLL
           for (int col = 0; col < kElementsPerAccess; ++col) {
             int accum_n = mma_n * InstructionShape::kN * OpDelta::kColumn +
-                col + lane_offset.column();
+                          col + lane_offset.column();
             int idx = mma_accum_start + row * kElementsPerAccess + col;
             op(accum_m, accum_n, idx);
           }
@@ -118,10 +113,9 @@ struct AccumLambdaIteratorSm80 {
 
 template <typename T, typename accum_t, int kWarpSize>
 struct AccumLambdaIteratorSm70 {
-  static_assert(
-      cutlass::platform::
-          is_same<typename T::Layout, cutlass::layout::RowMajor>::value,
-      "only RowMajor is supported");
+  static_assert(cutlass::platform::is_same<typename T::Layout,
+                                           cutlass::layout::RowMajor>::value,
+                "only RowMajor is supported");
 
   using Policy = typename T::Policy;
   using InstructionShape = typename T::InstructionShape;
@@ -132,16 +126,14 @@ struct AccumLambdaIteratorSm70 {
   static int const kElementsPerPartial = 4;
   using EleShapePerPatial = typename cutlass::platform::conditional<
       cutlass::platform::is_same<Element, float>::value,
-      cutlass::MatrixShape<2, 2>,
-      cutlass::MatrixShape<1, 4>>::type;
+      cutlass::MatrixShape<2, 2>, cutlass::MatrixShape<1, 4>>::type;
   static int const kElementsPerMma = 8;
   static int const kAccumulatorPatials = 2;
   using QuadShapePerPatialMma = cutlass::MatrixShape<4, 4>;
 
-  static cutlass::MatrixCoord CUTLASS_DEVICE get_lane_offset(
-      int8_t lane_id,
-      int8_t warp_id,
-      typename T::TensorCoord const& tile_offset) {
+  static cutlass::MatrixCoord CUTLASS_DEVICE
+  get_lane_offset(int8_t lane_id, int8_t warp_id,
+                  typename T::TensorCoord const& tile_offset) {
     int quad = (lane_id >> 2);
     int lane_in_quad = (lane_id & 3);
     int accum_m, accum_n;
@@ -155,7 +147,7 @@ struct AccumLambdaIteratorSm70 {
           (lane_in_quad & 2);
     } else {
       accum_m = (((quad & 0x4) >> 1) + (quad & 0x1)) * 8 +
-          lane_in_quad; // (quad[2],quad[0])
+                lane_in_quad;  // (quad[2],quad[0])
       accum_n = ((quad >> 1) & 0x1) * kElementsPerPartial * kAccumulatorPatials;
     }
     return cutlass::MatrixCoord(
@@ -165,9 +157,8 @@ struct AccumLambdaIteratorSm70 {
 
   template <typename DT, typename F>
   CUTLASS_DEVICE static bool reduceSameRow(int lane_id, DT& myValue, F fn) {
-    static_assert(
-        cutlass::platform::is_same<Element, float>::value,
-        "update to support non-float accum");
+    static_assert(cutlass::platform::is_same<Element, float>::value,
+                  "update to support non-float accum");
     // https://docs.nvidia.com/cuda/parallel-thread-execution/index.html#warp-level-matrix-fragment-mma-884-f16
     // T0 & T2 share same line within a quad
     auto otherV = __shfl_xor_sync(0xffffffff, myValue, 1 << 1);
@@ -179,11 +170,8 @@ struct AccumLambdaIteratorSm70 {
   }
 
   template <typename FA, typename FB, typename FC>
-  CUTLASS_DEVICE static void iterateRows(
-      cutlass::MatrixCoord& lane_offset,
-      FA beginRow,
-      FB op,
-      FC endRow) {
+  CUTLASS_DEVICE static void iterateRows(cutlass::MatrixCoord& lane_offset,
+                                         FA beginRow, FB op, FC endRow) {
     CUTLASS_PRAGMA_UNROLL
     for (int tile_m = 0; tile_m < Policy::TileIterations::kRow; ++tile_m) {
       CUTLASS_PRAGMA_UNROLL
@@ -191,7 +179,8 @@ struct AccumLambdaIteratorSm70 {
         CUTLASS_PRAGMA_UNROLL
         for (int m = 0; m < EleShapePerPatial::kRow; ++m) {
           int accum_m = tile_m * Policy::InterleavedTile::kRow +
-              mma_m * QuadShapePerPatialMma::kRow + m * 2 + lane_offset.row();
+                        mma_m * QuadShapePerPatialMma::kRow + m * 2 +
+                        lane_offset.row();
           beginRow(accum_m);
 
           CUTLASS_PRAGMA_UNROLL
@@ -212,11 +201,11 @@ struct AccumLambdaIteratorSm70 {
                        mma_m) *
                       kElementsPerMma;
                   int accum_n = tile_n * Policy::InterleavedTile::kColumn +
-                      mma_n * QuadShapePerPatialMma::kColumn +
-                      p * Policy::InterleavedTile::kColumn / 2 + n +
-                      lane_offset.column();
+                                mma_n * QuadShapePerPatialMma::kColumn +
+                                p * Policy::InterleavedTile::kColumn / 2 + n +
+                                lane_offset.column();
                   int idx = mma_accum_start + p * kElementsPerPartial +
-                      m * EleShapePerPatial::kColumn + n;
+                            m * EleShapePerPatial::kColumn + n;
                   op(accum_m, accum_n, idx);
                 }
               }
@@ -236,10 +225,9 @@ struct AccumLambdaIteratorSimt {
   using Element = typename T::Element;
   using Delta = typename T::Delta;
   using Shape = typename T::Shape;
-  static_assert(
-      cutlass::platform::
-          is_same<typename T::Layout, cutlass::layout::RowMajor>::value,
-      "only RowMajor is supported");
+  static_assert(cutlass::platform::is_same<typename T::Layout,
+                                           cutlass::layout::RowMajor>::value,
+                "only RowMajor is supported");
 
   template <typename DT, typename F>
   CUTLASS_DEVICE static bool reduceSameRow(int lane_id, DT& myValue, F fn) {
@@ -252,11 +240,8 @@ struct AccumLambdaIteratorSimt {
   }
 
   template <typename FA, typename FB, typename FC>
-  CUTLASS_DEVICE static void iterateRows(
-      cutlass::MatrixCoord& lane_offset,
-      FA beginRow,
-      FB op,
-      FC endRow) {
+  CUTLASS_DEVICE static void iterateRows(cutlass::MatrixCoord& lane_offset,
+                                         FA beginRow, FB op, FC endRow) {
     CUTLASS_PRAGMA_UNROLL
     for (int mma_m = 0; mma_m < Iterations::kRow; ++mma_m) {
       CUTLASS_PRAGMA_UNROLL
@@ -271,11 +256,10 @@ struct AccumLambdaIteratorSimt {
               lane_offset.column();
           CUTLASS_PRAGMA_UNROLL
           for (int n = 0; n < Policy::LaneMmaShape::kN; ++n) {
-            int idx = n +
-                Policy::LaneMmaShape::kN *
-                    (mma_n +
-                     Iterations::kColumn *
-                         (m + mma_m * Policy::LaneMmaShape::kM));
+            int idx =
+                n + Policy::LaneMmaShape::kN *
+                        (mma_n + Iterations::kColumn *
+                                     (m + mma_m * Policy::LaneMmaShape::kM));
             op(accum_m, accum_n + n, idx);
           }
         }
@@ -284,22 +268,21 @@ struct AccumLambdaIteratorSimt {
     }
   }
 
-  static cutlass::MatrixCoord CUTLASS_DEVICE get_lane_offset(
-      int8_t lane_id,
-      int8_t warp_id,
-      typename T::TensorCoord const& tile_offset) {
-    static_assert(
-        cutlass::platform::is_same<
-            typename Policy::LaneLayout,
-            cutlass::layout::RowMajorInterleaved<1>>::value,
-        "");
+  static cutlass::MatrixCoord CUTLASS_DEVICE
+  get_lane_offset(int8_t lane_id, int8_t warp_id,
+                  typename T::TensorCoord const& tile_offset) {
+    static_assert(cutlass::platform::is_same<
+                      typename Policy::LaneLayout,
+                      cutlass::layout::RowMajorInterleaved<1>>::value,
+                  "");
     typename Policy::LaneLayout lane_layout = Policy::get_lane_layout();
 
-    cutlass::MatrixCoord lane_offset = lane_layout.inverse(lane_id) *
+    cutlass::MatrixCoord lane_offset =
+        lane_layout.inverse(lane_id) *
         cutlass::MatrixCoord(Policy::LaneMmaShape::kM,
                              Policy::LaneMmaShape::kN);
     return lane_offset +
-        tile_offset * cutlass::MatrixCoord(Shape::kRow, Shape::kColumn);
+           tile_offset * cutlass::MatrixCoord(Shape::kRow, Shape::kColumn);
   }
 };
 
@@ -308,24 +291,12 @@ struct DefaultMmaAccumLambdaIterator;
 
 // Simt
 template <typename S, typename P, typename accum_t, int kWarpSize>
-struct DefaultMmaAccumLambdaIterator<
-    cutlass::gemm::warp::MmaSimtTileIterator<
-        S,
-        cutlass::gemm::Operand::kC,
-        accum_t,
-        cutlass::layout::RowMajor,
-        P,
-        1,
-        1>,
-    accum_t,
-    kWarpSize> {
+struct DefaultMmaAccumLambdaIterator<cutlass::gemm::warp::MmaSimtTileIterator<
+                                         S, cutlass::gemm::Operand::kC, accum_t,
+                                         cutlass::layout::RowMajor, P, 1, 1>,
+                                     accum_t, kWarpSize> {
   using WarpIterator = typename cutlass::gemm::warp::MmaSimtTileIterator<
-      S,
-      cutlass::gemm::Operand::kC,
-      accum_t,
-      cutlass::layout::RowMajor,
-      P,
-      1,
+      S, cutlass::gemm::Operand::kC, accum_t, cutlass::layout::RowMajor, P, 1,
       1>;
   using Iterator = AccumLambdaIteratorSimt<WarpIterator, accum_t, kWarpSize>;
 };
@@ -334,45 +305,24 @@ struct DefaultMmaAccumLambdaIterator<
 template <typename S1, typename S2, typename accum_t, int kWarpSize>
 struct DefaultMmaAccumLambdaIterator<
     cutlass::gemm::warp::MmaVoltaTensorOpAccumulatorTileIterator<
-        S1,
-        accum_t,
-        cutlass::layout::RowMajor,
-        S2,
-        cutlass::MatrixShape<1, 1>>,
-    accum_t,
-    kWarpSize> {
+        S1, accum_t, cutlass::layout::RowMajor, S2, cutlass::MatrixShape<1, 1>>,
+    accum_t, kWarpSize> {
   using WarpIterator =
       typename cutlass::gemm::warp::MmaVoltaTensorOpAccumulatorTileIterator<
-          S1,
-          accum_t,
-          cutlass::layout::RowMajor,
-          S2,
+          S1, accum_t, cutlass::layout::RowMajor, S2,
           cutlass::MatrixShape<1, 1>>;
   using Iterator = AccumLambdaIteratorSm70<WarpIterator, accum_t, kWarpSize>;
 };
 
 // TensorOp - Sm75+
-template <
-    typename S1,
-    typename S2,
-    typename S3,
-    typename accum_t,
-    int kWarpSize>
+template <typename S1, typename S2, typename S3, typename accum_t,
+          int kWarpSize>
 struct DefaultMmaAccumLambdaIterator<
     cutlass::gemm::warp::MmaTensorOpAccumulatorTileIterator<
-        S1,
-        accum_t,
-        cutlass::layout::RowMajor,
-        S2,
-        S3>,
-    accum_t,
-    kWarpSize> {
+        S1, accum_t, cutlass::layout::RowMajor, S2, S3>,
+    accum_t, kWarpSize> {
   using WarpIterator =
       typename cutlass::gemm::warp::MmaTensorOpAccumulatorTileIterator<
-          S1,
-          accum_t,
-          cutlass::layout::RowMajor,
-          S2,
-          S3>;
+          S1, accum_t, cutlass::layout::RowMajor, S2, S3>;
   using Iterator = AccumLambdaIteratorSm80<WarpIterator, accum_t, kWarpSize>;
 };
