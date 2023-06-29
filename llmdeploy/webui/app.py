7dc1ee3eb3470d8ed32f8c993ad021bfb50d2db0
AllentDan <AllentDan@yeah.net>
2023-06-29 10:48:27 +0800

    resolve comments

--- llmdeploy/webui/app.py
+++ llmdeploy/webui/app.py
@@ -1,14 +1,40 @@
 # flake8: noqa
 from functools import partial
+import threading
 
 import fire
 import gradio as gr
 import os
-from llama import chat_stream
-from miscs.strings import ABSTRACT, TITLE
-from miscs.styles import PARENT_BLOCK_CSS
+from strings import ABSTRACT, TITLE
+from styles import PARENT_BLOCK_CSS
 
 from llmdeploy.serve.fastertransformer.chatbot import Chatbot
+
+
+def chat_stream(instruction,
+                state_chatbot,
+                llama_chatbot,
+                model_name: str = None):
+    bot_summarized_response = ''
+    model_type = 'fastertransformer'
+    state_chatbot = state_chatbot + [(instruction, None)]
+    session_id = threading.current_thread().ident
+    bot_response = llama_chatbot.stream_infer(
+        session_id, instruction, f'{session_id}-{len(state_chatbot)}')
+
+    yield (state_chatbot, state_chatbot, f'{bot_summarized_response}'.strip())
+
+    for status, tokens, _ in bot_response:
+        if state_chatbot[-1][-1] is None or model_type != 'fairscale':
+            state_chatbot[-1] = (state_chatbot[-1][0], tokens)
+        else:
+            state_chatbot[-1] = (state_chatbot[-1][0],
+                                 state_chatbot[-1][1] + tokens
+                                 )  # piece by piece
+        yield (state_chatbot, state_chatbot,
+               f'{bot_summarized_response}'.strip())
+
+    yield (state_chatbot, state_chatbot, f'{bot_summarized_response}'.strip())
 
 
 def reset_textbox():
@@ -28,6 +54,16 @@ def reset_everything_func(instruction_txtbox, state_chatbot, llama_chatbot,
         state_chatbot,
         state_chatbot,
         gr.Textbox.update(value=''),
+    )
+
+
+def cancel_func(instruction_txtbox, state_chatbot, llama_chatbot):
+    session_id = llama_chatbot._session.session_id
+    llama_chatbot.cancel(session_id)
+
+    return (
+        llama_chatbot,
+        state_chatbot,
     )
 
 
@@ -78,7 +114,10 @@ def run(triton_server_addr: str,
             [instruction_txtbox],
         )
 
-        cancel_btn.click(None, None, None, cancels=[send_event])
+        cancel_btn.click(
+            cancel_func, [instruction_txtbox, state_chatbot, llama_chatbot],
+            [llama_chatbot, chatbot],
+            cancels=[send_event])
 
         reset_btn.click(
             reset_everything,
