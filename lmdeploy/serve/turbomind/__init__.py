0f0e0cf2ee2b2ac72fe0f4a7b279c8385fb5ad4d
lvhan028 <lvhan_028@163.com>
2023-06-30 15:13:13 +0800

    update

--- lmdeploy/serve/turbomind/__init__.py
+++ lmdeploy/serve/turbomind/__init__.py
@@ -1,2 +1,2 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from lmdeploy.serve.fastertransformer.chatbot import Chatbot  # noqa: F401,F403
+from lmdeploy.serve.turbomind.chatbot import Chatbot  # noqa: F401,F403
