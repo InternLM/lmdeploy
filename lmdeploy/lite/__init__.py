423a31413986cd80e133feb9d41169cad66fa66d
pppppM <gjf_mail@126.com>
2023-07-04 13:53:25 +0800

    fix hints

--- lmdeploy/lite/__init__.py
+++ lmdeploy/lite/__init__.py
@@ -1,3 +1,4 @@
-from .utils import *
-from .quantization import *
-from .apis import *
+# Copyright (c) OpenMMLab. All rights reserved.
+from .apis import *  # noqa: F401,F403
+from .quantization import *  # noqa: F401,F403
+from .utils import *  # noqa: F401,F403
--- lmdeploy/lite/utils/__init__.py
+++ lmdeploy/lite/utils/__init__.py
@@ -1,16 +1,18 @@
-
-from .memory_efficient import memory_efficient_inference
-from .cal_qparams import (
-    cal_qparams_per_channel_absmax, cal_qparams_per_channel_minmax, 
-    cal_qparams_per_group_absmax, cal_qparams_per_group_minmax, 
-    cal_qparams_per_tensor_absmax, cal_qparams_per_tensor_minmax)
-
+# Copyright (c) OpenMMLab. All rights reserved.
+from .cal_qparams import (cal_qparams_per_channel_absmax,
+                          cal_qparams_per_channel_minmax,
+                          cal_qparams_per_group_absmax,
+                          cal_qparams_per_group_minmax,
+                          cal_qparams_per_tensor_absmax,
+                          cal_qparams_per_tensor_minmax)
 from .calib_dataloader import get_calib_loaders
 from .collect import collect_target_modules, collect_target_weights
+from .memory_efficient import memory_efficient_inference
+
 __all__ = [
     'cal_qparams_per_channel_absmax', 'cal_qparams_per_channel_minmax',
     'cal_qparams_per_group_absmax', 'cal_qparams_per_group_minmax',
     'cal_qparams_per_tensor_absmax', 'cal_qparams_per_tensor_minmax',
-    'get_calib_loaders', 'memory_efficient_inference', 'collect_target_modules',
-    'collect_target_weights'
+    'get_calib_loaders', 'memory_efficient_inference',
+    'collect_target_modules', 'collect_target_weights'
 ]
