cab24bd2a64ece2ff447ee3e00d3cda3487dfafc
lvhan028 <lvhan_028@163.com>
2023-07-05 13:33:40 +0800

    update internlm model

--- lmdeploy/model.py
+++ lmdeploy/model.py
@@ -23,18 +23,14 @@ class Vicuna:
         return None
 
 
-@MODELS.register_module(name='puyu')
+@MODELS.register_module(name='internlm')
 class Puyu:
 
     def __init__(self):
-        self.system = """meta instruction
-You are an AI assistant whose name is InternLM (书生·浦语).
-- 书生·浦语 is a conversational language model that is developed by Shanghai AI Laboratory (上海人工智能实验室). It is designed to be helpful, honest, and harmless.
-- 书生·浦语 can understand and communicate fluently in the language chosen by the user such as English and 中文.
-conversation"""  # noqa: E501
-        self.user = '<|Human|>'
-        self.eou = 'െ'
-        self.assistant = '<|Assistant|>'
+        self.system = ''
+        self.user = '<|User|>'
+        self.eou = '<TOKENS_UNUSED_0>'
+        self.assistant = '<|Bot|>'
 
     def get_prompt(self, prompt, sequence_start=True):
         if sequence_start:
@@ -46,7 +42,21 @@ conversation"""  # noqa: E501
 
     @property
     def stop_words(self):
-        return [45623]
+        return [103027, 103028]
+
+
+@MODELS.register_module(name='llama')
+class Llama:
+
+    def __init__(self):
+        pass
+
+    def get_prompt(self, prompt, sequence_start=True):
+        return prompt
+
+    @property
+    def stop_words(self):
+        return None
 
 
 def main(model_name: str = 'test'):
