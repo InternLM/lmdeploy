cfc9beb0f415956dad961ef13a8d39889f78f3f5
grimoire <yaoqian@pjlab.org.cn>
2023-07-17 18:54:10 +0800

    refactor tokenizer

--- lmdeploy/turbomind/tokenizer.py
+++ lmdeploy/turbomind/tokenizer.py
@@ -6,6 +6,128 @@ import torch
 from torch.nn.utils.rnn import pad_sequence
 
 
+class SentencePieceTokenizer:
+    """Tokenizer of sentencepiece.
+
+    Args:
+        model_file (str): the path of the tokenizer model
+    """
+
+    def __init__(self, model_file: str):
+        from sentencepiece import SentencePieceProcessor
+        self.model = SentencePieceProcessor(model_file=model_file)
+
+    @property
+    def vocab_size(self):
+        """vocabulary size."""
+        return self.model.vocab_size()
+
+    @property
+    def bos_token_id(self):
+        """begine of the sentence token id."""
+        return self.model.bos_id()
+
+    @property
+    def eos_token_id(self):
+        """end of the sentence token id."""
+        return self.model.eos_id()
+
+    def encode(self, s: str):
+        """Tokenize a prompt.
+
+        Args:
+            s (str): a prompt
+        Returns:
+            list[int]: token ids
+        """
+        add_bos = False
+        add_eos = False
+        if s.find('<BOS>') != -1:
+            s = s.replace('<BOS>', '')
+            add_bos = True
+        if s == '<EOS>':
+            s = ''
+            add_eos = True
+        return self.model.Encode(s, add_bos=add_bos, add_eos=add_eos)
+
+    def decode(self, t: Sequence[int]):
+        """De-tokenize.
+
+        Args:
+            t (List[int]): a list of token ids
+        Returns:
+            str: text of decoding tokens
+        """
+        if isinstance(t, torch.Tensor):
+            t = t.tolist()
+        return self.model.Decode(t)
+
+
+class HuggingFaceTokenizer:
+    """Tokenizer of sentencepiece.
+
+    Args:
+        model_dir (str): the directory of the tokenizer model
+    """
+
+    def __init__(self, model_dir: str):
+        from transformers import AutoTokenizer
+        model_file = osp.join(model_dir, 'tokenizer.model')
+        backend_tokenizer_file = osp.join(model_dir, 'tokenizer.json')
+        model_file_exists = osp.exists(model_file)
+        if not osp.exists(backend_tokenizer_file) and model_file_exists:
+            print('WARNING: Can not find tokenizer.json. '
+                  'It may take long time to initialize the tokenizer.')
+        self.model = AutoTokenizer.from_pretrained(model_dir,
+                                                   trust_remote_code=True)
+        # save tokenizer.json to reuse
+        if not osp.exists(backend_tokenizer_file) and model_file_exists:
+            self.model.backend_tokenizer.save(backend_tokenizer_file)
+
+    @property
+    def vocab_size(self):
+        """vocabulary size."""
+        return self.model.vocab_size
+
+    @property
+    def bos_token_id(self):
+        """begine of the sentence token id."""
+        return self.model.bos_token_id
+
+    @property
+    def eos_token_id(self):
+        """end of the sentence token id."""
+        return self.model.eos_token_id
+
+    def encode(self, s: str):
+        """Tokenize a prompt.
+
+        Args:
+            s (str): a prompt
+        Returns:
+            list[int]: token ids
+        """
+        add_special_tokens = False
+        if s.find('<BOS>') != -1:
+            s = s.replace('<BOS>', '<s>')
+        if s == '<EOS>':
+            s = '</s>'
+        if len(s) == 0:
+            add_special_tokens = True
+        return self.model.encode(s, add_special_tokens=add_special_tokens)
+
+    def decode(self, t: Sequence[int]):
+        """De-tokenize.
+
+        Args:
+            t (List[int]): a list of token ids
+        Returns:
+            str: text of decoding tokens
+        """
+        skip_special_tokens = True
+        return self.model.decode(t, skip_special_tokens=skip_special_tokens)
+
+
 class Tokenizer:
     """Tokenize prompts or de-tokenize tokens into texts.
 
@@ -25,27 +147,25 @@ class Tokenizer:
         config_exists = osp.exists(tokenizer_config_file)
         use_hf_model = config_exists or not model_file_exists
 
-        self.use_hf_model = use_hf_model
-        if not self.use_hf_model:
-            from sentencepiece import SentencePieceProcessor
-            self.model = SentencePieceProcessor(model_file=model_file)
-            self.vocab_size = self.model.vocab_size()
-            self.bos_token_id = self.model.bos_id()
-            self.eos_token_id = self.model.eos_id()
+        if not use_hf_model:
+            self.model = SentencePieceTokenizer(model_file)
         else:
-            from transformers import AutoTokenizer
-            backend_tokenizer_file = osp.join(model_folder, 'tokenizer.json')
-            if not osp.exists(backend_tokenizer_file) and model_file_exists:
-                print('WARNING: Can not find tokenizer.json. '
-                      'It may take long time to initialize the tokenizer.')
-            self.model = AutoTokenizer.from_pretrained(model_folder,
-                                                       trust_remote_code=True)
-            self.vocab_size = self.model.vocab_size
-            self.bos_token_id = self.model.bos_token_id
-            self.eos_token_id = self.model.eos_token_id
-            # save tokenizer.json to reuse
-            if not osp.exists(backend_tokenizer_file) and model_file_exists:
-                self.model.backend_tokenizer.save(backend_tokenizer_file)
+            self.model = HuggingFaceTokenizer(model_folder)
+
+    @property
+    def vocab_size(self):
+        """vocabulary size."""
+        return self.model.vocab_size
+
+    @property
+    def bos_token_id(self):
+        """begine of the sentence token id."""
+        return self.model.bos_token_id
+
+    @property
+    def eos_token_id(self):
+        """end of the sentence token id."""
+        return self.model.eos_token_id
 
     def encode(self, s: str):
         """Tokenize a prompt.
@@ -55,25 +175,7 @@ class Tokenizer:
         Returns:
             list[int]: token ids
         """
-        if not self.use_hf_model:
-            add_bos = False
-            add_eos = False
-            if s.find('<BOS>') != -1:
-                s = s.replace('<BOS>', '')
-                add_bos = True
-            if s == '<EOS>':
-                s = ''
-                add_eos = True
-            return self.model.Encode(s, add_bos=add_bos, add_eos=add_eos)
-        else:
-            add_special_tokens = False
-            if s.find('<BOS>') != -1:
-                s = s.replace('<BOS>', '<s>')
-            if s == '<EOS>':
-                s = '</s>'
-            if len(s) == 0:
-                add_special_tokens = True
-            return self.model.encode(s, add_special_tokens=add_special_tokens)
+        return self.model.encode(s)
 
     def decode(self, t: Sequence[int]):
         """De-tokenize.
@@ -83,14 +185,7 @@ class Tokenizer:
         Returns:
             str: text of decoding tokens
         """
-        if not self.use_hf_model:
-            if isinstance(t, torch.Tensor):
-                t = t.tolist()
-            return self.model.Decode(t)
-        else:
-            skip_special_tokens = True
-            return self.model.decode(t,
-                                     skip_special_tokens=skip_special_tokens)
+        return self.model.decode(t)
 
 
 class Preprocessor:
