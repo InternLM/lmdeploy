66c0058bc4b8b05b19553e51d2e86ce93d333fbf
wangruohui <12756472+wangruohui@users.noreply.github.com>
2023-07-25 14:15:51 +0800

    add docs

--- benchmark/profile_hf_generation.py
+++ benchmark/profile_hf_generation.py
@@ -1,4 +1,4 @@
-"""Benchmark huggingface models and maybe speedup by deepspeed
+"""Benchmark huggingface models and maybe speedup by deepspeed.
 
 Theoretically, this tool is compatible with all huggingface models.
 
@@ -27,9 +27,28 @@ python profile_hf_generation.py \
     --out_file profile_hf.csv
 ```
 
-Result will be saved in `profile_hf.csv`, which is a comma-separated file with the following fields.
+Result will be saved in `profile_hf.csv`, which is a comma-separated file
+with the following fields.
 
-model, batch_size, input_seqlen, gen_len, total_len, first_time, next_time, last_time, total time, throughput(total), throughput(gen)
+1. model: name of model, specified with --model_log_name
+2. batch_size: as name
+3. input_seqlen: as name
+4. gen_len: length of sequence length to generate
+5. total_len: gen_len + input_len
+
+In total, the model will take a random input ids of shape
+(batch_size, input_seqlen) and run forward `gen_len` times
+to generate output ids of shape (batch_size, gen_len)
+
+6. first_time: latency to forward the first (batch_size, input_seqlen) ids
+    to get `input_seqlen+1`-th batch of output of shape (batch_size, 1)
+7. next_time: average latency of the next samples (averaged of 5 sample),
+    this measure latency when context length is short
+8. last_time: average latency of the last samples (averaged of 5 sample),
+    this measure latency when context length is long
+9. total time: total time to generate all tokens
+10. throughput(total): bs * total_len / total_time (same as vllm)
+11. throughput(gen): bs * gen_len / total_time (same as vllm)
 """
 
 import csv
@@ -49,8 +68,8 @@ logger.setLevel(logging.DEBUG)
 info = logger.info
 warning = logger.warning
 debug = logger.debug
-cinfo = lambda x: info("\033[1;32m" + x + "\033[0m")
-cprint = lambda x: print("\033[1;32m" + x + "\033[0m")
+cinfo = lambda x: info('\033[1;32m' + x + '\033[0m')
+cprint = lambda x: print('\033[1;32m' + x + '\033[0m')
 avg = lambda x: sum(x) / len(x)
 
 
@@ -86,11 +105,10 @@ class TimingStreamer:
         # self.tokens = torch.hstack([_atleast_2d(v) for v in self.token_cache])
 
     def get_times(self):
-        """
-        Maybe deprecated.
+        """Maybe deprecated.
+
         Returns:
           a list of times in ms for (first prompt, avg next token, total time)
-
         """
         first = self.evts[0].elapsed_time(self.evts[1])
         rest = [
@@ -115,33 +133,33 @@ class CSVWRitter:
 
     def __init__(
         self,
-        file="unnamed.csv",
+        file='unnamed.csv',
         header=[
-            "model",
-            "batch_size",
-            "input_seqlen",
-            "gen_len",
-            "total_len",
-            "first_time",
-            "next_time",
-            "last_time",
-            "total time",
-            "throughput(total)",
-            "throughput(gen)",
+            'model',
+            'batch_size',
+            'input_seqlen',
+            'gen_len',
+            'total_len',
+            'first_time',
+            'next_time',
+            'last_time',
+            'total time',
+            'throughput(total)',
+            'throughput(gen)',
         ],
     ):
         if self.on_master:
             self.file = file
-            csv.writer(open(file, "a")).writerow(header)
+            csv.writer(open(file, 'a')).writerow(header)
 
     def write(self, line):
         if self.on_master:
-            csv.writer(open(self.file, "a")).writerow(line)
+            csv.writer(open(self.file, 'a')).writerow(line)
 
     @property
     def on_master(self):
         # return not torch.distributed.is_initialized() or torch.distributed.get_rank() == 0
-        rank = int(os.environ.get("RANK", 0))
+        rank = int(os.environ.get('RANK', 0))
         return rank == 0
 
 
@@ -151,7 +169,7 @@ def init_hf_model(model_path: str):
         model = AutoModelForCausalLM.from_pretrained(model_path,
                                                      torch_dtype=torch.float16,
                                                      trust_remote_code=True)
-    print(f"load model in {time.monotonic() -start} s")
+    print(f'load model in {time.monotonic() -start} s')
     return model
 
 
@@ -159,7 +177,7 @@ def accel_deepspeed(model, max_out_tokens, tp_size=1):
     import deepspeed
     ds_model = deepspeed.init_inference(
         model=model,  # Transformers models
-        tensor_parallel={"tp_size": tp_size},
+        tensor_parallel={'tp_size': tp_size},
         dtype=torch.float16,  # dtype of the weights (fp16)
         replace_with_kernel_inject=True,
         max_out_tokens=max_out_tokens,
@@ -174,7 +192,7 @@ def main(model_path: str,
          gen_seqlen: int,
          test_round: int = 1,
          accel: Optional[str] = None,
-         out_file: Optional[str] = "profile_hf.csv",
+         out_file: Optional[str] = 'profile_hf.csv',
          model_log_name: Optional[str] = None):
 
     total_seqlen = input_seqlen + gen_seqlen
@@ -187,23 +205,23 @@ def main(model_path: str,
 
     if accel is None:
         model = model.cuda()
-    elif accel == "deepspeed":
+    elif accel == 'deepspeed':
         model = accel_deepspeed(model, total_seqlen + 6)
         # longer total seqlen for fault tolerance
     else:
-        raise NotImplementedError(f"accel {accel} not supported.")
+        raise NotImplementedError(f'accel {accel} not supported.')
 
     # log to file
     csvwritter = CSVWRitter(out_file)
 
-    cprint("Benchmarking {} "
-           f"with batch_size={batch_size}, input_seqlen={input_seqlen}, "
-           f"gen_seqlen={gen_seqlen}, accel={accel}")
+    cprint('Benchmarking {} '
+           f'with batch_size={batch_size}, input_seqlen={input_seqlen}, '
+           f'gen_seqlen={gen_seqlen}, accel={accel}')
     for r in range(test_round):
         # TODO: now write every round to csv
         # Use external tool for analysis
 
-        cprint(f"Test round {r}")
+        cprint(f'Test round {r}')
         # input_id = 0 sometimes cause some cuda error
         fake_inputs = torch.randint(10, vocab_size, (batch_size, input_seqlen))
         fake_inputs = fake_inputs.cuda()
@@ -229,12 +247,12 @@ def main(model_path: str,
         last_time = avg(raw_times[-5:])
         tt = batch_size * total_seqlen * 1000 / total_time
         tg = batch_size * gen_seqlen * 1000 / total_time
-        cprint(f"First token/ms: {first_time:.1f}, "
-               f"Next tokens/ms: {next_time:.1f}, "
-               f"Last tokens/ms: {last_time:.1f}, "
-               f"Total Time/ms: {total_time:5.3f}, "
-               f"Throughput Total(tok/s): {tt:5.3f}, "
-               f"Throughput Gen(tok/s): {tg:5.3f}")
+        cprint(f'First token/ms: {first_time:.1f}, '
+               f'Next tokens/ms: {next_time:.1f}, '
+               f'Last tokens/ms: {last_time:.1f}, '
+               f'Total Time/ms: {total_time:5.3f}, '
+               f'Throughput Total(tok/s): {tt:5.3f}, '
+               f'Throughput Gen(tok/s): {tg:5.3f}')
 
         if test_round > 1 and r > 0:
             # First round is warm up
@@ -245,5 +263,5 @@ def main(model_path: str,
             ])
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     fire.Fire(main)
