ff07853667d8bfdc3c89f2a189f5092b5d471f18
pppppM <gjf_mail@126.com>
2023-08-07 18:39:31 +0800

    fix lint

--- lmdeploy/lite/apis/calibrate.py
+++ lmdeploy/lite/apis/calibrate.py
@@ -1,21 +1,19 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from pathlib import Path
-from typing import List, Tuple, Optional, Union
+from typing import Union
 
 import fire
 import torch
-from tqdm import tqdm
-from transformers import AutoModelForCausalLM, AutoConfig, AutoTokenizer
+from accelerate import (infer_auto_device_map, init_empty_weights,
+                        load_checkpoint_in_model)
+from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
 
-from lmdeploy.lite.utils import collect_target_modules
-from lmdeploy.lite.utils import get_calib_loaders
 from lmdeploy.lite.quantization.calibration import Calibration
+from lmdeploy.lite.utils import collect_target_modules, get_calib_loaders
 
-from mmengine import Config
-from accelerate import init_empty_weights,load_checkpoint_in_model, infer_auto_device_map
+
 def main(model: str,
-         layer_type: Union[str, type]='LlamaDecoderLayer',
-         norm_type: Union[str, type]='LlamaRMSNorm',
+         layer_type: Union[str, type],
+         norm_type: Union[str, type],
          smooth: bool = True,
          w_bits: int = 4,
          w_sym: bool = False,
@@ -26,7 +24,6 @@ def main(model: str,
          calib_seqlen: int = 2048,
          work_dir='./work_dir',
          device: str = 'cuda'):
-    
 
     assert calib_dataset in ['c4', 'ptb', 'wikitext2', 'pileval'], \
         'Currently, only support `c4`, `ptb`, `wikitext2`, or `pileval`.'
@@ -36,17 +33,19 @@ def main(model: str,
     checkpoint = hf_config._name_or_path
 
     with init_empty_weights():
-        model = AutoModelForCausalLM.from_pretrained(model, torch_dtype=torch.float16)
-    
+        model = AutoModelForCausalLM.from_pretrained(model,
+                                                     torch_dtype=torch.float16)
+
     model.config.use_cache = False
     decoder_layers = collect_target_modules(model, layer_type)
-    device_map = infer_auto_device_map(model,no_split_module_classes=[layer_type])
+    device_map = infer_auto_device_map(model,
+                                       no_split_module_classes=[layer_type])
     for name in device_map.keys():
         if name in decoder_layers:
             device_map[name] = 'cpu'
         else:
             device_map[name] = 0
-    load_checkpoint_in_model(model, checkpoint,device_map)
+    load_checkpoint_in_model(model, checkpoint, device_map)
 
     print('Loading calibrate dataset ...')
     calib_loader, _ = get_calib_loaders(calib_dataset,
@@ -54,21 +53,24 @@ def main(model: str,
                                         nsamples=calib_samples,
                                         seqlen=calib_seqlen)
 
-    
-    calibrator = Calibration(
-                    model,
-                    layer_type=layer_type,
-                    norm_type=norm_type,
-                    smooth=smooth,
-                    w_qconfig=w_qconfig,
-                    work_dir=work_dir,
-                    device=device) 
+    calibrator = Calibration(model,
+                             layer_type=layer_type,
+                             norm_type=norm_type,
+                             smooth=smooth,
+                             w_bits=w_bits,
+                             w_sym=w_sym,
+                             w_granularity=w_granularity,
+                             w_group_size=w_group_size,
+                             work_dir=work_dir,
+                             device=device)
 
     with calibrator:
-        all_data = torch.cat([data if isinstance(data, torch.Tensor) else data[0] for data in calib_loader]).to(device)
+        all_data = torch.cat([
+            data if isinstance(data, torch.Tensor) else data[0]
+            for data in calib_loader
+        ]).to(device)
         calibrator.step(all_data)
-    
-    
+
 
 if __name__ == '__main__':
 
