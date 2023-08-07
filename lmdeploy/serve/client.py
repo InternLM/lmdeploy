3de0dbb649b8296903337ae770e05622059ea289
lvhan028 <lvhan_028@163.com>
2023-08-07 18:26:46 +0800

    Add non-stream inference api for chatbot (#200)
    * add non-stream inference api for chatbot
    
    * update according to reviewer's comments

--- lmdeploy/serve/client.py
+++ lmdeploy/serve/client.py
@@ -13,7 +13,9 @@ def input_prompt():
     return '\n'.join(iter(input, sentinel))
 
 
-def main(tritonserver_addr: str, session_id: int = 1):
+def main(tritonserver_addr: str,
+         session_id: int = 1,
+         stream_output: bool = True):
     """An example to communicate with inference server through the command line
     interface.
 
@@ -21,9 +23,12 @@ def main(tritonserver_addr: str, session_id: int = 1):
         tritonserver_addr (str): the address in format "ip:port" of
           triton inference server
         session_id (int): the identical id of a session
+        stream_output (bool): indicator for streaming output or not
     """
     log_level = os.environ.get('SERVICE_LOG_LEVEL', 'WARNING')
-    chatbot = Chatbot(tritonserver_addr, log_level=log_level, display=True)
+    chatbot = Chatbot(tritonserver_addr,
+                      log_level=log_level,
+                      display=stream_output)
     nth_round = 1
     while True:
         prompt = input_prompt()
@@ -33,12 +38,19 @@ def main(tritonserver_addr: str, session_id: int = 1):
             chatbot.end(session_id)
         else:
             request_id = f'{session_id}-{nth_round}'
-            for status, res, n_token in chatbot.stream_infer(
-                    session_id,
-                    prompt,
-                    request_id=request_id,
-                    request_output_len=512):
-                continue
+            if stream_output:
+                for status, res, n_token in chatbot.stream_infer(
+                        session_id,
+                        prompt,
+                        request_id=request_id,
+                        request_output_len=512):
+                    continue
+            else:
+                status, res, n_token = chatbot.infer(session_id,
+                                                     prompt,
+                                                     request_id=request_id,
+                                                     request_output_len=512)
+                print(res)
         nth_round += 1
 
 
