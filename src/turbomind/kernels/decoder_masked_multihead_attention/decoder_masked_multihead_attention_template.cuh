32e05472cf188661717e74aaaab9e157e24745bf
Xin Chen <xinchen.tju@gmail.com>
2023-08-07 13:53:42 +0800

    remove not

--- src/turbomind/kernels/decoder_masked_multihead_attention/decoder_masked_multihead_attention_template.cuh
+++ src/turbomind/kernels/decoder_masked_multihead_attention/decoder_masked_multihead_attention_template.cuh
@@ -1464,7 +1464,7 @@ __global__ void masked_multihead_attention_kernel(Multihead_attention_params<T> 
                     int offset = bhi * params.memory_max_len * Dh + co * params.memory_max_len * QK_ELTS_IN_16B
                                  + tlength_circ * QK_ELTS_IN_16B + ci;
 
-                    if (not QUANT_POLICY) {
+                    if (!QUANT_POLICY) {
                         *reinterpret_cast<Qk_vec_m*>(&params.k_cache[offset]) = vec_conversion<Qk_vec_m, Qk_vec_k>(k);
                     }
                     else if (QUANT_POLICY == 4) {
@@ -1486,7 +1486,7 @@ __global__ void masked_multihead_attention_kernel(Multihead_attention_params<T> 
                                  + tlength_circ * Dh + co * QK_ELTS_IN_16B + ci;
                     }
 
-                    if (not QUANT_POLICY) {
+                    if (!QUANT_POLICY) {
                         *reinterpret_cast<Qk_vec_m*>(&params.k_cache_per_sample[bi][offset]) =
                             vec_conversion<Qk_vec_m, Qk_vec_k>(k);
                     }
@@ -1577,7 +1577,7 @@ __global__ void masked_multihead_attention_kernel(Multihead_attention_params<T> 
     T*      k_cache_batch      = nullptr;
     int8_t* k_cache_batch_int8 = nullptr;
 
-    if (not QUANT_POLICY) {
+    if (!QUANT_POLICY) {
         k_cache_batch = params.k_cache_per_sample ? (params.k_cache_per_sample[bi] + params.kv_cache_per_sample_offset
                                                      + kvhi * params.memory_max_len * Dh + ki) :
                                                     &params.k_cache[bhi * params.memory_max_len * Dh + ki];
@@ -1630,7 +1630,7 @@ __global__ void masked_multihead_attention_kernel(Multihead_attention_params<T> 
                         beam_offset = beam_indices[ti_circ] * params.num_heads * params.memory_max_len * Dh;
                     }
 
-                    if (not QUANT_POLICY) {
+                    if (!QUANT_POLICY) {
                         k[ii] = vec_conversion<K_vec_k, K_vec_m>(
                             (*reinterpret_cast<const K_vec_m*>(&k_cache_batch[beam_offset + jj * QK_ELTS_IN_16B])));
                     }
@@ -1764,7 +1764,7 @@ __global__ void masked_multihead_attention_kernel(Multihead_attention_params<T> 
     int8_t* v_cache_int8       = nullptr;
     int8_t* v_cache_batch_int8 = nullptr;
 
-    if (not QUANT_POLICY) {
+    if (!QUANT_POLICY) {
 
         v_cache = params.v_cache_per_sample ? (params.v_cache_per_sample[bi] + params.kv_cache_per_sample_offset
                                                + kvhi * params.memory_max_len * Dh + vi) :
@@ -1819,7 +1819,7 @@ __global__ void masked_multihead_attention_kernel(Multihead_attention_params<T> 
             // Load the values from the cache.
             V_vec_k v;
 
-            if (not QUANT_POLICY) {
+            if (!QUANT_POLICY) {
                 v = vec_conversion<V_vec_k, V_vec_m>(
                     *reinterpret_cast<const V_vec_m*>(&v_cache_batch[beam_offset + ti * Dh]));
             }
@@ -1866,7 +1866,7 @@ __global__ void masked_multihead_attention_kernel(Multihead_attention_params<T> 
             const int beam_offset = HAS_BEAMS ? beam_src * params.num_heads * params.memory_max_len * Dh : 0;
             // Load the values from the cache.
             V_vec_k v;
-            if (not QUANT_POLICY) {
+            if (!QUANT_POLICY) {
                 v = vec_conversion<V_vec_k, V_vec_m>(
                     *reinterpret_cast<const V_vec_m*>(&v_cache_batch[beam_offset + ti_circ * Dh]));
             }
@@ -1926,7 +1926,7 @@ __global__ void masked_multihead_attention_kernel(Multihead_attention_params<T> 
             // Store the values with bias back to global memory in the cache for V.
             //*reinterpret_cast<V_vec_k*>(&v_cache[params.timestep*Dh]) = v;
 
-            if (not QUANT_POLICY) {
+            if (!QUANT_POLICY) {
                 *reinterpret_cast<V_vec_m*>(&v_cache[tlength_circ * Dh]) = vec_conversion<V_vec_m, V_vec_k>(v);
             }
             else if (QUANT_POLICY == 4) {
