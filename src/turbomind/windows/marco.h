d57bd0150337680feed4269fc350fecf011f557f
Xin Chen <xinchen.tju@gmail.com>
2023-08-07 23:29:02 +0800

    better build pthread-win32

--- src/turbomind/windows/marco.h
+++ src/turbomind/windows/marco.h
@@ -1,11 +1,17 @@
 #pragma once
 
 #if !defined(__PRETTY_FUNCTION__) && !defined(__GNUC__)
+
 #define __PRETTY_FUNCTION__ __FUNCSIG__
+
+namespace turbomind {
 typedef struct timeval {
     long tv_sec;
     long tv_usec;
 } TIMEVAL, *PTIMEVAL, *LPTIMEVAL;
+
+}  // namespace turbomind
+
 #endif
 
 typedef unsigned int uint;
