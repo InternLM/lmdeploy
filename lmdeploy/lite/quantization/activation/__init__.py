a5fcc471294f13cdef69fa38ed36e5670e29db2e
pppppM <gjf_mail@126.com>
2023-08-07 18:40:30 +0800

    add observers and quantizers

---
+++ lmdeploy/lite/quantization/activation/__init__.py
@@ -0,0 +1,2 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .observer import KVCacheObserver, ActivationObserver
---
+++ lmdeploy/lite/quantization/weight/__init__.py
@@ -0,0 +1,3 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .quantizer import WeightQuantizer
+from .observer import WeightObserver
