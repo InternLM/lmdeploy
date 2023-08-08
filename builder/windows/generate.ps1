953652e7f112800e33ff73320ec08243bd398c6c
Xin Chen <xinchen.tju@gmail.com>
2023-08-08 14:45:23 +0800

    update build readme

---
+++ builder/windows/generate.ps1
@@ -0,0 +1,10 @@
+cmake .. -A x64 -T v142,cuda="$env:CUDA_PATH" `
+    -DCMAKE_BUILD_TYPE=Release `
+    -DCMAKE_INSTALL_PREFIX=install `
+    -DBUILD_PY_FFI=ON `
+    -DBUILD_MULTI_GPU=OFF `
+    -DCMAKE_CUDA_FLAGS="-lineinfo" `
+    -DUSE_NVTX=ON
+
+
+
