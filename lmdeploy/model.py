5da37fcdb0041965f52e034e307b713f14ad808b
AllentDan <AllentDan@yeah.net>
2023-08-11 16:57:06 +0800

    pass args like meta_prompt to model

--- lmdeploy/model.py
+++ lmdeploy/model.py
@@ -8,12 +8,18 @@ MODELS = Registry('model', locations=['lmdeploy.model'])
 class BaseModel:
     """Base model."""
 
-    def __init__(self):
-        self.session_len = 2048
-        self.top_p = 0.8
-        self.top_k = None
-        self.temperature = 0.8
-        self.repetition_penalty = 1.0
+    def __init__(self,
+                 session_len=2048,
+                 top_p=0.8,
+                 top_k=None,
+                 temperature=0.8,
+                 repetition_penalty=1.0,
+                 **kwargs):
+        self.session_len = session_len
+        self.top_p = top_p
+        self.top_k = top_k
+        self.temperature = temperature
+        self.repetition_penalty = repetition_penalty
 
     @staticmethod
     def get_prompt(prompt, sequence_start=True):
@@ -39,11 +45,13 @@ class BaseModel:
 class Vicuna(BaseModel):
     """Chat template of vicuna model."""
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         self.system = """A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. """  # noqa: E501
-        self.user = 'USER'
-        self.assistant = 'ASSISTANT'
+        self.user = kwargs['user'] if 'user' in kwargs else 'USER'
+        self.assistant = kwargs[
+            'assistant'] if 'assistant' in kwargs else 'ASSISTANT'
+        self.system = kwargs['system'] if 'system' in kwargs else self.system
 
     def get_prompt(self, prompt, sequence_start=True):
         """Return the prompt that is concatenated with other elements in the
@@ -65,21 +73,22 @@ class Vicuna(BaseModel):
 @MODELS.register_module(name='internlm')
 class InternLM(BaseModel):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
 
 @MODELS.register_module(name='internlm-chat-7b')
 class InternLMChat7B(BaseModel):
     """Chat template of InternLM model."""
 
-    def __init__(self):
-        super().__init__()
-        self.system = ''
-        self.user = '<|User|>'
-        self.eoh = '<eoh>'
-        self.eoa = '<eoa>'
-        self.assistant = '<|Bot|>'
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.system = kwargs['system'] if 'system' in kwargs else ''
+        self.user = kwargs['user'] if 'user' in kwargs else '<|User|>'
+        self.eoh = kwargs['eoh'] if 'eoh' in kwargs else '<eoh>'
+        self.eoa = kwargs['eoa'] if 'eoa' in kwargs else '<eoa>'
+        self.assistant = kwargs[
+            'assistant'] if 'assistant' in kwargs else '<|Bot|>'
 
     def get_prompt(self, prompt, sequence_start=True):
         """Return the prompt that is concatenated with other elements in the
@@ -108,9 +117,10 @@ class InternLMChat7B(BaseModel):
 @MODELS.register_module(name='internlm-chat-7b-8k')
 class InternLMChat7B8K(InternLMChat7B):
 
-    def __init__(self):
-        super(InternLMChat7B8K, self).__init__()
-        self.session_len = 8192
+    def __init__(self, **kwargs):
+        super(InternLMChat7B8K, self).__init__(**kwargs)
+        self.session_len = kwargs[
+            'session_len'] if 'session_len' in kwargs else 8192
 
 
 @MODELS.register_module(name='puyu')
@@ -118,16 +128,18 @@ class Puyu(BaseModel):
     """Chat template of puyu model.This is only for internal usage in Shanghai
     AI Laboratory."""
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         self.system = """meta instruction
 You are an AI assistant whose name is InternLM (书生·浦语).
 - 书生·浦语 is a conversational language model that is developed by Shanghai AI Laboratory (上海人工智能实验室). It is designed to be helpful, honest, and harmless.
 - 书生·浦语 can understand and communicate fluently in the language chosen by the user such as English and 中文.
 conversation"""  # noqa: E501
-        self.user = '<|Human|>'
-        self.eoh = 'െ'
-        self.assistant = '<|Assistant|>'
+        self.user = kwargs['user'] if 'user' in kwargs else '<|Human|>'
+        self.eoh = kwargs['eoh'] if 'eoh' in kwargs else 'െ'
+        self.assistant = kwargs[
+            'assistant'] if 'assistant' in kwargs else '<|Assistant|>'
+        self.system = kwargs['system'] if 'system' in kwargs else self.system
 
     def get_prompt(self, prompt, sequence_start=True):
         if sequence_start:
@@ -146,9 +158,10 @@ conversation"""  # noqa: E501
 @MODELS.register_module(name='baichuan-7b')
 class Baichuan7B(BaseModel):
 
-    def __init__(self):
-        super().__init__()
-        self.repetition_penalty = 1.1
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.repetition_penalty = kwargs[
+            'repetition_penalty'] if 'repetition_penalty' in kwargs else 1.1
 
 
 @MODELS.register_module(name='llama2')
