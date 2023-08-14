19271be5b53850779b7a102a58f5b4e53f2edd96
Xin Chen <irexyc@gmail.com>
2023-08-15 01:17:19 +0800

    remove pthread-win32

--- src/turbomind/windows/macro.h
+++ src/turbomind/windows/macro.h
@@ -4,14 +4,6 @@
 
 #define __PRETTY_FUNCTION__ __FUNCSIG__
 
-namespace turbomind {
-typedef struct timeval {
-    long tv_sec;
-    long tv_usec;
-} TIMEVAL, *PTIMEVAL, *LPTIMEVAL;
-
-}  // namespace turbomind
-
 #endif
 
 typedef unsigned int uint;
