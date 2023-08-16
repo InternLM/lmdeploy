73684b4b537b64ec28ba1607143be826fa526413
Xin Chen <irexyc@gmail.com>
2023-08-16 15:20:34 +0800

    fix lint

--- src/turbomind/utils/cuda_utils.h
+++ src/turbomind/utils/cuda_utils.h
@@ -17,9 +17,9 @@
 #pragma once
 
 #include "3rdparty/INIReader.h"
+#include "src/turbomind/macro.h"
 #include "src/turbomind/utils/cuda_bf16_wrapper.h"
 #include "src/turbomind/utils/logger.h"
-#include "src/turbomind/macro.h"
 
 #include <cublasLt.h>
 #include <cublas_v2.h>
