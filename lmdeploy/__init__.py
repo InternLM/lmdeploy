d4d609bd8f513377bd9745974d350960ec78b087
Chen Xin <irexyc@gmail.com>
2023-08-29 16:14:43 +0800

    Fix turbomind import error on windows (#316)

--- lmdeploy/__init__.py
+++ lmdeploy/__init__.py
@@ -1,1 +1,16 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+def bootstrap():
+    import os
+    import sys
+
+    has_turbomind = False
+    pwd = os.path.dirname(__file__)
+    if os.path.exists(os.path.join(pwd, 'lib')):
+        has_turbomind = True
+    if os.name == 'nt' and has_turbomind:
+        if sys.version_info[:2] >= (3, 8):
+            CUDA_PATH = os.getenv('CUDA_PATH')
+            os.add_dll_directory(os.path.join(CUDA_PATH, 'bin'))
+
+
+bootstrap()
