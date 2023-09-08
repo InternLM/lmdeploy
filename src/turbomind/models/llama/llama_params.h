02ab71bdba7d6e2ab5190c5a5bb72d93f66a38e4
lvhan028 <lvhan_028@163.com>
2023-09-08 18:02:57 +0800

    rotary_emb_base to rotary_embedding_base

--- src/turbomind/models/llama/llama_params.h
+++ src/turbomind/models/llama/llama_params.h
@@ -6,7 +6,7 @@ namespace turbomind {
 
 struct LlamaAttentionParams {
     int   rotray_embedding_dim;
-    float rotary_emb_base;
+    float rotary_embedding_base;
     int   max_position_embeddings;
     bool  use_dynamic_ntk;
     bool  use_logn_attn;
