9f17d2288134fffb2c66a23cc9210dcba035196b
lvhan028 <lvhan_028@163.com>
2023-09-28 12:05:35 +0800

    update

--- src/turbomind/triton_backend/llama/LlamaTritonModel.h
+++ src/turbomind/triton_backend/llama/LlamaTritonModel.h
@@ -109,7 +109,7 @@ private:
     std::shared_ptr<typename ft::LlamaV2<T>::SharedState> shared_state_;
 
     std::vector<std::shared_ptr<LlamaTritonSharedModelInstance<T>>> shared_instances_;
-    std::deque<std::mutex>                                        shared_mutexes_;  // is locking really needed?
+    std::deque<std::mutex>                                          shared_mutexes_;  // is locking really needed?
 
     bool is_fp16_;
     int  enable_custom_all_reduce_ = 0;
