4feecabd4876f0366c81f023638937522693f8cd
grimoire <yaoqian@pjlab.org.cn>
2023-10-09 18:50:07 +0800

    optimize block manager

--- lmdeploy/pytorch_poc/messages.py
+++ lmdeploy/pytorch_poc/messages.py
@@ -1,7 +1,12 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import enum
+import time
+from copy import deepcopy
 from dataclasses import dataclass, field
-from typing import Any, List, Sequence
+from typing import Any, Dict, List, Sequence
+
+import torch
+from torch import Tensor
 
 from lmdeploy.pytorch_poc.block import LogicalTokenBlock
 
@@ -42,16 +47,83 @@ class MessageStatus(enum.Enum):
     ABORTED = enum.auto()
 
 
+_MSG_COUNT = 0
+
+
+def _new_msg_id():
+    """get a new message id."""
+    global _MSG_COUNT
+    msg_id = _MSG_COUNT
+    _MSG_COUNT += 1
+    return msg_id
+
+
 class SchedulerSession:
     """Scheduler session."""
 
-    def __init__(self, session_id: int, arrive_time: float = 0.0) -> None:
+    def __init__(self, session_id: int) -> None:
         self.session_id = session_id
-        self.logical_blocks: Sequence[LogicalTokenBlock] = []
-        self.block_table = {}
-        self.status: MessageStatus = MessageStatus.WAITING
-        self.arrive_time: float = arrive_time
-        self.history_length: int = 0
+        self.messages: Dict[SchedulerMessage] = dict()
+
+    def add_message(
+            self,
+            token_ids: Tensor,
+            max_output_len: int = 512,
+            sampling_param: SamplingParam = None) -> 'SchedulerMessage':
+        """Add a new message."""
+        if sampling_param is None:
+            sampling_param = SamplingParam()
+
+        msg = SchedulerMessage(msg_id=_new_msg_id(),
+                               token_ids=token_ids,
+                               session=self,
+                               status=MessageStatus.WAITING,
+                               remain_output_len=max_output_len,
+                               sampling_param=sampling_param,
+                               arrive_time=time.time())
+        self.messages[msg.msg_id] = msg
+        return msg
+
+    def fork_message(self,
+                     token_ids: Tensor,
+                     msg: 'SchedulerMessage',
+                     max_output_len: int = 512) -> 'SchedulerMessage':
+        """Fork a new message from exist message."""
+        assert msg.session == self
+
+        new_msg = SchedulerMessage(
+            msg_id=_new_msg_id(),
+            token_ids=token_ids,
+            session=self,
+            history_token_ids=msg.history_token_ids.clone(),
+            status=msg.status,
+            remain_output_len=max_output_len,
+            logical_blocks=deepcopy(msg.logical_blocks),
+            sampling_param=deepcopy(msg.sampling_param),
+            arrive_time=time.time(),
+            meta=deepcopy(msg.meta))
+
+        self.messages[new_msg.msg_id] = new_msg
+        return new_msg
+
+
+@dataclass
+class SchedulerMessage:
+    """Scheduler message."""
+    msg_id: int
+    token_ids: Tensor
+    session: SchedulerSession
+    history_token_ids: Tensor = field(default=torch.empty(0, dtype=torch.long))
+    status: MessageStatus = MessageStatus.WAITING
+    remain_output_len: int = 0
+    logical_blocks: Sequence[LogicalTokenBlock] = field(default_factory=list)
+    sampling_param: SamplingParam = field(default_factory=SamplingParam)
+    arrive_time: float = 0.0
+    meta: Any = None
+
+    @property
+    def history_len(self):
+        return len(self.history_token_ids)
 
     def append_tokens(self, num_tokens: int, block_size: int):
         """Append new tokens, update logical blocks.
@@ -78,17 +150,3 @@ class SchedulerSession:
                                               block_size)
             logical_block.append_tokens(num_tokens=num_tokens)
             self.logical_blocks.append(logical_block)
-
-
-@dataclass
-class SchedulerMessage:
-    """Scheduler message."""
-
-    token_ids: Sequence
-    session_id: int
-    status: MessageStatus = MessageStatus.WAITING
-    max_request_output_len: int = 0
-    request_output_len: int = 0
-    meta: Any = None
-    req_id: int = 0
-    sampling_param: SamplingParam = field(default_factory=SamplingParam)
