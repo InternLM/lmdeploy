9780108bed3df1442991e273774bd88efc566b9e
Li Zhang <lzhang329@gmail.com>
2023-11-29 13:01:44 +0000

    lint

--- src/turbomind/utils/dispatch.h
+++ src/turbomind/utils/dispatch.h
@@ -15,7 +15,7 @@ template<class F, class P, class G, int... Xs, std::size_t... Is>
 bool dispatch_impl(F&& f, P&& p, G g, std::integer_sequence<int, Xs...>, std::index_sequence<Is...>)
 {
     constexpr int N = sizeof...(Xs);
-    return (((((P&&)p)(_Int<Xs>) || (g && Is == N - 1)) && (((F&&)f)(_Int<Xs>), 1)) || ...);
+    return (((((P &&) p)(_Int<Xs>) || (g && Is == N - 1)) && (((F &&) f)(_Int<Xs>), 1)) || ...);
 }
 
 }  // namespace detail
@@ -23,13 +23,13 @@ bool dispatch_impl(F&& f, P&& p, G g, std::integer_sequence<int, Xs...>, std::in
 template<class F, class P, int... Is, class G = std::true_type>
 bool dispatch(std::integer_sequence<int, Is...> seq, P&& p, F&& f, G g = {})
 {
-    return detail::dispatch_impl((F&&)f, (P&&)p, g, seq, std::make_index_sequence<sizeof...(Is)>{});
+    return detail::dispatch_impl((F &&) f, (P &&) p, g, seq, std::make_index_sequence<sizeof...(Is)>{});
 }
 
 template<class F, int... Is, class G = std::true_type>
 bool dispatch(std::integer_sequence<int, Is...> seq, F&& f)
 {
-    return (((F&&)f)(detail::_Int<Is>) || ...);
+    return (((F &&) f)(detail::_Int<Is>) || ...);
 }
 
 }  // namespace turbomind
