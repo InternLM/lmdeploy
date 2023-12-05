9c1a4be526fa1930f481995973d65f6dd50e3af3
grimoire <yaoqian@sensetime.com>
2023-12-05 18:11:58 +0800

    fix python lint

--- lmdeploy/turbomind/deploy/target_model/bf16.py
+++ lmdeploy/turbomind/deploy/target_model/bf16.py
@@ -1,9 +1,16 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from lmdeploy.turbomind.deploy.source_model.base import BaseInputModel
+
 from .base import OUTPUT_MODELS, TurbomindModelConfig
 from .fp import TurbomindModel
 
+
 @OUTPUT_MODELS.register_module(name='bf16')
 class TurbomindBF16Model(TurbomindModel):
-    def __init__(self, input_model: BaseInputModel, cfg: TurbomindModelConfig, to_file: bool = True, out_dir: str = ''):
+
+    def __init__(self,
+                 input_model: BaseInputModel,
+                 cfg: TurbomindModelConfig,
+                 to_file: bool = True,
+                 out_dir: str = ''):
         super().__init__(input_model, cfg, to_file, out_dir)
