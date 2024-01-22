d6ffcac50d750977fdbccfa92273307107e6a9ff
zhulin1 <zhulin1@pjlab.org.cn>
2024-01-22 20:22:45 +0800

    add pipeline chat

---
+++ autotest/utils/pipeline_chat.py
@@ -0,0 +1,31 @@
+from lmdeploy import pipeline
+from lmdeploy.messages import (GenerationConfig, PytorchEngineConfig, TurbomindEngineConfig,Response)
+
+class PipelineChat:
+
+    def __init__(self,
+                 hf_path: str, 
+                 tp: int == 1):
+        self.hf_path = hf_path
+        backend_config = TurbomindEngineConfig(tp = tp)
+        pipe = pipeline(hf_path, backend_config=backend_config)
+        self.pipe = pipe
+
+    def default_pipeline_chat(self, prompt):
+        gen_config = GenerationConfig(temperature=0.01)
+        return self.pipe([prompt], gen_config=gen_config)[0]
+
+class PipelinePytorchChat:
+
+    def __init__(self,
+                 hf_path: str, 
+                 tp: int == 1):
+        self.hf_path = hf_path
+        backend_config = PytorchEngineConfig(tp = tp)
+        pipe = pipeline(hf_path, backend_config=backend_config)
+        self.pipe = pipe
+
+
+    def default_pipeline_chat(self, prompt):
+        gen_config = GenerationConfig(temperature=0.01)
+        return self.pipe([prompt], gen_config=gen_config)[0]
