3e3328bd95ce094d83dfe609dfe6d33874289ec4
zhyncs <me@zhyncs.com>
2024-01-26 15:54:08 +0800

    fix comment

--- lmdeploy/turbomind/turbomind.py
+++ lmdeploy/turbomind/turbomind.py
@@ -2,7 +2,6 @@
 import asyncio
 import copy
 import logging
-import os
 import os.path as osp
 import sys
 from configparser import ConfigParser
@@ -88,20 +87,14 @@ def _update_tm_config(dst: TurbomindModelConfig, src: TurbomindEngineConfig):
     return TurbomindModelConfig.from_dict(dst_dict)
 
 
-def _compare_individual_gpu_memory():
+def _compare_individual_gpu_memory(tp: int):
     logging.basicConfig(level=logging.INFO)
-
-    devices = os.getenv('CUDA_VISIBLE_DEVICES', '')
-    device_ids = list(map(int, devices.split(','))) if devices else []
 
     try:
         total_mem = []
         free_mem = []
 
-        for i in range(torch.cuda.device_count()):
-            if device_ids and i not in device_ids:
-                continue
-
+        for i in range(tp):
             torch.cuda.set_device(i)
             free, total = torch.cuda.mem_get_info()
             total_mem.append(total / (1024**2))
@@ -113,7 +106,7 @@ def _compare_individual_gpu_memory():
         if not all_total_equal or not all_free_equal:
             logging.warning(
                 f'Memory discrepancy detected: Total Memory={total_mem} MB, \
-                    Free Memory={free_mem} MB')
+Free Memory={free_mem} MB')
 
     except Exception as e:
         logging.error(f'An exception occurred: {e}')
@@ -154,7 +147,7 @@ class TurboMind:
                  **kwargs):
         # check memory equality when tp
         if tp is not None and tp > 1:
-            _compare_individual_gpu_memory()
+            _compare_individual_gpu_memory(tp)
         # check model_name equal in engine_config and passed in
         if engine_config is not None and engine_config.model_name is not None:
             if model_name is not None:
