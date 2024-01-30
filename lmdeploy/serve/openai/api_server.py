e860bcb6ca3b579ba90dcf2d3b377fb36a251121
zhyncs <me@zhyncs.com>
2024-01-30 20:59:08 +0800

    fix use TM_LOG_LEVEL environment variable first

--- lmdeploy/serve/openai/api_server.py
+++ lmdeploy/serve/openai/api_server.py
@@ -949,7 +949,8 @@ def serve(model_path: str,
         log_level(str): set log level whose value among [CRITICAL, ERROR, WARNING, INFO, DEBUG]
         qos_config_path (str): qos policy config path
     """ # noqa E501
-    os.environ['TM_LOG_LEVEL'] = log_level
+    if os.getenv('TM_LOG_LEVEL') is None:
+        os.environ['TM_LOG_LEVEL'] = log_level
 
     if allow_origins:
         app.add_middleware(
