fddefc9f40a0520e4d44df61497515442a846ed4
lvhan028 <lvhan_028@163.com>
2024-02-21 18:36:32 +0800

    update

--- benchmark/benchmark_pytorch_engine_a100.sh
+++ benchmark/benchmark_pytorch_engine_a100.sh
@@ -5,25 +5,29 @@ dataset_path="benchmark/ShareGPT_V3_unfiltered_cleaned_split.json"
 ## 7B
 tp=1
 max_batch_size=256
+cache_max_entry_count=0.95
 model_path="/workspace/models-140/llama2/huggingface/llama-2-7b-chat"
-CUDA_VISIBLE_DEVICES="4" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path} --backend pytorch --tp ${tp} --concurrency ${max_batch_size}
+CUDA_VISIBLE_DEVICES="6" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path} --backend pytorch --tp ${tp} --concurrency ${max_batch_size} --cache-max-entry-count ${cache_max_entry_count} --csv llama2_pt_7b_thr.csv
 
 ## 13B
 tp=1
 max_batch_size=256
+cache_max_entry_count=0.9
 model_path="/workspace/models-140/llama2/huggingface/llama-2-13b-chat"
-CUDA_VISIBLE_DEVICES="4" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path} --backend pytorch --tp ${tp} --concurrency ${max_batch_size}
+CUDA_VISIBLE_DEVICES="6" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path} --backend pytorch --tp ${tp} --concurrency ${max_batch_size} --cache-max-entry-count ${cache_max_entry_count} --csv llama2_pt_7b_thr.csv
 
 # 20B
 tp=2
 max_batch_size=256
+cache_max_entry_count=0.9
 model_path="/workspace/models-140/InternLM/internlm-chat-20b"
-CUDA_VISIBLE_DEVICES="4,5" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path}  --backend pytorch --tp ${tp} --concurrency ${max_batch_size}
+CUDA_VISIBLE_DEVICES="5,6" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path}  --backend pytorch --tp ${tp} --concurrency ${max_batch_size} --cache-max-entry-count ${cache_max_entry_count} --csv llama2_pt_7b_thr.csv
 
 # 70B
 tp=4
 max_batch_size=256
+cache_max_entry_count=0.9
 model_path="/workspace/models-140/llama2/huggingface/llama-2-70b-chat-hf"
-CUDA_VISIBLE_DEVICES="4,5,6,7" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path}  --backend pytorch --tp ${tp} --concurrency ${max_batch_size}
+CUDA_VISIBLE_DEVICES="4,5,6,7" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path}  --backend pytorch --tp ${tp} --concurrency ${max_batch_size} --cache-max-entry-count ${cache_max_entry_count} --csv llama2_pt_7b_thr.csv
 
 ########################################## PyTorch engine: w8a8 ##########################################
--- benchmark/benchmark_turbomind_engine_a100.sh
+++ benchmark/benchmark_turbomind_engine_a100.sh
@@ -8,7 +8,7 @@ max_batch_size=256
 cache_max_entry_count=0.95
 model_path="/workspace/models-140/llama2/huggingface/llama-2-7b-chat"
 CUDA_VISIBLE_DEVICES="6" python3 -m lmdeploy.turbomind.generate_gemm_config --tensor-para-size ${tp} --max-batch-size ${max_batch_size} --model-path ${model_path}
-CUDA_VISIBLE_DEVICES="6" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path} --tp ${tp} --concurrency ${max_batch_size} --cache-max-entry-count ${cache_max_entry_count} --csv llama2_7b_thr.csv
+CUDA_VISIBLE_DEVICES="6" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path} --tp ${tp} --concurrency ${max_batch_size} --cache-max-entry-count ${cache_max_entry_count} --csv llama2_tb_7b_thr.csv
 rm gemm_config.in
 
 # 13B. gemm_tune -> profile_throughput
@@ -17,7 +17,7 @@ max_batch_size=256
 cache_max_entry_count=0.9
 model_path="/workspace/models-140/llama2/huggingface/llama-2-13b-chat"
 CUDA_VISIBLE_DEVICES="6" python3 -m lmdeploy.turbomind.generate_gemm_config --tensor-para-size ${tp} --max-batch-size ${max_batch_size} --model-path ${model_path}
-CUDA_VISIBLE_DEVICES="6" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path} --tp ${tp} --concurrency ${max_batch_size} --cache-max-entry-count ${cache_max_entry_count} --csv llama2_13b_thr.csv
+CUDA_VISIBLE_DEVICES="6" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path} --tp ${tp} --concurrency ${max_batch_size} --cache-max-entry-count ${cache_max_entry_count} --csv llama2_tb_13b_thr.csv
 rm gemm_config.in
 
 # 20B. gemm_tune -> profile_throughput
@@ -26,7 +26,7 @@ max_batch_size=256
 cache_max_entry_count=0.9
 model_path="/workspace/models-140/InternLM/internlm-chat-20b"
 CUDA_VISIBLE_DEVICES="5,6" python3 -m lmdeploy.turbomind.generate_gemm_config --tensor-para-size ${tp} --max-batch-size ${max_batch_size} --model-path ${model_path}
-CUDA_VISIBLE_DEVICES="5,6" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path} --tp ${tp} --concurrency ${max_batch_size} --cache-max-entry-count ${cache_max_entry_count} --csv internlm_20b_thr.csv
+CUDA_VISIBLE_DEVICES="5,6" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path} --tp ${tp} --concurrency ${max_batch_size} --cache-max-entry-count ${cache_max_entry_count} --csv internlm_tb_20b_thr.csv
 rm gemm_config.in
 
 # 70B
@@ -34,7 +34,7 @@ tp=4
 max_batch_size=256
 cache_max_entry_count=0.9
 model_path="/workspace/models-140/llama2/huggingface/llama-2-70b-chat-hf"
-CUDA_VISIBLE_DEVICES="4,5,6,7" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path} --tp ${tp} --concurrency ${max_batch_size} --cache-max-entry-count ${cache_max_entry_count} --csv llama2_70b_thr.csv
+CUDA_VISIBLE_DEVICES="4,5,6,7" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path} --tp ${tp} --concurrency ${max_batch_size} --cache-max-entry-count ${cache_max_entry_count} --csv llama2_tb_70b_thr.csv
 
 # ########################################## TurboMind engine: w4a16 ##########################################
 # 7B
@@ -42,25 +42,25 @@ tp=1
 max_batch_size=256
 cache_max_entry_count=0.95
 model_path="/workspace/models/quantization/llama-2-7b-chat-4bit"
-CUDA_VISIBLE_DEVICES="6" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path} --tp ${tp} --concurrency ${max_batch_size} --cache-max-entry-count ${cache_max_entry_count} --model-format awq --num-prompts 10000 --csv llama2_7b_4bit_thr.csv
+CUDA_VISIBLE_DEVICES="6" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path} --tp ${tp} --concurrency ${max_batch_size} --cache-max-entry-count ${cache_max_entry_count} --model-format awq --num-prompts 10000 --csv llama2_tb_7b_4bit_thr.csv
 
 # 13B
 tp=1
 max_batch_size=256
 cache_max_entry_count=0.9
 model_path="/workspace/models/quantization/llama-2-13b-chat-4bit"
-CUDA_VISIBLE_DEVICES="6" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path} --tp ${tp} --concurrency ${max_batch_size} --cache-max-entry-count ${cache_max_entry_count} --model-format awq --num-prompts 10000 --csv llama2_13b_4bit_thr.csv
+CUDA_VISIBLE_DEVICES="6" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path} --tp ${tp} --concurrency ${max_batch_size} --cache-max-entry-count ${cache_max_entry_count} --model-format awq --num-prompts 10000 --csv llama2_tb_13b_4bit_thr.csv
 
 # 20B
 tp=2
 max_batch_size=256
 cache_max_entry_count=0.9
 model_path="/workspace/models/quantization/internlm-chat-20b-4bit"
-CUDA_VISIBLE_DEVICES="5,6" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path} --tp ${tp} --concurrency ${max_batch_size} --cache-max-entry-count ${cache_max_entry_count} --model-format awq --num-prompts 10000 --csv internlm_20b_4bit_thr.csv
+CUDA_VISIBLE_DEVICES="5,6" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path} --tp ${tp} --concurrency ${max_batch_size} --cache-max-entry-count ${cache_max_entry_count} --model-format awq --num-prompts 10000 --csv internlm_tb_20b_4bit_thr.csv
 
 # 70B
 tp=4
 max_batch_size=256
 cache_max_entry_count=0.9
 model_path="/workspace/models/quantization/llama-2-70b-chat-hf-4bit"
-CUDA_VISIBLE_DEVICES="4,5,6,7" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path} --tp ${tp} --concurrency ${max_batch_size} --cache-max-entry-count ${cache_max_entry_count} --model-format awq --num-prompts 10000 --csv llama2_70b_4bit_thr.csv
+CUDA_VISIBLE_DEVICES="4,5,6,7" python3 benchmark/profile_throughput.py ${dataset_path} ${model_path} --tp ${tp} --concurrency ${max_batch_size} --cache-max-entry-count ${cache_max_entry_count} --model-format awq --num-prompts 10000 --csv llama2_tb_70b_4bit_thr.csv
