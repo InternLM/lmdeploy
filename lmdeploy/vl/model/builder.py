62b20ff308fbc10eee3340cba9382e09e7255648
irexyc <irexyc@gmail.com>
2024-02-29 04:29:57 +0000

    fix load vl model

--- lmdeploy/vl/model/builder.py
+++ lmdeploy/vl/model/builder.py
@@ -1,5 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import os
+
 from lmdeploy.turbomind.utils import get_hf_config_content
+from lmdeploy.utils import get_model
 
 from .llava import LlavaVLModel
 from .qwen import QwenVLModel
@@ -7,6 +10,8 @@ from .qwen import QwenVLModel
 
 def load_vl_model(model_path):
     config = get_hf_config_content(model_path)
+    if not os.path.exists(model_path):
+        model_path = get_model(model_path)
     arch = config['architectures'][0]
     if arch == 'QWenLMHeadModel':
         return QwenVLModel(model_path)
