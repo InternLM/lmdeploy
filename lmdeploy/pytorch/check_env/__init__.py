3399fb4087594aecb88c638fd48ae774824d41dd
grimoire <yaoqian@pjlab.org.cn>
2024-02-29 19:07:34 +0800

    check model required transformers version

--- lmdeploy/pytorch/check_env/__init__.py
+++ lmdeploy/pytorch/check_env/__init__.py
@@ -4,13 +4,18 @@ from logging import Logger
 from lmdeploy.utils import get_logger
 
 
-def _handle_exception(e: Exception, mod_name: str, logger: Logger):
+def _handle_exception(e: Exception,
+                      mod_name: str,
+                      logger: Logger,
+                      message: str = None):
     red_color = '\033[31m'
     reset_color = '\033[0m'
+    if message is None:
+        message = 'Please ensure it has been installed correctly.'
     logger.debug('Exception', exc_info=1)
     logger.error(f'{type(e).__name__}: {e}')
-    logger.error(f'{red_color}<{mod_name}> environment test failed. '
-                 'Please ensure it has been installed correctly.'
+    logger.error(f'{red_color}'
+                 f'<{mod_name}> test failed!\n {message}'
                  f'{reset_color}')
     exit(1)
 
@@ -54,3 +59,46 @@ def check_env():
     logger.info('Checking environment for PyTorch Engine.')
     check_env_torch()
     check_env_triton()
+
+
+def check_transformers_version(model_path: str):
+    """check transformers version."""
+    from packaging import version
+    logger = get_logger('lmdeploy')
+    logger.debug('Checking <transformers> version.')
+
+    trans_version = None
+    try:
+        import transformers
+        trans_version = version.parse(transformers.__version__)
+    except Exception as e:
+        _handle_exception(e, 'transformers', logger)
+
+    model_trans_version = None
+    try:
+        from transformers import AutoConfig
+        config = AutoConfig.from_pretrained(model_path)
+        model_trans_version = getattr(config, 'transformers_version')
+    except Exception as e:
+        message = (
+            f'Load model config with transformers=={trans_version} failed. '
+            'Please make sure model can be loaded with huggingface API.')
+        _handle_exception(e, 'transformers', logger, message=message)
+
+    try:
+        model_trans_version = version.parse(model_trans_version)
+        assert trans_version >= model_trans_version
+    except Exception as e:
+        message = {
+            f'model `{model_path}` requires '
+            f'transformers version {model_trans_version} '
+            f'but transformers {trans_version} is installed.'
+        }
+        _handle_exception(e, 'transformers', logger, message=message)
+
+
+def check_model(model_path: str):
+    """check model requirements."""
+    logger = get_logger('lmdeploy')
+    logger.info('Checking model.')
+    check_transformers_version(model_path)
