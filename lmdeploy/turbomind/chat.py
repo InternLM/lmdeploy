c3211026aeea785c0d20b5406a5b7f06cd78781c
ispobock <ispobaoke@163.com>
2024-02-29 17:18:53 +0800

    fix chat change

--- lmdeploy/turbomind/chat.py
+++ lmdeploy/turbomind/chat.py
@@ -31,17 +31,6 @@ def valid_str(string, coding='utf-8'):
     return ret
 
 
-def fill_config(cfg, kwargs):
-    """Fill config attributes with kwargs."""
-    new_kwargs = {}
-    for k, v in kwargs.items():
-        if hasattr(cfg, k):
-            setattr(cfg, k, v)
-        else:
-            new_kwargs[k] = v
-    return new_kwargs
-
-
 def main(model_path: str,
          model_name: str = None,
          session_id: int = 1,
@@ -50,7 +39,6 @@ def main(model_path: str,
          stream_output: bool = True,
          request_output_len: int = 1024,
          chat_template_cfg: ChatTemplateConfig = None,
-         engine_cfg: TurbomindEngineConfig = None,
          **kwargs):
     """An example to perform model inference through the command line
     interface.
@@ -71,10 +59,15 @@ def main(model_path: str,
     if chat_template_cfg is None:
         chat_template_cfg = ChatTemplateConfig(model_name=model_name,
                                                capability=cap)
-        kwargs = fill_config(chat_template_cfg, kwargs)
-    if engine_cfg is None:
-        engine_cfg = TurbomindEngineConfig(model_name=model_name)
-        kwargs = fill_config(engine_cfg, kwargs)
+        new_kwargs = {}
+        for k, v in kwargs.items():
+            if hasattr(chat_template_cfg, k):
+                setattr(chat_template_cfg, k, v)
+            else:
+                new_kwargs[k] = v
+        kwargs = new_kwargs
+
+    engine_cfg = TurbomindEngineConfig(**kwargs)
 
     tm_model = tm.TurboMind.from_pretrained(
         model_path,
