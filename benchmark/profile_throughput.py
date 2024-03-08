4e219576b918a94dfc77c87559a79c913f305fd8
ispobock <ispobaoke@163.com>
2024-03-08 20:06:24 +0800

    make sure enough data for small sample size

--- benchmark/profile_throughput.py
+++ benchmark/profile_throughput.py
@@ -34,7 +34,7 @@ def sample_requests(
                 data['conversations'][1]['value']) for data in dataset]
 
     # pre-sample to avoid go through all the dataset
-    dataset = random.sample(dataset, num_requests * 2)
+    dataset = random.sample(dataset, max(int(num_requests * 1.2), 1000))
 
     # Tokenize the prompts and completions.
     prompts = [prompt for prompt, _ in dataset]
