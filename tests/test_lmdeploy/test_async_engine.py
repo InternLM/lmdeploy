96f42845ca659d83273cb2fc290aba9d1962559c
AllentDan <AllentDan@yeah.net>
2024-03-11 14:23:10 +0800

    fix UT

--- tests/test_lmdeploy/test_async_engine.py
+++ tests/test_lmdeploy/test_async_engine.py
@@ -7,16 +7,16 @@ from lmdeploy.serve.async_engine import deduce_a_name
 
 @pytest.mark.parametrize(
     'backend_config',
-    [TurbomindEngineConfig('internlm-chat'),
+    [TurbomindEngineConfig('internlm'),
      PytorchEngineConfig(None), None])
 @pytest.mark.parametrize(
     'chat_template_config',
-    [ChatTemplateConfig('internlm-chat'),
+    [ChatTemplateConfig('internlm'),
      ChatTemplateConfig(None), None])
-@pytest.mark.parametrize('model_name', ['internlm-chat', None])
+@pytest.mark.parametrize('model_name', ['internlm', None])
 @pytest.mark.parametrize('model_path', ['/path/to/internlm-chat-7b'])
 def test_deduce_a_name(model_path, model_name, chat_template_config,
                        backend_config):
     name = deduce_a_name(model_path, model_name, chat_template_config,
                          backend_config)
-    assert name == 'internlm-chat'
+    assert name == 'internlm'
