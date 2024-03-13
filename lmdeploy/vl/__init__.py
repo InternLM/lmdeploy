c65e41531c74ba584ae40b35285f10e0ddcf6810
irexyc <irexyc@gmail.com>
2024-03-13 06:38:44 +0000

    remove unused

--- lmdeploy/vl/__init__.py
+++ lmdeploy/vl/__init__.py
@@ -1,6 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-
-from .api import pipeline
 from .utils import load_image_from_url
 
-__all__ = ['pipeline', 'load_image_from_url']
+__all__ = ['load_image_from_url']
