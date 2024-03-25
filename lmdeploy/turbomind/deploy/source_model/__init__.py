717a4ca9c5d0a0be582337fc0471c96aa98127f9
AllentDan <AllentDan@yeah.net>
2024-03-25 14:50:22 +0800

    fix

--- lmdeploy/turbomind/deploy/source_model/__init__.py
+++ lmdeploy/turbomind/deploy/source_model/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .baichuan import Baichuan2Model, BaichuanModel  # noqa: F401
 from .baichuan_awq import Baichuan2AwqModel, BaichuanAwqModel  # noqa: F401
-from .deepseek_vl import DeepSeekVLAwqModel, DeepSeekVLModel  # noqa: F401
+from .deepseek_vl import DeepSeekVLModel  # noqa: F401
 from .internlm2 import InternLM2AwqModel, InternLM2Model  # noqa: F401
 from .llama import LlamaModel  # noqa: F401
 from .llama_awq import LlamaAwqModel  # noqa: F401
