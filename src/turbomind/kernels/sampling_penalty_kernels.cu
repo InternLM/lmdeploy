0f404cbe78ed4f7fcf975ca13978eae4be4d5959
irexyc <irexyc@gmail.com>
2024-03-27 10:03:30 +0000

    fix batchApplyRepetitionPenalty

--- src/turbomind/kernels/sampling_penalty_kernels.cu
+++ src/turbomind/kernels/sampling_penalty_kernels.cu
@@ -412,6 +412,10 @@ __global__ void batchApplyRepetitionPenalty(T*           logits,
         }
     }
 
+    if (blockDim.x > 32) {
+        __syncthreads();
+    }
+
     // Phase 2. Replace a logit value by the penalized one.
     for (int index = threadIdx.x; index < step; index += blockDim.x) {
         // Skip the padding tokens in input sequences.
