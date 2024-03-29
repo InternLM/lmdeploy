5987d68213236e41ac1cc82927e2b66be0613569
zhulinJulia24 <145004780+zhulinJulia24@users.noreply.github.com>
2024-03-29 18:47:32 +0800

    Create env.sh

---
+++ .github/scripts/env.sh
@@ -0,0 +1,4 @@
+echo "MODEL_FORMAT2=--model-format awq2" >> "$GITHUB_ENV"
+echo "MODEL_FORMAT3=--model-format awq2" >> "$GITHUB_ENV"
+echo "MODEL_FORMAT4=--model-format awq2" >> "$GITHUB_ENV"
+echo "MODEL_FORMAT5=--model-format awq2" >> "$GITHUB_ENV"
