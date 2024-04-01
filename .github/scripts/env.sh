e10e0fe6632fa13f9ba9fa8f0781bd1e90a2761b
zhulinJulia24 <145004780+zhulinJulia24@users.noreply.github.com>
2024-04-01 13:22:56 +0800

    Update env.sh

--- .github/scripts/env.sh
+++ .github/scripts/env.sh
@@ -1,4 +1,25 @@
-echo "MODEL_FORMAT2=--model-format awq2" >> "$GITHUB_ENV"
-echo "MODEL_FORMAT3=--model-format awq2" >> "$GITHUB_ENV"
-echo "MODEL_FORMAT4=--model-format awq2" >> "$GITHUB_ENV"
-echo "MODEL_FORMAT5=--model-format awq2" >> "$GITHUB_ENV"
+#!/bin/bash
+if  [ -z "$1" ] 
+then
+    echo "please input model_name" >> "$GITHUB_ENV"
+    exit 1
+fi
+
+if [[ $1 == *"w4a16" ]] || [[ $1 == *"4bit" ]]
+then
+    echo "MODEL_FORMAT=--model-format awq" >> "$GITHUB_ENV"
+fi
+
+if [[ $1 == *"llama"* ]] || [[ $1 == *"Llama"* ]]
+then
+    echo "MAX_ENTRY_COUNT=--cache-max-entry-count 0.95" >> "$GITHUB_ENV"
+else
+    echo "MAX_ENTRY_COUNT=--cache-max-entry-count 0.90" >> "$GITHUB_ENV"
+fi
+
+
+if [[ $1 == *"internlm2-chat-20b"* ]]
+then 
+  echo "TP_INFO=--tp 2" >> "$GITHUB_ENV"
+fi
+
