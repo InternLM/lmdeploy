8d7da6d37a7e8b23c7fbef56c8bca2b0a46a46f3
grimoire <streetyao@live.com>
2024-04-19 12:54:20 +0800

    save cos/sin

--- lmdeploy/pytorch/models/internlm2.py
+++ lmdeploy/pytorch/models/internlm2.py
@@ -72,8 +72,14 @@ class PatchedInternLM2Attention(nn.Module):
 
         def __rotary_emb_fn(query_states, key_states, value_states):
             """rotary embedding func."""
-            cos, sin = self.rotary_emb(value_states.transpose(0, 1),
-                                       seq_len=max_kv_seq_length)
+            if not hasattr(context, '_cos'):
+                cos, sin = self.rotary_emb(value_states.transpose(0, 1),
+                                           seq_len=max_kv_seq_length)
+                context._cos = cos
+                context._sin = sin
+            else:
+                cos = context._cos
+                sin = context._sin
             query_states, key_states = apply_rotary_pos_emb(
                 query_states,
                 key_states,
