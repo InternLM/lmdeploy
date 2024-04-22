2ed7ca6cb5674c5c88a9c07ba89592426e418971
irexyc <irexyc@gmail.com>
2024-04-22 17:24:06 +0000

    fix serve

--- lmdeploy/cli/entrypoint.py
+++ lmdeploy/cli/entrypoint.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import os
 import sys
 
 from .chat import SubCliChat
@@ -10,8 +11,8 @@ from .serve import SubCliServe
 def run():
     """The entry point of running LMDeploy CLI."""
     args = sys.argv[1:]
-    is_deprecated_chat_cli = args[0] == 'chat' and (args[1]
-                                                    in ['torch', 'turbomind'])
+    is_deprecated_chat_cli = (len(args) > 0 and args[0] == 'chat') and \
+        (len(args) > 1 and args[1] in ['torch', 'turbomind'])
     CLI.add_parsers()
     if is_deprecated_chat_cli:
         SubCliChat.add_parsers()
@@ -23,6 +24,14 @@ def run():
     args = parser.parse_args()
 
     if 'run' in dir(args):
+        from lmdeploy.utils import get_model
+        model_path = getattr(args, 'model_path', None)
+        if model_path is not None and not os.path.exists(args.model_path):
+            args.model_path = get_model(args.model_path)
+        model_path_or_server = getattr(args, 'model_path_or_server', None)
+        if model_path_or_server is not None and ':' not in model_path_or_server:  # noqa: E501
+            args.model_path_or_server = get_model(args.model_path_or_server)
+
         args.run(args)
     else:
         try:
