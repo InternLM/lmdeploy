c1461677febe1fd82ef8ce11f40bf650b060b7d7
Epiphany1864 <zjh_epiphany@163.com>
2024-04-23 11:35:21 +0800

    edit exception

--- autotest/interface/triton/test_triton_press.py
+++ autotest/interface/triton/test_triton_press.py
@@ -1,29 +1,28 @@
-import string
-import yaml
 import random
-import pytest
-from utils.triton_engine import Engine
-from lmdeploy.serve.turbomind import chatbot
-from lmdeploy.serve.turbomind.chatbot import Session
+import string
 
+import pytest
+import yaml
+from utils.triton_engine import Engine
 
 SERVER_ADDR = '0.0.0.0:33337'
 
+
 def get_random_session_id():
-        characters = string.digits
-        random_chars = ''.join(random.choice(characters) for i in range(6))
-        return int(random_chars)
+    characters = string.digits
+    random_chars = ''.join(random.choice(characters) for i in range(6))
+    return int(random_chars)
+
 
 class Test_TritonInterface_press:
 
-    
+    @pytest.mark.tmp
     def test_diff_dict(self):
         with open('autotest/interface/triton/gen_case_dict.yaml', 'r') as f:
             case_config = yaml.load(f, Loader=yaml.SafeLoader)
 
-        try:
-            for case in case_config.keys():
-                print('dict case:' + case)
+        for case in case_config.keys():
+            try:
                 dict_info = case_config[case]
                 if dict_info is None:
                     dict_info = {}
@@ -32,173 +31,165 @@ class Test_TritonInterface_press:
                 engine = Engine(SERVER_ADDR, log_level='ERROR', **dict_info)
 
                 session_id = get_random_session_id()
-                engine.triton_infer(session_id, '你好，你叫什么名字', 'req_id:' + case, 500, True, True)
+                engine.triton_infer(session_id, '你好，你叫什么名字', 'req_id:' + case,
+                                    500, True, True)
                 engine.triton_resume(session_id)
                 engine.triton_end(session_id)
                 engine.triton_set_session(None)
-        except Exception as e:
-            print(f'error: {e}')
+            except Exception as e:
+                print(f'error: {e}')
 
-
-    
     def test_start_stop_param(self):
-            dict_info = {
-                'assistant': '<|Assistant|>െ',
-                'eoa': 'ി\n ',
-                'eoh': '\n ',
-                'eosys': '\n ',
-                'meta_instruction':
-                'You are an AI assistant whose name is InternLM (书生·浦语).',
-                'meta_tag': '<BOS>',
-                'model_name': 'puyu',
-                'repetition_penalty': 1.02,
-                'replace_token': 'ി',
-                'session_len': 8192,
-                'stop_words': ['ി'],
-                'system': '<|System|>െ',
-                'temperature': 1,
-                'top_k': 40,
-                'top_p': 0.8,
-                'user': '<|Human|>െ'
-            }
+        dict_info = {
+            'assistant': '<|Assistant|>െ',
+            'eoa': 'ി\n ',
+            'eoh': '\n ',
+            'eosys': '\n ',
+            'meta_instruction':
+            'You are an AI assistant whose name is InternLM (书生·浦语).',
+            'meta_tag': '<BOS>',
+            'model_name': 'puyu',
+            'repetition_penalty': 1.02,
+            'replace_token': 'ി',
+            'session_len': 8192,
+            'stop_words': ['ി'],
+            'system': '<|System|>െ',
+            'temperature': 1,
+            'top_k': 40,
+            'top_p': 0.8,
+            'user': '<|Human|>െ'
+        }
 
-            engine = Engine(SERVER_ADDR, log_level='ERROR', **dict_info)
+        engine = Engine(SERVER_ADDR, log_level='ERROR', **dict_info)
 
-            # case1 - triton_infer有各类入参
-            session_id = get_random_session_id()
-            engine.triton_infer(session_id, '你好，你叫什么名字' + str(1),
-                                'req_id:' + str(1), 50, True, False)
-            engine.triton_infer(session_id, '你好，你叫什么名字' + str(2),
-                                'req_id:' + str(2), 50, False, False)
-            engine.triton_infer(session_id, '你好，你叫什么名字' + str(3),
-                                'req_id:' + str(3), 50, False, True)
-            engine.triton_infer(session_id, '你好，你叫什么名字' + str(4),
-                                'req_id:' + str(4), 50, True, True)
-            engine.triton_infer(session_id, '你好，你叫什么名字' + str(5),
-                                'req_id:' + str(5), 50, True, False)
+        # case1 - triton_infer有各类入参
+        session_id = get_random_session_id()
+        engine.triton_infer(session_id, '你好，你叫什么名字' + str(1),
+                            'req_id:' + str(1), 50, True, False)
+        engine.triton_infer(session_id, '你好，你叫什么名字' + str(2),
+                            'req_id:' + str(2), 50, False, False)
+        engine.triton_infer(session_id, '你好，你叫什么名字' + str(3),
+                            'req_id:' + str(3), 50, False, True)
+        engine.triton_infer(session_id, '你好，你叫什么名字' + str(4),
+                            'req_id:' + str(4), 50, True, True)
+        engine.triton_infer(session_id, '你好，你叫什么名字' + str(5),
+                            'req_id:' + str(5), 50, True, False)
 
-
-
-    
     def test_start_stop_param2(self):
-            dict_info = {
-                'assistant': '<|Assistant|>െ',
-                'eoa': 'ി\n ',
-                'eoh': '\n ',
-                'eosys': '\n ',
-                'meta_instruction':
-                'You are an AI assistant whose name is InternLM (书生·浦语).',
-                'meta_tag': '<BOS>',
-                'model_name': 'puyu',
-                'repetition_penalty': 1.02,
-                'replace_token': 'ി',
-                'session_len': 8192,
-                'stop_words': ['ി'],
-                'system': '<|System|>െ',
-                'temperature': 1,
-                'top_k': 40,
-                'top_p': 0.8,
-                'user': '<|Human|>െ'
-            }
+        dict_info = {
+            'assistant': '<|Assistant|>െ',
+            'eoa': 'ി\n ',
+            'eoh': '\n ',
+            'eosys': '\n ',
+            'meta_instruction':
+            'You are an AI assistant whose name is InternLM (书生·浦语).',
+            'meta_tag': '<BOS>',
+            'model_name': 'puyu',
+            'repetition_penalty': 1.02,
+            'replace_token': 'ി',
+            'session_len': 8192,
+            'stop_words': ['ി'],
+            'system': '<|System|>െ',
+            'temperature': 1,
+            'top_k': 40,
+            'top_p': 0.8,
+            'user': '<|Human|>െ'
+        }
 
-            engine = Engine(SERVER_ADDR, log_level='ERROR', **dict_info)
+        engine = Engine(SERVER_ADDR, log_level='ERROR', **dict_info)
 
-            session_id = get_random_session_id()
-            engine.triton_infer(session_id, '你好，你叫什么名字' + str(1),
-                                'req_id:' + str(1), 50, True, True)
-            engine.triton_resume(session_id)
-            engine.triton_end(session_id)
+        session_id = get_random_session_id()
+        engine.triton_infer(session_id, '你好，你叫什么名字' + str(1),
+                            'req_id:' + str(1), 50, True, True)
+        engine.triton_resume(session_id)
+        engine.triton_end(session_id)
 
-            engine.triton_set_session(None)
-            session_id = get_random_session_id()
-            engine.triton_infer(session_id, '你好，你叫什么名字' + str(2),
-                                'req_id:' + str(2), 50, False, True)
-            engine.triton_resume(session_id)
-            engine.triton_end(session_id)
+        engine.triton_set_session(None)
+        session_id = get_random_session_id()
+        engine.triton_infer(session_id, '你好，你叫什么名字' + str(2),
+                            'req_id:' + str(2), 50, False, True)
+        engine.triton_resume(session_id)
+        engine.triton_end(session_id)
 
-            engine.triton_set_session(None)
-            session_id = get_random_session_id()
-            engine.triton_infer(session_id, '你好，你叫什么名字' + str(3),
-                                'req_id:' + str(3), 50, False, False)
-            engine.triton_resume(session_id)
-            engine.triton_end(session_id)
+        engine.triton_set_session(None)
+        session_id = get_random_session_id()
+        engine.triton_infer(session_id, '你好，你叫什么名字' + str(3),
+                            'req_id:' + str(3), 50, False, False)
+        engine.triton_resume(session_id)
+        engine.triton_end(session_id)
 
-            engine.triton_set_session(None)
-            session_id = get_random_session_id()
-            engine.triton_infer(session_id, '你好，你叫什么名字' + str(4),
-                                'req_id:' + str(4), 50, True, True)
-            engine.triton_resume(session_id)
-            engine.triton_end(session_id)
+        engine.triton_set_session(None)
+        session_id = get_random_session_id()
+        engine.triton_infer(session_id, '你好，你叫什么名字' + str(4),
+                            'req_id:' + str(4), 50, True, True)
+        engine.triton_resume(session_id)
+        engine.triton_end(session_id)
 
-
-    
     def test_long_input(self):
-            dict_info = {
-                'assistant': '<|Assistant|>െ',
-                'eoa': 'ി\n ',
-                'eoh': '\n ',
-                'eosys': '\n ',
-                'meta_instruction':
-                'You are an AI assistant whose name is InternLM (书生·浦语).',
-                'meta_tag': '<BOS>',
-                'model_name': 'puyu',
-                'repetition_penalty': 1.02,
-                'replace_token': 'ി',
-                'session_len': 8192,
-                'stop_words': ['ി'],
-                'system': '<|System|>െ',
-                'temperature': 1,
-                'top_k': 40,
-                'top_p': 0.8,
-                'user': '<|Human|>െ'
-            }
+        dict_info = {
+            'assistant': '<|Assistant|>െ',
+            'eoa': 'ി\n ',
+            'eoh': '\n ',
+            'eosys': '\n ',
+            'meta_instruction':
+            'You are an AI assistant whose name is InternLM (书生·浦语).',
+            'meta_tag': '<BOS>',
+            'model_name': 'puyu',
+            'repetition_penalty': 1.02,
+            'replace_token': 'ി',
+            'session_len': 8192,
+            'stop_words': ['ി'],
+            'system': '<|System|>െ',
+            'temperature': 1,
+            'top_k': 40,
+            'top_p': 0.8,
+            'user': '<|Human|>െ'
+        }
 
-            engine = Engine(SERVER_ADDR, log_level='ERROR', **dict_info)
+        engine = Engine(SERVER_ADDR, log_level='ERROR', **dict_info)
 
-            session_id = get_random_session_id()
-            engine.triton_infer(session_id, '你好，你叫什么名字' * 10000,
-                                'req_id:' + str(1), 50, True, True)
-            engine.triton_resume(session_id)
-            engine.triton_end(session_id)
-
+        session_id = get_random_session_id()
+        engine.triton_infer(session_id, '你好，你叫什么名字' * 10000,
+                            'req_id:' + str(1), 50, True, True)
+        engine.triton_resume(session_id)
+        engine.triton_end(session_id)
 
     def test_history_crash_case_set(self):
-            dict_info = {
-                'assistant': '<|Assistant|>െ',
-                'eoa': 'ി\n ',
-                'eoh': '\n ',
-                'eosys': '\n ',
-                'meta_instruction':
-                'You are an AI assistant whose name is InternLM (书生·浦语).\n',
-                'meta_tag': '<BOS>',
-                'model_name': 'puyu',
-                'repetition_penalty': 1.02,
-                'replace_token': 'ി',
-                'session_len': 8192,
-                'stop_words': ['ി'],
-                'system': '<|System|>െ',
-                'temperature': 1,
-                'top_k': 40,
-                'top_p': 0.8,
-                'user': '<|Human|>െ'
-            }
+        dict_info = {
+            'assistant': '<|Assistant|>െ',
+            'eoa': 'ി\n ',
+            'eoh': '\n ',
+            'eosys': '\n ',
+            'meta_instruction':
+            'You are an AI assistant whose name is InternLM (书生·浦语).\n',
+            'meta_tag': '<BOS>',
+            'model_name': 'puyu',
+            'repetition_penalty': 1.02,
+            'replace_token': 'ി',
+            'session_len': 8192,
+            'stop_words': ['ി'],
+            'system': '<|System|>െ',
+            'temperature': 1,
+            'top_k': 40,
+            'top_p': 0.8,
+            'user': '<|Human|>െ'
+        }
 
-            engine = Engine(SERVER_ADDR, log_level='ERROR', **dict_info)
+        engine = Engine(SERVER_ADDR, log_level='ERROR', **dict_info)
 
-            # case1 - double end the ended session
-            session_id = get_random_session_id()
-            engine.triton_infer(session_id, '你好，你叫什么名字',
-                                'req_id:' + str(session_id), 1250)
-            engine.triton_end(session_id)
-            engine.triton_set_session(session_id)
-            engine.triton_end(session_id)
-            engine.triton_set_session(None)
+        # case1 - double end the ended session
+        session_id = get_random_session_id()
+        engine.triton_infer(session_id, '你好，你叫什么名字',
+                            'req_id:' + str(session_id), 1250)
+        engine.triton_end(session_id)
+        engine.triton_set_session(session_id)
+        engine.triton_end(session_id)
+        engine.triton_set_session(None)
 
-            # case2 until the session_len is exhausted
-            session_id = get_random_session_id()
-            engine.triton_infer(session_id, '你好，你叫什么名字' + str(session_id),
-                                'req_id:' + str(session_id), 200)
-            for i in range(250):
-                engine.triton_infer(session_id, '给我讲个200字的童话故事' + str(i),
-                                    'req_id:' + str(i), 200)
-
+        # case2 until the session_len is exhausted
+        session_id = get_random_session_id()
+        engine.triton_infer(session_id, '你好，你叫什么名字' + str(session_id),
+                            'req_id:' + str(session_id), 200)
+        for i in range(250):
+            engine.triton_infer(session_id, '给我讲个200字的童话故事' + str(i),
+                                'req_id:' + str(i), 200)
--- autotest/interface/triton/test_triton_server_func.py
+++ autotest/interface/triton/test_triton_server_func.py
@@ -13,7 +13,7 @@ SERVER_ADDR = 'localhost:33337'
 @pytest.mark.triton
 @pytest.mark.flaky(reruns=0)
 class TestTritonInterface:
-    
+
     def test_return_info(self):
         dict_info = {}
 
@@ -25,9 +25,6 @@ class TestTritonInterface:
         assert status == chatbot.StatusCode.TRITON_STREAM_END, status
         assert len(res) > 0, res
         assert tokens <= 51, res
-
-    
-
 
     def test_stopword(self):
         dict_info = {
@@ -57,10 +54,7 @@ class TestTritonInterface:
         assert status == chatbot.StatusCode.TRITON_STREAM_END, status
         assert ' city' not in res
         assert tokens <= 201, res
-    
 
-
-    
     def test_stopwords(self):
         dict_info = {
             'assistant': '<|Assistant|>െ',
@@ -91,9 +85,6 @@ class TestTritonInterface:
         assert ' city' not in res, res
         assert ' China' not in res, res
         assert tokens <= 201, res
-    
-
-
 
     def test_session_out_of_limit(self):
         dict_info = {
@@ -122,8 +113,6 @@ class TestTritonInterface:
                                                   'req_id:', 80, True, True)
         assert status == chatbot.StatusCode.TRITON_SESSION_OUT_OF_LIMIT, status
         assert tokens == 0, res
-    
-
 
     def test_meta_instruction(self):
         dict_info = {
@@ -131,8 +120,7 @@ class TestTritonInterface:
             'eoa': 'ി\n ',
             'eoh': '\n ',
             'eosys': '\n ',
-            'meta_instruction':
-            'You are an AI assistant whose name is puyu.',
+            'meta_instruction': 'You are an AI assistant whose name is puyu.',
             'meta_tag': '<BOS>',
             'model_name': 'puyu',
             'repetition_penalty': 1.02,
@@ -148,13 +136,12 @@ class TestTritonInterface:
 
         engine = Engine(SERVER_ADDR, log_level='ERROR', **dict_info)
         session_id = get_random_session_id()
-        status, res, tokens = engine.triton_infer(session_id, 'What is your name?',
+        status, res, tokens = engine.triton_infer(session_id,
+                                                  'What is your name?',
                                                   'req_id:', 50, True, True)
         assert status == chatbot.StatusCode.TRITON_STREAM_END, status
         assert ' puyu' or ' Puyu' in res, res
         assert tokens <= 51, res
-    
-
 
     def test_resume_session(self):
         dict_info = {
@@ -162,8 +149,7 @@ class TestTritonInterface:
             'eoa': 'ി\n ',
             'eoh': '\n ',
             'eosys': '\n ',
-            'meta_instruction':
-            'You are an AI assistant whose name is puyu.',
+            'meta_instruction': 'You are an AI assistant whose name is puyu.',
             'meta_tag': '<BOS>',
             'model_name': 'puyu',
             'repetition_penalty': 1.02,
@@ -179,18 +165,19 @@ class TestTritonInterface:
 
         engine = Engine(SERVER_ADDR, log_level='ERROR', **dict_info)
         session_id = get_random_session_id()
-        status, res, tokens = engine.triton_infer(session_id, 'What is your name?',
+        status, res, tokens = engine.triton_infer(session_id,
+                                                  'What is your name?',
                                                   'req_id:', 50, True, True)
         assert status == chatbot.StatusCode.TRITON_STREAM_END, status
         assert ' puyu' or ' Puyu' in res, res
         assert tokens <= 51, res
         engine.triton_resume(session_id)
-        status, res, tokens = engine.triton_infer(session_id, 'What is your name?',
+        status, res, tokens = engine.triton_infer(session_id,
+                                                  'What is your name?',
                                                   'req_id:', 50, True, True)
         assert status == chatbot.StatusCode.TRITON_STREAM_END, status
         assert ' puyu' or ' Puyu' in res, res
         assert tokens <= 51, res
-    
 
     def test_cancel_session(self):
         dict_info = {
@@ -198,8 +185,7 @@ class TestTritonInterface:
             'eoa': 'ി\n ',
             'eoh': '\n ',
             'eosys': '\n ',
-            'meta_instruction':
-            'You are an AI assistant whose name is puyu.',
+            'meta_instruction': 'You are an AI assistant whose name is puyu.',
             'meta_tag': '<BOS>',
             'model_name': 'puyu',
             'repetition_penalty': 1.02,
@@ -215,19 +201,13 @@ class TestTritonInterface:
 
         engine = Engine(SERVER_ADDR, log_level='ERROR', **dict_info)
         session_id = get_random_session_id()
-        status, res, tokens = engine.triton_infer(session_id, 'What is your name?',
+        status, res, tokens = engine.triton_infer(session_id,
+                                                  'What is your name?',
                                                   'req_id:', 50, True, True)
-        print("tokens",tokens)
+        print('tokens', tokens)
         assert status == chatbot.StatusCode.TRITON_STREAM_END, status
         status = engine.triton_cancel(session_id)
         assert status == chatbot.StatusCode.TRITON_SERVER_ERR, status
-
-
-
-
-        
-
-
 
 
 def get_random_session_id():
