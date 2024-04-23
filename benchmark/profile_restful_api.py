e49213ede4033512a87582347b3297806c5d6e76
rentianyue-jk <rentianyue-jk@360shuke.com>
2024-04-23 14:33:15 +0800

    [benchmark] Add api_key and model_name parmameters for restful api

--- benchmark/profile_restful_api.py
+++ benchmark/profile_restful_api.py
@@ -4,7 +4,7 @@ import random
 import time
 from queue import Queue
 from threading import Thread
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 import fire
 import numpy as np
@@ -66,21 +66,28 @@ class Engine:
                  temperature: float = 0.8,
                  top_p: float = 1.0,
                  csv: str = '',
+                 api_key: Optional[str] = None,
+                 model_name: Optional[str] = None,
                  **kwargs):
         self.tokenizer = Tokenizer(tokenzier_path)
         self.server_addr = server_addr
         self.temperature = temperature
         self.top_p = top_p
         self.csv = csv
-        client = APIClient(self.server_addr)
-        self.model_name = client.available_models[0]
+        self.api_key = api_key
+        client = APIClient(self.server_addr, api_key=self.api_key)
+        if model_name == None:
+            self.model_name = client.available_models[0]
+            print(f"using model:{self.model_name}")
+        else:
+            self.model_name = model_name
         self.pbar = None
 
     def _inference(self, req_queue: Queue, res_queue: Queue, session_id: int,
                    stream_output: bool):
 
         stats = []
-        client = APIClient(self.server_addr)
+        client = APIClient(self.server_addr, api_key=self.api_key)
 
         for prompt, input_seqlen, output_seqlen in iter(
                 req_queue.get, [None, None, None]):
@@ -207,6 +214,8 @@ class Engine:
 def main(server_addr: str,
          tokenizer_path: str,
          dataset: str,
+         api_key:Optional[str] = None,
+         model_name: Optional[str] = None,
          concurrency: int = 128,
          num_prompts: int = 5000,
          top_p: float = 1.0,
@@ -243,7 +252,9 @@ def main(server_addr: str,
                     tokenizer_path,
                     top_p=top_p,
                     temperature=temperature,
-                    csv=csv)
+                    csv=csv,
+                    api_key=api_key,
+                    model_name=model_name)
 
     requests = sample_requests(dataset, num_prompts, engine.tokenizer)
 
