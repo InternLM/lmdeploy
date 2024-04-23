43e8f503c1bc44284752a97cd596345fb31c3895
grimoire <streetyao@live.com>
2024-04-23 16:22:59 +0800

    fix triton2.1

--- lmdeploy/pytorch/kernels/utils.py
+++ lmdeploy/pytorch/kernels/utils.py
@@ -7,7 +7,7 @@ if version.parse(triton.__version__) <= version.parse('2.2.0'):
 
     def get_kernel_meta(tensor: torch.Tensor):
         """kernel meta."""
-        from triton import get_cuda_stream
+        from triton.runtime.jit import get_cuda_stream
 
         device = tensor.device
         device_idx = device.index
