0863a5b428a1fe1274a6ad239516116305af4a19
irexyc <irexyc@gmail.com>
2024-04-30 10:38:12 +0000

    fix import turbomind on windows

--- lmdeploy/turbomind/__init__.py
+++ lmdeploy/turbomind/__init__.py
@@ -7,7 +7,7 @@ def bootstrap():
 
     has_turbomind = False
     pwd = os.path.dirname(__file__)
-    if os.path.exists(os.path.join(pwd, 'lib')):
+    if os.path.exists(os.path.join(pwd, '..', 'lib')):
         has_turbomind = True
     if os.name == 'nt' and has_turbomind:
         if sys.version_info[:2] >= (3, 8):
