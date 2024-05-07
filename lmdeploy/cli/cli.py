3f5757d6f25c0db56f6116a1d8c41f8167bd5202
ispobock <ispobaoke@163.com>
2024-05-07 23:14:06 +0800

    add api

--- lmdeploy/cli/cli.py
+++ lmdeploy/cli/cli.py
@@ -110,7 +110,6 @@ class CLI(object):
         # pytorch engine args
         pt_group = parser.add_argument_group('PyTorch engine arguments')
         ArgumentHelper.adapters(pt_group)
-        ArgumentHelper.enable_prefix_caching(pt_group)
 
         # common engine args
         tp_act = ArgumentHelper.tp(pt_group)
@@ -118,6 +117,7 @@ class CLI(object):
         session_len_act = ArgumentHelper.session_len(pt_group)
         max_batch_size_act = ArgumentHelper.max_batch_size(pt_group)
         cache_max_entry_act = ArgumentHelper.cache_max_entry_count(pt_group)
+        prefix_caching_act = ArgumentHelper.enable_prefix_caching(pt_group)
 
         # turbomind args
         tb_group = parser.add_argument_group('TurboMind engine arguments')
@@ -127,6 +127,7 @@ class CLI(object):
         tb_group._group_actions.append(session_len_act)
         tb_group._group_actions.append(max_batch_size_act)
         tb_group._group_actions.append(cache_max_entry_act)
+        tb_group._group_actions.append(prefix_caching_act)
         ArgumentHelper.model_format(tb_group)
         ArgumentHelper.quant_policy(tb_group)
         ArgumentHelper.rope_scaling_factor(tb_group)
