d592dec4960a4eb4d394e7422cf05dec77591771
AllentDan <AllentDan@yeah.net>
2024-05-11 12:35:18 +0800

    missed a file

--- lmdeploy/lite/quantization/__init__.py
+++ lmdeploy/lite/quantization/__init__.py
@@ -1,9 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .activation import ActivationObserver, KVCacheObserver
-from .calibration import AWQCalibrationContext, CalibrationContext
+from .calibration import CalibrationContext, CalibrationContextV2
 from .weight import WeightQuantizer
 
 __all__ = [
     'WeightQuantizer', 'ActivationObserver', 'KVCacheObserver',
-    'CalibrationContext', 'AWQCalibrationContext'
+    'CalibrationContext', 'CalibrationContextV2'
 ]
