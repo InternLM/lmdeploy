4dd32e65c5436f8ccf3f2de980b78c4c3bc92df6
AllentDan <AllentDan@yeah.net>
2024-05-20 14:24:42 +0800

    Add UT

--- tests/test_lmdeploy/test_vl_template.py
+++ tests/test_lmdeploy/test_vl_template.py
@@ -3,6 +3,7 @@ import PIL
 from lmdeploy.model import MODELS
 from lmdeploy.vl.constants import IMAGE_TOKEN
 from lmdeploy.vl.templates import VLChatTemplateWrapper
+from lmdeploy.vl.utils import encode_image_base64, load_image
 
 
 def test_prompt_to_messages():
@@ -34,3 +35,13 @@ def test_messages2prompt():
     prompt = templtae.messages2prompt(messages)
     assert isinstance(prompt, str)
     assert prompt.count(IMAGE_TOKEN) == 1
+
+
+def test_load_image_base64():
+    im = PIL.Image.new(mode='RGB', size=(200, 200))
+    encoded = encode_image_base64(im)
+    url = f'data:image/jpeg;base64,{encoded}'
+    load_image(url)
+    import pytest
+    with pytest.raises(ValueError):
+        load_image(url[:-100])
