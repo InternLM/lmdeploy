9d48c9596747419931791d08788deef5e61230de
irexyc <irexyc@gmail.com>
2024-05-22 06:31:01 +0000

    update ut

--- tests/test_lmdeploy/test_vl_template.py
+++ tests/test_lmdeploy/test_vl_template.py
@@ -34,3 +34,37 @@ def test_messages2prompt():
     prompt = templtae.messages2prompt(messages)
     assert isinstance(prompt, str)
     assert prompt.count(IMAGE_TOKEN) == 1
+
+
+def test_llava_conv_chatml_direct():
+    model = MODELS.get('llava-chatml')()
+    templtae = VLChatTemplateWrapper(model)
+    messages = [{
+        'role':
+        'user',
+        'content': [{
+            'type': 'text',
+            'text': 'q1'
+        }, {
+            'type': 'image_url',
+            'image_url': {
+                'url': 'xxx'
+            }
+        }]
+    }]
+
+    prompt = templtae.messages2prompt(messages)
+    expected = ('<|im_start|>system\nAnswer the questions.<|im_end|>'
+                '<|im_start|>user\n<IMAGE_TOKEN>\nq1<|im_end|>'
+                '<|im_start|>assistant\n')
+    assert prompt == expected
+
+    messages.append({'role': 'assistant', 'content': 'a1'})
+    messages.append({'role': 'user', 'content': 'q2'})
+    prompt = templtae.messages2prompt(messages)
+    expected = ('<|im_start|>system\nAnswer the questions.<|im_end|>'
+                '<|im_start|>user\n<IMAGE_TOKEN>\nq1<|im_end|>'
+                '<|im_start|>assistant\na1<|im_end|>'
+                '<|im_start|>user\nq2<|im_end|>'
+                '<|im_start|>assistant\n')
+    assert prompt == expected
