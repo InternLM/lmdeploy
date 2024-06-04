e5482683641681ad2620194dabe854875f955125
irexyc <irexyc@gmail.com>
2024-06-04 12:46:56 +0000

    support quant

--- lmdeploy/vl/model/builder.py
+++ lmdeploy/vl/model/builder.py
@@ -57,6 +57,8 @@ def vl_model_with_tokenizer(model_path: str, with_llm: bool = True):
     llm = vl_model
     if hasattr(vl_model, 'language_model'):  # deepseek vl
         llm = vl_model.language_model
+    if hasattr(vl_model, 'llm'):  # MiniCPMV
+        llm = vl_model.llm
     llm.config.use_cache = False
     llm.half().eval()
     from transformers import AutoTokenizer
