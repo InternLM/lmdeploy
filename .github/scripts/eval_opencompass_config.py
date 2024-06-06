ec91ed2b40661ee41af65e1e6dc59cc7cc020b99
zhulin1 <zhulin1@pjlab.org.cn>
2024-06-06 18:29:14 +0800

    update datasets

--- .github/scripts/eval_opencompass_config.py
+++ .github/scripts/eval_opencompass_config.py
@@ -3,22 +3,45 @@ from opencompass.models import LmdeployPytorchModel, TurboMindModel
 
 with read_base():
     # choose a list of datasets
-    from .datasets.ceval.ceval_gen_5f30c7 import \
+    from .datasets.bbh.bbh_gen_5b92b0 import bbh_datasets  # noqa: F401, E501
+    from .datasets.ceval.ceval_gen_2daf24 import \
         ceval_datasets  # noqa: F401, E501
+    from .datasets.cmmlu.cmmlu_gen_c13365 import \
+        cmmlu_datasets  # noqa: F401, E501
     from .datasets.crowspairs.crowspairs_gen_381af0 import \
         crowspairs_datasets  # noqa: F401, E501
+    from .datasets.GaokaoBench.GaokaoBench_no_subjective_gen_4c31db import \
+        GaokaoBench_datasets  # noqa: F401, E501
+    from .datasets.gpqa.gpqa_gen_4baadb import \
+        gpqa_datasets  # noqa: F401, E501
     from .datasets.gsm8k.gsm8k_gen_1d7fe4 import \
         gsm8k_datasets  # noqa: F401, E501
-    from .datasets.mmlu.mmlu_gen_a484b3 import \
+    from .datasets.hellaswag.hellaswag_10shot_gen_e42710 import \
+        hellaswag_datasets  # noqa: F401, E501
+    from .datasets.humaneval.humaneval_gen_8e312c import \
+        humaneval_datasets  # noqa: F401, E501
+    from .datasets.IFEval.IFEval_gen_3321a3 import \
+        ifeval_datasets  # noqa: F401, E501
+    from .datasets.math.math_0shot_gen_393424 import \
+        math_datasets  # noqa: F401, E501
+    from .datasets.mbpp.sanitized_mbpp_gen_a0fc46 import \
+        sanitized_mbpp_datasets  # noqa: F401, E501
+    from .datasets.mmlu.mmlu_gen_4d595a import \
         mmlu_datasets  # noqa: F401, E501
+    from .datasets.nq.nq_open_1shot_gen_01cf41 import \
+        nq_datasets  # noqa: F401, E501
     from .datasets.race.race_gen_69ee4f import \
         race_datasets  # noqa: F401, E501
     from .datasets.SuperGLUE_WiC.SuperGLUE_WiC_gen_d06864 import \
         WiC_datasets  # noqa: F401, E501
     from .datasets.SuperGLUE_WSC.SuperGLUE_WSC_gen_7902a7 import \
         WSC_datasets  # noqa: F401, E501
-    from .datasets.triviaqa.triviaqa_gen_2121ce import \
+    from .datasets.TheoremQA.TheoremQA_5shot_gen_6f0af8 import \
+        TheoremQA_datasets  # noqa: F401, E501
+    from .datasets.triviaqa.triviaqa_wiki_1shot_gen_eaf81e import \
         triviaqa_datasets  # noqa: F401, E501
+    from .datasets.winogrande.winogrande_5shot_gen_b36770 import \
+        winogrande_datasets  # noqa: F401, E501
     # read hf models
     from .models.baichuan.hf_baichuan2_7b_chat import \
         models as hf_baichuan2_chat_7b  # noqa: F401, E501
