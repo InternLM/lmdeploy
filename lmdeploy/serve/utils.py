449977dd4efd359539abe41ce8ebf7315218266b
irexyc <irexyc@gmail.com>
2024-06-06 06:23:13 +0000

    reduce memory

--- lmdeploy/serve/utils.py
+++ lmdeploy/serve/utils.py
@@ -188,34 +188,66 @@ class LogitsMixin:
         for input_id in input_ids:
             assert len(input_id) > 1
 
-        logits = self.get_logits(input_ids)
-        # get target ids
-        padding_token_id = -100
-        target_ids = [(_input_ids + [padding_token_id])[1:]
-                      for _input_ids in input_ids]
-        target_ids = [
-            torch.Tensor(torch.LongTensor(_target_ids))
-            for _target_ids in target_ids
-        ]
-        target_ids = pad_sequence(target_ids,
-                                  batch_first=True,
-                                  padding_value=padding_token_id)
-        target_ids = target_ids.to(logits.device)
-        target_mask = target_ids != padding_token_id
-        target_count = torch.sum(target_mask, dim=-1)
+        max_input_len = self.backend_config.max_prefill_token_num
+        n_max_iter = np.ceil(
+            max([len(input_id)
+                 for input_id in input_ids]) / max_input_len).astype(int)
 
-        # compute cross entropy loss
-        bsz, seq_len, vocab_size = logits.shape
-        flat_logits = logits.contiguous().view(-1, vocab_size)
-        flat_target_ids = target_ids.contiguous().view(-1)
-        flat_loss_matrix = torch.nn.functional.cross_entropy(
-            flat_logits,
-            flat_target_ids,
-            reduction='none',
-            ignore_index=padding_token_id)
+        index_range_starts = []
+        index_range_ends = []
+        for input_id in input_ids:
+            index_range_start = np.array(
+                [i * max_input_len for i in range(n_max_iter)])
+            index_range_end = index_range_start + max_input_len
+            index_range_start[index_range_start >= len(input_id)] = len(
+                input_id)
+            index_range_end[index_range_end >= len(input_id)] = len(input_id)
+            index_range_starts.append(index_range_start)
+            index_range_ends.append(index_range_end)
 
-        loss_matrix = flat_loss_matrix.view(bsz, seq_len)
-        loss_sum = torch.sum(loss_matrix * target_mask, dim=1)
+        generator = self.engine.create_instance()
+        all_loss_matrix = []
+        all_target_mask = []
+        for i in range(n_max_iter):
+            steps = [start[i] for start in index_range_starts]
+            _input_ids = [
+                input_id[start[i]:end[i]] for input_id, start, end in zip(
+                    input_ids, index_range_starts, index_range_ends)
+            ]
+            _logits = generator.decode(_input_ids,
+                                       steps,
+                                       sequence_start=(i == 0),
+                                       sequence_end=(i == n_max_iter - 1))
+            _logits = _logits.cpu()
+            padding_token_id = -100
+            target_ids = [(x + [padding_token_id])[1:] for x in _input_ids]
+            target_ids = [
+                torch.Tensor(torch.LongTensor(_target_ids))
+                for _target_ids in target_ids
+            ]
+            target_ids = pad_sequence(target_ids,
+                                      batch_first=True,
+                                      padding_value=padding_token_id)
+            target_ids = target_ids.to(_logits.device)
+            target_mask = target_ids != padding_token_id
+            target_count = torch.sum(target_mask, dim=-1)
+            # compute cross entropy loss
+            bsz, seq_len, vocab_size = _logits.shape
+            flat_logits = _logits.contiguous().view(-1, vocab_size)
+            flat_target_ids = target_ids.contiguous().view(-1)
+            flat_loss_matrix = torch.nn.functional.cross_entropy(
+                flat_logits,
+                flat_target_ids,
+                reduction='none',
+                ignore_index=padding_token_id)
+
+            all_loss_matrix.append(flat_loss_matrix.view(bsz, seq_len))
+            all_target_mask.append(target_mask)
+
+        all_loss_matrix = torch.cat(all_loss_matrix, dim=1)
+        all_target_mask = torch.cat(all_target_mask, dim=1)
+        target_count = torch.sum(all_target_mask, dim=-1)
+        loss_sum = torch.sum(all_loss_matrix * all_target_mask, dim=1)
         loss_avg = loss_sum / target_count
         loss_avg = loss_avg.cpu().numpy()
         return loss_avg
