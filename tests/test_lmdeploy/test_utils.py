5bf95f0191708de69a8bbd8dbc2c9084a4fd32cb
lvhan028 <lvhan_028@163.com>
2024-06-17 17:19:59 +0800

    fix ut

--- tests/test_lmdeploy/test_utils.py
+++ tests/test_lmdeploy/test_utils.py
@@ -8,7 +8,7 @@ def test_get_and_verify_max_len():
     # with PretrainedConfig
     config = AutoConfig.from_pretrained('internlm/internlm2-chat-7b',
                                         trust_remote_code=True)
-    assert (_get_and_verify_max_len(config, None) == 32768)
+    assert (_get_and_verify_max_len(config, None) == 65536)
     assert (_get_and_verify_max_len(config, 1024) == 1024)
     assert (_get_and_verify_max_len(config, 102400) == 102400)
 
