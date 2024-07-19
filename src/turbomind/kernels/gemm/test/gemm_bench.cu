6ccf1cbff92a31e43821e94c604c345dbf3ff7f6
Li Zhang <lzhang329@gmail.com>
2024-07-19 08:15:06 +0000

    add tp

--- src/turbomind/kernels/gemm/test/gemm_bench.cu
+++ src/turbomind/kernels/gemm/test/gemm_bench.cu
@@ -14,38 +14,48 @@ std::vector<std::pair<int64_t, int64_t>> config{
     {13696 * 2, 4096}, {4096, 13696}, {4608, 4096},  {4096, 4096},  // glm4-9b
     {18944 * 2, 3584}, {3584, 18944}, {4608, 3584},  {3584, 3584},  // qwen2-7b
     {28672 * 2, 8192}, {8192, 28672}, {10240, 8192}, {8192, 8192},  // llama2-70b / llama3-70b
-    // {29568 * 2, 8192}, {8192, 29568}, {10240, 8192}, {8192, 8192},  // qwen2-72b
-    {29696 * 2, 8192}, {8192, 29696}, {10240, 8192}, {8192, 8192},  // qwen2-72b-instruct-awq
+    {29696 * 2, 8192}, {8192, 29696}, {10240, 8192}, {8192, 8192}   // qwen2-72b-instruct-awq
 };
+
+// {29568 * 2, 8192}, {8192, 29568}, {10240, 8192}, {8192, 8192},  // qwen2-72b
 
 void gemm_bench(nvbench::state& state)
 {
-    const auto index = state.get_int64("index");
+    const auto idx = state.get_int64("idx");
 
-    const auto m      = state.get_int64("bs");
-    const auto [n, k] = config[index];
+    const auto bs = state.get_int64("bs");
+    const auto tp = state.get_int64("tp");
+
+    auto [n, k] = config[idx];
 
     // const auto n      = state.get_int64("batch size");
     // const auto [m, k] = config[index];
 
+    if (idx % 4 == 0 || idx % 4 == 2) {
+        n /= tp;
+    }
+    else {
+        k /= tp;
+    }
+
     using turbomind::gemm::get_test;
 
-    get_test().Initialize(m, n, k, 128, state.get_cuda_stream());
+    get_test().Initialize(bs, n, k, 128, state.get_cuda_stream());
 
-    state.add_element_count((size_t)m * n * k * 2);  // mul + add
+    state.add_element_count((size_t)bs * n * k * 2);  // mul + add
 
     // state.collect_dram_throughput();
     // state.collect_l2_hit_rates();
 
     if constexpr (1) {
         // state.add_global_memory_reads(m * k / 2 + sizeof(half) * n * k);
-        state.add_global_memory_reads(m * k * 2 + n * k / 2);
+        state.add_global_memory_reads(bs * k * 2 + n * k / 2);
         state.exec(nvbench::exec_tag::sync, [&](nvbench::launch&) {  //
             get_test().Run();
         });
     }
     else {
-        state.add_global_memory_reads(sizeof(half) * (m * k + n * k));
+        state.add_global_memory_reads(sizeof(half) * (bs * k + n * k));
         state.exec([&](nvbench::launch&) {  //
             // g_testbed->RunCublas();
         });
@@ -53,8 +63,9 @@ void gemm_bench(nvbench::state& state)
 }
 
 NVBENCH_BENCH(gemm_bench)
-    .add_int64_axis("index", nvbench::range(0, 27))
-    .add_int64_power_of_two_axis("bs", nvbench::range(0, 10));
+    .add_int64_axis("idx", nvbench::range(0, 27))
+    .add_int64_power_of_two_axis("bs", nvbench::range(0, 10))
+    .add_int64_axis("tp", {1, 2, 4});
 
 int main(int argc, char* argv[])
 {
