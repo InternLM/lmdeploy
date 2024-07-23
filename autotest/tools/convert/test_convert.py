31156140a1a72e23da3d42382220ac287547c0a6
lvhan028 <lvhan_028@163.com>
2024-07-23 18:01:35 +0800

    fix tc

--- autotest/tools/convert/test_convert.py
+++ autotest/tools/convert/test_convert.py
@@ -39,8 +39,7 @@ def convert(config, model_case, cuda_prefix):
                               or 'awq' in model_case.lower()):
         cmd = get_command_with_extra(' '.join([
             'lmdeploy convert', origin_model_path, '--model-name', model_name,
-            '--dst-path', dst_path,
-            '--model-format awq --group-size 128 --trust-remote-code'
+            '--dst-path', dst_path, '--model-format awq --group-size 128'
         ]),
                                      config,
                                      model_case,
@@ -49,7 +48,7 @@ def convert(config, model_case, cuda_prefix):
     else:
         cmd = get_command_with_extra(' '.join([
             'lmdeploy convert', origin_model_path, '--model-name', model_name,
-            '--dst-path', dst_path, '--trust-remote-code'
+            '--dst-path', dst_path
         ]),
                                      config,
                                      model_case,
