afebd3b02d989f6029237e30092a835711986358
irexyc <irexyc@gmail.com>
2024-08-15 10:28:44 +0000

    update encode_image_base64

--- lmdeploy/vl/utils.py
+++ lmdeploy/vl/utils.py
@@ -14,31 +14,35 @@ logger = get_logger('lmdeploy')
 
 def encode_image_base64(image: Union[str, Image.Image]) -> str:
     """encode raw date to base64 format."""
-    res = ''
-    if isinstance(image, str):
-        url_or_path = image
-        if url_or_path.startswith('http'):
-            FETCH_TIMEOUT = int(os.environ.get('LMDEPLOY_FETCH_TIMEOUT', 10))
-            headers = {
-                'User-Agent':
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
-                '(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-            }
-            try:
+    buffered = BytesIO()
+    FETCH_TIMEOUT = int(os.environ.get('LMDEPLOY_FETCH_TIMEOUT', 10))
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+        '(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    try:
+        if isinstance(image, str):
+            url_or_path = image
+            if url_or_path.startswith('http'):
                 response = requests.get(url_or_path,
                                         headers=headers,
                                         timeout=FETCH_TIMEOUT)
                 response.raise_for_status()
-                res = base64.b64encode(response.content).decode('utf-8')
-            except Exception:
-                pass
-        elif os.path.exists(url_or_path):
-            with open(url_or_path, 'rb') as image_file:
-                res = base64.b64encode(image_file.read()).decode('utf-8')
-    elif isinstance(image, Image.Image):
-        buffered = BytesIO()
+                buffered.write(response.content)
+            elif os.path.exists(url_or_path):
+                with open(url_or_path, 'rb') as image_file:
+                    buffered.write(image_file.read())
+        elif isinstance(image, Image.Image):
+            image.save(buffered, format='PNG')
+    except Exception as error:
+        if isinstance(image, str) and len(image) > 100:
+            image = image[:100] + ' ...'
+        logger.error(f'{error}, image={image}')
+        # use dummy image
+        image = Image.new('RGB', (32, 32))
         image.save(buffered, format='PNG')
-        res = base64.b64encode(buffered.getvalue()).decode('utf-8')
+    res = base64.b64encode(buffered.getvalue()).decode('utf-8')
     return res
 
 
