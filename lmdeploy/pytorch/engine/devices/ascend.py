052069bb82d65c6e1fb8a3dd18f58a9325e5e44d
Wei Tao <1136862851@qq.com>
2024-08-19 22:03:44 +0800

    ascend: align attention mask to 32bytes (#7)

--- lmdeploy/pytorch/engine/devices/ascend.py
+++ lmdeploy/pytorch/engine/devices/ascend.py
@@ -17,7 +17,7 @@ class ASCENDDeviceUtils(DIPUDeviceUtils):
             single_attention_mask = torch.logical_not(
                 torch.tril(
                     torch.ones(step_context.q_seq_length[i],
-                               step_context.kv_seq_length[i],
+                               (step_context.kv_seq_length[i] + 31) & (~31),
                                dtype=torch.bool).cuda(),
                     diagonal=step_context.kv_seq_length[i] -
                     step_context.q_seq_length[i],
