96a3906fa610cf7792b7258e97e395fbb0313bff
Li Zhang <lzhang329@gmail.com>
2024-08-20 05:36:48 +0000

    add test utils

--- src/turbomind/kernels/gemm/test/gemm_test.cu
+++ src/turbomind/kernels/gemm/test/gemm_test.cu
@@ -1,3 +1,4 @@
+// Copyright (c) OpenMMLab. All rights reserved.
 
 #include "src/turbomind/kernels/attention/quantization.h"
 
@@ -5,6 +6,7 @@
 #include "src/turbomind/kernels/gemm/gemm.h"
 #include "src/turbomind/kernels/gemm/gpu_metric.h"
 #include "src/turbomind/kernels/gemm/kernel.h"
+#include "src/turbomind/kernels/gemm/test/models.h"
 #include "src/turbomind/kernels/gemm/test/quantization.h"
 #include "src/turbomind/kernels/gemm/test/test_utils.h"
 #include "src/turbomind/kernels/gemm/test/testbed.h"
@@ -13,6 +15,8 @@
 #include <limits>
 #include <thrust/universal_vector.h>
 
+#include <numeric>
+#include <random>
 #include <type_traits>
 
 using namespace turbomind;
@@ -34,6 +38,8 @@ void ComputeRefCpu(half* C, const half* A, const half* B, int m, int n, int k)
     }
 }
 
+static int g_check = 0;
+
 void Run(int batch_size, int output_dims, int input_dims, int g = 128)
 {
     auto& test = get_test();
@@ -46,23 +52,41 @@ void Run(int batch_size, int output_dims, int input_dims, int g = 128)
     std::cerr << "m" << m << "n" << n << "k" << k << "\n";
     test.Initialize(m, n, k, g, 0);
 
-    for (int i = 0; i < 10; ++i) {
-        test.Run();
+    if (g_check) {
+        test.Check();
     }
-
-    // test.CompareB();
-    test.CompareC();
-
-    return;
+    else {
+        for (int i = 0; i < 10; ++i) {
+            test.Run();
+        }
+        test.CompareC();
+    }
 }
 
 int main(int argc, char* argv[])
 {
-    Run(16384, 16384, 16384);
 
-    if (auto ec = cudaDeviceSynchronize(); ec != cudaSuccess) {
-        std::cerr << "un-clean exit: " << cudaGetErrorString(ec) << "\n";
-    }
+    g_check = 0;
+    Run(16384, 16384, 16384);
+    // Run(1, 7168, 7168);
+
+    // g_check = 1;
+    // std::vector<int> bsz(1024);
+    // {
+    //     std::iota(bsz.begin(), bsz.end(), 1);
+    //     std::random_device rd;
+    //     std::mt19937       g(rd());
+    //     std::shuffle(bsz.begin() + 1, bsz.end(), g);
+    // }
+    // for (const auto& b : bsz) {
+    //     for (const auto& [out, in] : config) {
+    //         Run(b, out, in);
+    //     }
+    // }
+
+    // if (auto ec = cudaDeviceSynchronize(); ec != cudaSuccess) {
+    //     std::cerr << "un-clean exit: " << cudaGetErrorString(ec) << "\n";
+    // }
 
     return 0;
 }
