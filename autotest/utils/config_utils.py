9748e94b7d6b70fc45009841363e0d44a1b4fb38
zhulin1 <zhulin1@pjlab.org.cn>
2024-08-21 14:19:13 +0800

    update

--- autotest/utils/config_utils.py
+++ autotest/utils/config_utils.py
@@ -32,7 +32,7 @@ def get_torch_model_list(tp_num: int = None, model_type: str = 'chat_model'):
     for key in quatization_case_config.get('w8a8'):
         if key in case_list:
             case_list.append(key + '-inner-w8a8')
-    for key in quatization_case_config.get('4bits'):
+    for key in quatization_case_config.get('awq'):
         if key in case_list:
             case_list.append(key + '-inner-4bits')
 
