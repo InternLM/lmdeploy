fc90d3aef9f939dabb1340595e6c6471f6a3db0b
irexyc <irexyc@gmail.com>
2024-08-28 09:10:20 +0000

    larger interval

--- lmdeploy/vl/engine.py
+++ lmdeploy/vl/engine.py
@@ -137,8 +137,8 @@ class ImageEncoder:
         while True:
             while record.total == 0 or (self._que.qsize() and
                                         record.total < self.max_batch_size):
-                # while self._que.qsize() == 0:
-                #     await asyncio.sleep(0)
+                while self._que.qsize() == 0:
+                    await asyncio.sleep(0.01)
                 item = await self._que.get()
                 record.enqueue(item[0], item[1], item[2])
             inputs, kwargs = record.dequeue(self.max_batch_size)
