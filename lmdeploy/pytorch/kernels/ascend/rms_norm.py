7fc24a88e0e1dac0d87354ff8c82e5ee49399d4e
yaofengchen <67218893+yao-fengchen@users.noreply.github.com>
2024-08-28 15:18:30 +0800

    fix: fix rms_norm params (#18)

--- lmdeploy/pytorch/kernels/ascend/rms_norm.py
+++ lmdeploy/pytorch/kernels/ascend/rms_norm.py
@@ -3,5 +3,13 @@ import dlinfer.ops as ext_ops
 from torch import Tensor
 
 
-def rms_norm(hidden_states: Tensor, weight: Tensor, epsilon: float = 1e-6):
-    return ext_ops.rms_norm(hidden_states, weight, epsilon)
+def rms_norm(hidden_states: Tensor,
+             weight: Tensor,
+             eps: float = 1e-6,
+             out: Tensor = None):
+    rms_norm_out = ext_ops.rms_norm(hidden_states, weight, eps)
+    if out is None:
+        out = rms_norm_out
+    else:
+        out.copy_(rms_norm_out)
+    return rms_norm_out
