32c858055c58fe528cdc1be157dd646af3be6f53
yaofengchen <67218893+yao-fengchen@users.noreply.github.com>
2024-08-30 17:45:48 +0800

    refactor ascend kernels (#2355)
    * support ascend using infer_ext
    
    * fix(ascend): make infer_ext using TND format q,k,v in paged_token_attention
    
    * support ascend using infer_ext
    
    * feat: support ascend moe_gating_topk_softmax
    
    * feat: change infer_ext ops function param order (#2)
    
    * ascend: align attention mask to 32bytes (#7)
    
    * fix attn args (#9)
    
    * fix: expand shape of attn_mask (#10)
    
    * feat: udpate infer_ext ops interface (#13)
    
    * rename infer_ext to dlinfer
    
    * format code
    
    * Support internlm 2.5 (#14)
    
    * refactor ascend pagedattention
    
    * fix ascend apply_rotary_pos_emb
    
    * fix import dlinfer (#16)
    
    * fix: fix rms_norm params (#18)
    
    * fix sync on ascend
    
    ---------
    
    Co-authored-by: chenchiyu <chenchiyu@pjlab.org.cn>
    Co-authored-by: CyCle1024 <ccy_justin@163.com>
    Co-authored-by: Wei Tao <1136862851@qq.com>
    Co-authored-by: jinminxi104 <jinminxi104@hotmail.com>
    Co-authored-by: pdx1989 <pdx1989@gmail.com>

--- lmdeploy/pytorch/check_env/__init__.py
+++ lmdeploy/pytorch/check_env/__init__.py
@@ -30,7 +30,7 @@ def check_env_deeplink(device_type: str):
     if device_type in deeplink_device_type_list:
         logger = get_logger('lmdeploy')
         try:
-            import deeplink_ext  # noqa: F401
+            import dlinfer.framework.lmdeploy_ext  # noqa: F401
         except Exception as e:
             _handle_exception(e, 'PyTorch', logger)
 
--- lmdeploy/pytorch/engine/devices/ascend.py
+++ lmdeploy/pytorch/engine/devices/ascend.py
@@ -1,10 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import torch
 
-from .dipu import DIPUDeviceUtils
+from .base_device_utils import BaseDeviceUtils
 
 
-class ASCENDDeviceUtils(DIPUDeviceUtils):
+class ASCENDDeviceUtils(BaseDeviceUtils):
 
     device = 'ascend'
 
@@ -17,7 +17,8 @@ class ASCENDDeviceUtils(DIPUDeviceUtils):
             single_attention_mask = torch.logical_not(
                 torch.tril(
                     torch.ones(step_context.q_seq_length[i],
-                               step_context.kv_seq_length[i],
+                               step_context.block_offsets.shape[1] *
+                               block_size,
                                dtype=torch.bool).cuda(),
                     diagonal=step_context.kv_seq_length[i] -
                     step_context.q_seq_length[i],
@@ -28,7 +29,7 @@ class ASCENDDeviceUtils(DIPUDeviceUtils):
             block_loc = step_context.block_offsets[i][block_idx]
             token_loc = history_length % block_size
             for _ in range(step_context.q_seq_length[i]):
-                kv_start_indices.append(block_loc * block_size + token_loc)
+                kv_start_indices.append([block_loc * block_size + token_loc])
                 if _ == step_context.q_seq_length[i] - 1:
                     break
                 token_loc = (token_loc + 1) % block_size
@@ -38,4 +39,11 @@ class ASCENDDeviceUtils(DIPUDeviceUtils):
             kv_start_indices, device=step_context.block_offsets.device)
         setattr(step_context, 'kv_start_indices', kv_start_indices)
         setattr(step_context, 'attention_mask', attention_mask)
+        setattr(step_context, 'q_start_loc', step_context.q_start_loc.cpu())
+        setattr(step_context, 'q_seq_length', step_context.q_seq_length.cpu())
+        setattr(step_context, 'kv_seq_length',
+                step_context.kv_seq_length.cpu())
+        is_unpaged_prefill = (not step_context.is_decoding) and all(
+            (step_context.q_seq_length == step_context.kv_seq_length).tolist())
+        setattr(step_context, 'is_unpaged_prefill', is_unpaged_prefill)
         return step_context
