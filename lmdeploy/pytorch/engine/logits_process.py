99ffde7f0b576609eb1b201a69950357eabf5558
grimoire <yaoqian@pjlab.org.cn>
2024-09-05 14:21:06 +0800

    inplace logits process as default

--- lmdeploy/pytorch/engine/logits_process.py
+++ lmdeploy/pytorch/engine/logits_process.py
@@ -10,79 +10,58 @@ from lmdeploy.messages import LogitsProcessor
 from ..messages import SchedulerSequence
 
 
-def _process_temperature(scores: torch.Tensor,
-                         temperature: torch.Tensor,
-                         inplace: bool = True):
+def _process_temperature_(scores: torch.Tensor, temperature: torch.Tensor):
     """process temperature."""
     temperature = temperature.to(scores.dtype)
-    if not inplace:
-        scores = scores / temperature[:, None]
-    else:
-        scores /= temperature[:, None]
+    scores.div_(temperature[:, None])
     return scores
 
 
-def _process_bad_words(scores: torch.Tensor,
-                       bad_words: torch.LongTensor,
-                       filter_value: float = -float('inf'),
-                       inplace: bool = True):
+def _process_bad_words_(scores: torch.Tensor,
+                        bad_words: torch.LongTensor,
+                        filter_value: float = -float('inf')):
     """process bad words."""
     batch_size = scores.size(0)
     batch_idx = torch.arange(batch_size, device=scores.device)
     filtered_scores = scores[batch_idx[:, None], bad_words]
     filtered_scores[bad_words >= 0] = filter_value
-
-    if not inplace:
-        scores = scores.clone()
-
     scores[batch_idx[:, None], bad_words] = filtered_scores
     return scores
 
 
-def _process_repetition_penalty(scores: torch.Tensor,
-                                input_ids: torch.LongTensor,
-                                penalty: torch.Tensor,
-                                inplace: bool = True):
+def _process_repetition_penalty_(scores: torch.Tensor,
+                                 input_ids: torch.LongTensor,
+                                 penalty: torch.Tensor):
     """process repetition penalty."""
     score = torch.gather(scores, 1, input_ids)
     penalty = penalty.to(score.dtype)
     score = torch.where(score < 0, score * penalty[:, None],
                         score / penalty[:, None])
-    if not inplace:
-        scores = scores.clone()
     scores.scatter_(1, input_ids, score)
     return scores
 
 
-def _filter_topk_sorted(scores: torch.Tensor,
-                        topk: torch.LongTensor,
-                        filter_value: float = -float('inf'),
-                        inplace: bool = True):
+def _filter_topk_sorted_(scores: torch.Tensor,
+                         topk: torch.LongTensor,
+                         filter_value: float = -float('inf')):
     """filter topk on sorted scores."""
     filter_value = -float('inf')
     num_tokens = scores.size(1)
     token_idx = torch.arange(num_tokens, device=scores.device)
     mask = token_idx[None, :] >= topk[:, None]
-    if inplace:
-        scores.masked_fill_(mask, filter_value)
-    else:
-        scores = scores.masked_fill(mask, filter_value)
+    scores.masked_fill_(mask, filter_value)
     return scores
 
 
-def _filter_topp_sorted(scores: torch.Tensor,
-                        topp: torch.Tensor,
-                        filter_value: float = -float('inf'),
-                        inplace: bool = True):
+def _filter_topp_sorted_(scores: torch.Tensor,
+                         topp: torch.Tensor,
+                         filter_value: float = -float('inf')):
     """filter topp on sorted scores."""
     softmax_scores = scores.softmax(-1)
     cum_scores = softmax_scores.cumsum(1) - softmax_scores
     mask = cum_scores > topp[:, None]
     mask[:, 0] = False  # keep at least one
-    if inplace:
-        scores.masked_fill_(mask, filter_value)
-    else:
-        scores = scores.masked_fill(mask, filter_value)
+    scores.masked_fill_(mask, filter_value)
     return scores
 
 
@@ -266,21 +245,21 @@ class FusedLogitsProcessor(LogitsWarper):
 
         repetition_penalty = sampling_inputs.repetition_penalty
         if repetition_penalty is not None:
-            scores = _process_repetition_penalty(scores, all_ids,
-                                                 repetition_penalty)
+            scores = _process_repetition_penalty_(scores, all_ids,
+                                                  repetition_penalty)
 
         temperature = sampling_inputs.temperature
         if temperature is not None:
-            scores = _process_temperature(scores, temperature)
+            scores = _process_temperature_(scores, temperature)
 
         bad_words = sampling_inputs.bad_words
         if bad_words is not None:
-            scores = _process_bad_words(scores, bad_words)
+            scores = _process_bad_words_(scores, bad_words)
 
         stop_words = sampling_inputs.stop_words
         if stop_words is not None:
             stop_words = torch.where(self.ignore_eos[:, None], stop_words, -1)
-            scores = _process_bad_words(scores, stop_words)
+            scores = _process_bad_words_(scores, stop_words)
 
         return scores
 
@@ -299,11 +278,11 @@ class FusedLogitsProcessor(LogitsWarper):
                                         top_k)
 
             if top_k is not None:
-                scores = _filter_topk_sorted(scores, top_k)
+                scores = _filter_topk_sorted_(scores, top_k)
 
             top_p = sampling_inputs.top_p
             if top_p is not None:
-                scores = _filter_topp_sorted(scores, top_p)
+                scores = _filter_topp_sorted_(scores, top_p)
 
             softmax_scores = scores.softmax(1)
 
