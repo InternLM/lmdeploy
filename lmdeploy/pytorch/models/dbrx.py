4f747c016abb6ab2ee95469919d14af96343b5b2
AllentDan <AllentDan@yeah.net>
2024-09-11 18:06:14 +0800

    update all the models

--- lmdeploy/pytorch/models/dbrx.py
+++ lmdeploy/pytorch/models/dbrx.py
@@ -92,6 +92,11 @@ class DbrxAttention(nn.Module):
             past_key_value[0],
             past_key_value[1],
             attn_metadata,
+            k_scales_zeros=None
+            if len(past_key_value) == 2 else past_key_value[2],
+            v_scales_zeros=None
+            if len(past_key_value) == 2 else past_key_value[3],
+            quant_policy=0 if len(past_key_value) == 2 else past_key_value[4],
             inplace=True,
         )
         attn_output = attn_output.reshape(*hidden_states.shape[:-1], -1)
