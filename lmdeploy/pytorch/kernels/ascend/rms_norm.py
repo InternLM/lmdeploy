e681077b6ec11911d720ae58c18e24254341c3cd
yaofengchen <67218893+yao-fengchen@users.noreply.github.com>
2024-09-11 14:41:03 +0800

    refactor pytorch engine(ascend) (#2440)
    * add support for llama ascend 910b using torch layers
    
    * feat(ascend): modify the usage of ascend kernels to fit torch.compile
    
    * refactor for llama
    
    * format code
    
    * add AscendSoftmaxTopKBuilder
    
    * feat: support ascend mixtral
    
    * refactor step_context for fusion attention
    
    * fix ascend op_backend after rebase
    
    * add comment
    
    * format code
    
    * remove unused param in rms_norm

--- lmdeploy/pytorch/kernels/ascend/rms_norm.py
+++ lmdeploy/pytorch/kernels/ascend/rms_norm.py
@@ -5,11 +5,15 @@ from torch import Tensor
 
 def rms_norm(hidden_states: Tensor,
              weight: Tensor,
-             eps: float = 1e-6,
+             epsilon: float = 1e-6,
+             residual: Tensor = None,
              out: Tensor = None):
-    rms_norm_out = ext_ops.rms_norm(hidden_states, weight, eps)
-    if out is None:
-        out = rms_norm_out
+    if residual is None:
+        rms_norm_out = ext_ops.rms_norm(hidden_states, weight, epsilon)
+        if out is None:
+            out = rms_norm_out
+        else:
+            out.copy_(rms_norm_out)
+        return out
     else:
-        out.copy_(rms_norm_out)
-    return out
+        return ext_ops.add_rms_norm(hidden_states, residual, weight, epsilon)
