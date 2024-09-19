bbda7e7016864c06aa5f83d3ddccad81685a1fe9
zhoushenglong <zhoushenglong@pjlab.org.cn>
2024-09-19 09:01:05 +0000

    add maca mixtral model support.

--- lmdeploy/pytorch/backends/maca/moe.py
+++ lmdeploy/pytorch/backends/maca/moe.py
@@ -1,8 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-from lmdeploy.pytorch.kernels.maca import moe_gating_topk_softmax
+from lmdeploy.pytorch.kernels.maca import moe_topk_softmax
+from lmdeploy.pytorch.kernels.maca import silu_and_mul
 
 from ..moe import (FusedMoEBuilder, FusedMoEImpl, SoftmaxTopKBuilder,
                    SoftmaxTopKImpl)
@@ -16,7 +18,7 @@ class MacaSoftmaxTopKImpl(SoftmaxTopKImpl):
         self.dim = dim
 
     def forward(self, x: torch.Tensor):
-        routing_weights, selected_experts = moe_gating_topk_softmax(
+        routing_weights, selected_experts = moe_topk_softmax(
             x, self.top_k)
         return routing_weights.to(torch.float32), selected_experts.to(
             torch.int64)
@@ -42,31 +44,14 @@ class MacaFusedMoEImpl(FusedMoEImpl):
                 topk_ids: torch.LongTensor, gate_up_weights: torch.Tensor,
                 down_weights: torch.Tensor):
         """forward."""
-        seq_length = hidden_states.size(0)
-        moe_output = torch.zeros_like(hidden_states)
-
-        for i in range(seq_length):
-            current_hidden_state = hidden_states[i]
-
-            # faster than remove the for loop
-            for j in range(self.top_k):
-                expert_id = topk_ids[i][j]
-                weight = topk_weights[i][j]
-
-                up_weight = gate_up_weights[expert_id]
-                up_proj = torch.matmul(up_weight, current_hidden_state)
-
-                gate_cache, up_cache = up_proj.chunk(2, -1)
-                gate_cache = torch.nn.functional.silu(gate_cache,
-                                                      inplace=True) * up_cache
-
-                down_weight = down_weights[expert_id]
-                down_proj = torch.matmul(down_weight, gate_cache)
-
-                moe_output[i] += weight * down_proj
-
-        return moe_output
-
+        N, D= hidden_states.shape
+        hidden_states = hidden_states.view(N, -1, D).repeat(1, self.top_k, 1).reshape(-1, D)
+        out = torch.zeros(N * self.top_k, down_weights.shape[1], dtype=hidden_states.dtype, device=hidden_states.device)
+        for i in range(gate_up_weights.shape[0]):
+            mask = topk_ids == i
+            if mask.sum():
+                out[mask] = silu_and_mul(hidden_states[mask] @ gate_up_weights[i].transpose(0, 1)) @ down_weights[i].transpose(0, 1)
+        return (out.view(N, -1, down_weights.shape[1]) * topk_weights.view(N, -1, 1).to(out.dtype)).sum(dim=1)
 
 class MacaFusedMoEBuilder(FusedMoEBuilder):
     """maca fused moe builder."""
--- lmdeploy/pytorch/kernels/maca/__init__.py
+++ lmdeploy/pytorch/kernels/maca/__init__.py
@@ -2,15 +2,17 @@
 from ..default import multinomial_sampling
 from .apply_rotary_pos_emb import apply_rotary_pos_emb
 from .fill_kv_cache import fill_kv_cache
-from .moe_gating_topk_softmax import moe_gating_topk_softmax
+from .moe_topk_softmax import moe_topk_softmax
 from .pagedattention import paged_attention_fwd
 from .rms_norm import rms_norm
+from .silu_and_mul import silu_and_mul
 
 __all__ = [
-    'rms_norm',
+    'multinomial_sampling',
     'apply_rotary_pos_emb',
     'fill_kv_cache',
+    'moe_topk_softmax',
     'paged_attention_fwd',
-    'moe_gating_topk_softmax',
-    'multinomial_sampling',
+    'rms_norm',
+    'silu_and_mul',
 ]
