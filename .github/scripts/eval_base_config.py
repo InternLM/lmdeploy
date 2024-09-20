a3dcfedf4a42af5b16405946dccf4ddf69d8b515
zhulin1 <zhulin1@pjlab.org.cn>
2024-09-20 15:04:52 +0800

    update

--- .github/scripts/eval_base_config.py
+++ .github/scripts/eval_base_config.py
@@ -1,24 +1,64 @@
+from copy import deepcopy
+
 from mmengine.config import read_base
-from opencompass.models import TurboMindModel
 
 with read_base():
     # choose a list of datasets
+    from opencompass.configs.datasets.bbh.bbh_gen import \
+        bbh_datasets  # noqa: F401, E501
+    from opencompass.configs.datasets.ceval.ceval_ppl import \
+        ceval_datasets  # noqa: F401, E501
+    from opencompass.configs.datasets.cmmlu.cmmlu_ppl import \
+        cmmlu_datasets  # noqa: F401, E501
+    from opencompass.configs.datasets.crowspairs.crowspairs_ppl import \
+        crowspairs_datasets  # noqa: F401, E501
+    from opencompass.configs.datasets.gpqa.gpqa_ppl_6bf57a import \
+        gpqa_datasets  # noqa: F401, E501
     from opencompass.configs.datasets.gsm8k.gsm8k_gen import \
         gsm8k_datasets  # noqa: F401, E501
+    from opencompass.configs.datasets.hellaswag.hellaswag_ppl import \
+        hellaswag_datasets  # noqa: F401, E501
+    from opencompass.configs.datasets.mmlu.mmlu_ppl import \
+        mmlu_datasets  # noqa: F401, E501
+    from opencompass.configs.datasets.race.race_ppl import \
+        race_datasets  # noqa: F401, E501
+    # read models
+    from opencompass.configs.models.baichuan.hf_baichuan_7b import \
+        models as hf_baichuan_7b  # noqa: F401, E501
+    from opencompass.configs.models.gemma.hf_gemma_7b import \
+        models as hf_gemma_7b  # noqa: F401, E501
+    from opencompass.configs.models.hf_internlm.hf_internlm2_7b import \
+        models as hf_internlm2_7b  # noqa: F401, E501
+    from opencompass.configs.models.hf_internlm.hf_internlm2_20b import \
+        models as hf_internlm2_20b  # noqa: F401, E501
+    from opencompass.configs.models.hf_internlm.hf_internlm_7b import \
+        models as hf_internlm_7b  # noqa: F401, E501
+    from opencompass.configs.models.hf_internlm.hf_internlm_20b import \
+        models as hf_internlm_20b  # noqa: F401, E501
+    from opencompass.configs.models.hf_internlm.lmdeploy_internlm2_5_7b import \
+        models as lmdeploy_internlm2_5_7b  # noqa: F401, E501
+    from opencompass.configs.models.hf_llama.hf_llama2_7b import \
+        models as hf_llama2_7b  # noqa: F401, E501
+    from opencompass.configs.models.hf_llama.hf_llama3_8b import \
+        models as hf_llama3_8b  # noqa: F401, E501
+    from opencompass.configs.models.mistral.hf_mistral_7b_v0_1 import \
+        models as hf_mistral_7b_v0_1  # noqa: F401, E501
+    from opencompass.configs.models.mistral.hf_mixtral_8x7b_v0_1 import \
+        models as hf_mixtral_8x7b_v0_1  # noqa: F401, E501
+    from opencompass.configs.models.qwen.hf_qwen1_5_7b import \
+        models as hf_qwen1_5_7b  # noqa: F401, E501
+    from opencompass.configs.models.qwen.hf_qwen2_7b import \
+        models as hf_qwen2_7b  # noqa: F401, E501
+    from opencompass.configs.models.qwen.hf_qwen_7b import \
+        models as hf_qwen_7b  # noqa: F401, E501
+    from opencompass.configs.models.qwen.lmdeploy_qwen1_5_7b import \
+        models as lmdeploy_qwen1_5_7b  # noqa: F401, E501
+    from opencompass.configs.models.qwen.lmdeploy_qwen2_7b import \
+        models as lmdeploy_qwen2_7b  # noqa: F401, E501
+    # and output the results in a chosen format
     from opencompass.configs.summarizers.medium import \
         summarizer  # noqa: F401, E501
 
-tb_internlm2_5_7b = dict(
-    type=TurboMindModel,
-    abbr='internlm2_5-7b-turbomind',
-    path='internlm/internlm2_5-7b',
-    engine_config=dict(session_len=7168, max_batch_size=16, tp=1),
-    gen_config=dict(top_k=1, temperature=1e-6, top_p=0.9, max_new_tokens=1024),
-    max_seq_len=7168,
-    max_out_len=1024,
-    batch_size=16,
-    run_cfg=dict(num_gpus=1),
-)
-
-models = [tb_internlm2_5_7b]
-datasets = [*gsm8k_datasets]
+tb_qwen1_5_7b = deepcopy(*lmdeploy_qwen1_5_7b)
+tb_qwen2_7b = deepcopy(*lmdeploy_qwen2_7b)
+tb_internlm2_5_7b = deepcopy(*lmdeploy_internlm2_5_7b)
