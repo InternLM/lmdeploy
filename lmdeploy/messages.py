81530eeba89b24a6345a9fb9259aefd4e71fac96
lvhan028 <lvhan_028@163.com>
2024-09-24 14:15:31 +0800

    miss messages

--- lmdeploy/messages.py
+++ lmdeploy/messages.py
@@ -308,6 +308,31 @@ class ResponseType(enum.Enum):
             return self.value < other.value
         return NotImplemented
 
+    def __le__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value <= other.value
+        return NotImplemented
+
+    def __eq__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value == other.value
+        return NotImplemented
+
+    def __ne__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value != other.value
+        return NotImplemented
+
+    def __gt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value > other.value
+        return NotImplemented
+
+    def __ge__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value >= other.value
+        return NotImplemented
+
 
 @dataclass
 class Response:
