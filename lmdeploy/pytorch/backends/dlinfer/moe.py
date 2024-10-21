77be205df347f77f249afd8e2a83c5e0d71ca7f0
yaofengchen <67218893+yao-fengchen@users.noreply.github.com>
2024-10-21 11:03:55 +0800

    refactor fused_moe on ascend platform (#2613)

--- lmdeploy/pytorch/backends/dlinfer/moe.py
+++ lmdeploy/pytorch/backends/dlinfer/moe.py
@@ -2,7 +2,7 @@
 
 import torch
 
-from lmdeploy.pytorch.kernels.dlinfer import moe_gating_topk_softmax
+from lmdeploy.pytorch.kernels.dlinfer import fused_moe, moe_gating_topk_softmax
 
 from ..moe import (FusedMoEBuilder, FusedMoEImpl, SoftmaxTopKBuilder,
                    SoftmaxTopKImpl)
@@ -42,30 +42,8 @@ class DlinferFusedMoEImpl(FusedMoEImpl):
                 topk_ids: torch.LongTensor, gate_up_weights: torch.Tensor,
                 down_weights: torch.Tensor):
         """forward."""
-        seq_length = hidden_states.size(0)
-        moe_output = torch.zeros_like(hidden_states)
-
-        for i in range(seq_length):
-            current_hidden_state = hidden_states[i]
-
-            # faster than remove the for loop
-            for j in range(self.top_k):
-                expert_id = topk_ids[i][j]
-                weight = topk_weights[i][j]
-
-                up_weight = gate_up_weights[expert_id]
-                up_proj = torch.matmul(up_weight, current_hidden_state)
-
-                gate_cache, up_cache = up_proj.chunk(2, -1)
-                gate_cache = torch.nn.functional.silu(gate_cache,
-                                                      inplace=True) * up_cache
-
-                down_weight = down_weights[expert_id]
-                down_proj = torch.matmul(down_weight, gate_cache)
-
-                moe_output[i] += weight * down_proj
-
-        return moe_output
+        return fused_moe(hidden_states, self.top_k, topk_ids, topk_weights,
+                         gate_up_weights, down_weights)
 
 
 class DlinferFusedMoEBuilder(FusedMoEBuilder):
