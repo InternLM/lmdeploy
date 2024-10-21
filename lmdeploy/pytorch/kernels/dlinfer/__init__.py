718ed05406ad594fd9e7cf54a46461122f5e18aa
yaofengchen <fengchenyao@foxmail.com>
2024-10-21 10:47:57 +0000

    add linear op on ascend platform

--- lmdeploy/pytorch/kernels/dlinfer/__init__.py
+++ lmdeploy/pytorch/kernels/dlinfer/__init__.py
@@ -4,6 +4,7 @@ from .apply_rotary_pos_emb import apply_rotary_pos_emb
 from .awq_kernels import awq_linear
 from .fill_kv_cache import fill_kv_cache
 from .fused_moe import fused_moe
+from .linear import linear
 from .moe_gating_topk_softmax import moe_gating_topk_softmax
 from .pagedattention import paged_attention_fwd
 from .rms_norm import rms_norm
@@ -15,6 +16,7 @@ __all__ = [
     'fill_kv_cache',
     'fused_moe',
     'paged_attention_fwd',
+    'linear',
     'moe_gating_topk_softmax',
     'multinomial_sampling',
 ]
