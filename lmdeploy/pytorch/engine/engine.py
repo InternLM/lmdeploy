ffcc6104565e03a9c650c5cd66ef50ae49ce3107
jinminxi104 <jinminxi104@hotmail.com>
2024-10-23 11:53:17 +0800

    refine pre-post-process (#2632)
    * refine pre-post-process
    
    * pytorch engine uses modelagent stream
    
    * put num_appendable_ids.cuda() back

--- lmdeploy/pytorch/engine/engine.py
+++ lmdeploy/pytorch/engine/engine.py
@@ -162,7 +162,7 @@ class Engine:
         self.scheduler_config = scheduler_config
         self.cache_config = cache_config
         self.backend_config = backend_config
-        self.stream = torch.cuda.Stream()
+        self.stream = self.model_agent.stream
 
         self.req_manager = self._bind_request_manager()
 
@@ -526,7 +526,7 @@ class Engine:
             last_idx = seq_length.cumsum(-1) - 1
             return logits[last_idx, :]
 
-        split_logits = __get_last_logits().cuda()
+        split_logits = __get_last_logits()
         logits_processor = FusedLogitsProcessor(sampling_inputs, ignore_eos,
                                                 self.tokenizer.model.model)
         logits = logits_processor(all_ids, guided_input_ids, split_logits)
