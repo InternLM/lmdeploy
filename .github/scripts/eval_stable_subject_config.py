c887adc6b4738ee25c2ee3594a30754763661041
zhulinJulia24 <145004780+zhulinJulia24@users.noreply.github.com>
2024-10-23 15:57:24 +0800

    [ci] fix restful script (#2635)
    * updaste
    
    * update
    
    * update
    
    * Update stable.yml
    
    * Update stable.yml
    
    * update
    
    * update

--- .github/scripts/eval_stable_subject_config.py
+++ .github/scripts/eval_stable_subject_config.py
@@ -20,12 +20,6 @@ with read_base():
         mtbench101_datasets  # noqa: F401, E501
     from opencompass.configs.datasets.subjective.wildbench.wildbench_pair_judge import \
         wildbench_datasets  # noqa: F401, E501
-    from opencompass.configs.datasets.TheoremQA.TheoremQA_5shot_gen_6f0af8 import \
-        TheoremQA_datasets  # noqa: F401, E501 # noqa: F401, E501
-    from opencompass.configs.datasets.triviaqa.triviaqa_wiki_1shot_gen_bc5f21 import \
-        triviaqa_datasets  # noqa: F401, E501 # noqa: F401, E501
-    from opencompass.configs.datasets.winogrande.winogrande_5shot_gen_b36770 import \
-        winogrande_datasets  # noqa: F401, E501
 
 datasets = sum((v for k, v in locals().items()
                 if k.endswith('_datasets') and 'wildbench' not in k), [])
@@ -49,7 +43,7 @@ models = [
         tokenizer_path='/nvme/qa_test_models/internlm/internlm2_5-20b-chat',
         rpm_verbose=True,
         meta_template=api_meta_template,
-        query_per_second=50,
+        query_per_second=100,
         max_out_len=1024,
         max_seq_len=4096,
         temperature=0.01,
