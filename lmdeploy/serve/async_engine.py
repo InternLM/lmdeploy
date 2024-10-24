ed7ea96aaaace87c1dc57003f87ba46de9c29513
lvhan028 <lvhan_028@163.com>
2024-10-24 21:11:46 +0800

    update

--- lmdeploy/serve/async_engine.py
+++ lmdeploy/serve/async_engine.py
@@ -513,6 +513,13 @@ class AsyncEngine(LogitsMixin):
             logger.ERROR(f"n({gen_config.n}) > 1 hasn't been supported yet. "
                          f'Fallback to 1')
             gen_config.n = 1
+        if not gen_config.do_sample and gen_config.top_k > 1:
+            logger.warn(f'GenerationConfig: {gen_config}')
+            logger.warn(
+                'Since v0.6.0, lmdeploy add `do_sample` in '
+                'GenerationConfig. It defaults to False, meaning greedy '
+                'decoding. Please set `do_sample=True` if sampling '
+                ' decoding is needed')
         prompt = messages
         self.request_logger.log_prompt(session_id=session_id, prompt=prompt)
         prompt_input = await self._get_prompt_input(prompt,
