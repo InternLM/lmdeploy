4dd83fda6aee0b9480ae64f345d68372ef519176
zhulin1 <zhulin1@pjlab.org.cn>
2024-11-21 20:47:48 +0800

    update

--- autotest/utils/pipeline_chat.py
+++ autotest/utils/pipeline_chat.py
@@ -281,7 +281,7 @@ PIC1 = 'https://raw.githubusercontent.com/open-mmlab/mmdeploy/main/tests/data/ti
 PIC2 = 'https://raw.githubusercontent.com/open-mmlab/mmdeploy/main/demo/resources/human-pose.jpg'  # noqa E501
 PIC_BEIJING = 'https://raw.githubusercontent.com/QwenLM/Qwen-VL/master/assets/mm_tutorial/Beijing_Small.jpeg'  # noqa E501
 PIC_CHONGQING = 'https://raw.githubusercontent.com/QwenLM/Qwen-VL/master/assets/mm_tutorial/Chongqing_Small.jpeg'  # noqa E501
-PIC_RACCON = 'https://raw.githubusercontent.com/OpenGVLab/InternVL/main/internvl_chat/examples/image1.jpg'  # noqa E501
+PIC_REDPANDA = 'https://raw.githubusercontent.com/OpenGVLab/InternVL/main/internvl_chat/examples/image1.jpg'  # noqa E501
 PIC_PANDA = 'https://raw.githubusercontent.com/OpenGVLab/InternVL/main/internvl_chat/examples/image2.jpg'  # noqa E501
 DESC = 'What are the similarities and differences between these two images.'  # noqa E501
 
@@ -421,23 +421,23 @@ def internvl_vl_testcase(config, pipe, file):
                     text=f'{IMAGE_TOKEN}{IMAGE_TOKEN}\n{DESC}'  # noqa E251,E501
                 ),
                 dict(type='image_url',
-                     image_url=dict(max_dynamic_patch=12, url=PIC_RACCON)),
+                     image_url=dict(max_dynamic_patch=12, url=PIC_REDPANDA)),
                 dict(type='image_url',
                      image_url=dict(max_dynamic_patch=12, url=PIC_PANDA))
             ])
     ]
     response = pipe(messages)
-    result = 'ski' in response.text.lower() or '滑雪' in response.text.lower()
+    result = 'pandas' in response.text.lower() or '熊猫' in response.text.lower()
     file.writelines('result:' + str(result) +
-                    ', reason: Multi-turn example: ski not in ' +
+                    ', reason: combined images: panda not in ' +
                     response.text + '\n')
 
     messages.append(dict(role='assistant', content=response.text))
     messages.append(dict(role='user', content=DESC))
     response = pipe(messages)
-    result = 'ski' in response.text.lower() or '滑雪' in response.text.lower()
+    result = 'pandas' in response.text.lower() or '熊猫' in response.text.lower()
     file.writelines('result:' + str(result) +
-                    ', reason: Multi-turn example: ski not in ' +
+                    ', reason: combined images second: panda not in ' +
                     response.text + '\n')
 
     # multi-image multi-round conversation, separate images
@@ -451,23 +451,23 @@ def internvl_vl_testcase(config, pipe, file):
                     +  # noqa E251,E501
                     DESC),
                 dict(type='image_url',
-                     image_url=dict(max_dynamic_patch=12, url=PIC_RACCON)),
+                     image_url=dict(max_dynamic_patch=12, url=PIC_REDPANDA)),
                 dict(type='image_url',
                      image_url=dict(max_dynamic_patch=12, url=PIC_PANDA))
             ])
     ]
     response = pipe(messages)
-    result = 'ski' in response.text.lower() or '滑雪' in response.text.lower()
+    result = 'pandas' in response.text.lower() or '熊猫' in response.text.lower()
     file.writelines('result:' + str(result) +
-                    ', reason: Multi-turn example: ski not in ' +
+                    ', reason: separate images: panda not in ' +
                     response.text + '\n')
 
     messages.append(dict(role='assistant', content=response.text))
     messages.append(dict(role='user', content=DESC))
     response = pipe(messages)
-    result = 'ski' in response.text.lower() or '滑雪' in response.text.lower()
+    result = 'pandas' in response.text.lower() or '熊猫' in response.text.lower()
     file.writelines('result:' + str(result) +
-                    ', reason: Multi-turn example: ski not in ' +
+                    ', reason: separate images second: panda not in ' +
                     response.text + '\n')
 
     # video multi-round conversation
@@ -522,9 +522,9 @@ def internvl_vl_testcase(config, pipe, file):
 
     messages = [dict(role='user', content=content)]
     response = pipe(messages)
-    result = 'ski' in response.text.lower() or '滑雪' in response.text.lower()
+    result = 'pandas' in response.text.lower() or '熊猫' in response.text.lower()
     file.writelines('result:' + str(result) +
-                    ', reason: Multi-turn example: ski not in ' +
+                    ', reason: video images: red panda not in ' +
                     response.text + '\n')
 
     messages.append(dict(role='assistant', content=response.text))
@@ -532,9 +532,10 @@ def internvl_vl_testcase(config, pipe, file):
         dict(role='user',
              content='Describe this video in detail. Don\'t repeat.'))
     response = pipe(messages)
-    result = 'ski' in response.text.lower() or '滑雪' in response.text.lower()
+    result = 'red pandas' in response.text.lower(
+    ) or '熊猫' in response.text.lower()
     file.writelines('result:' + str(result) +
-                    ', reason: Multi-turn example: ski not in ' +
+                    ', reason: video images: red panda not in ' +
                     response.text + '\n')
 
 
@@ -549,17 +550,19 @@ def llava_vl_testcase(config, pipe, file):
              ])
     ]
     response = pipe(messages)
-    result = 'ski' in response.text.lower() or '滑雪' in response.text.lower()
+    result = 'buildings' in response.text.lower(
+    ) or '楼' in response.text.lower()
     file.writelines('result:' + str(result) +
-                    ', reason: Multi-turn example: ski not in ' +
+                    ', reason: combined images: buildings not in ' +
                     response.text + '\n')
 
     messages.append(dict(role='assistant', content=response.text))
     messages.append(dict(role='user', content=DESC))
     response = pipe(messages)
-    result = 'ski' in response.text.lower() or '滑雪' in response.text.lower()
+    result = 'buildings' in response.text.lower(
+    ) or '楼' in response.text.lower()
     file.writelines('result:' + str(result) +
-                    ', reason: Multi-turn example: ski not in ' +
+                    ', reason: combined images second: buildings not in ' +
                     response.text + '\n')
 
 
@@ -570,50 +573,53 @@ def MiniCPM_vl_testcase(config, pipe, file):
              content=[
                  dict(type='text', text='Describe the two images in detail.'),
                  dict(type='image_url',
-                      image_url=dict(max_slice_nums=9, url=PIC_RACCON)),
+                      image_url=dict(max_slice_nums=9, url=PIC_REDPANDA)),
                  dict(type='image_url',
                       image_url=dict(max_slice_nums=9, url=PIC_PANDA))
              ])
     ]
     response = pipe(messages)
-    result = 'ski' in response.text.lower() or '滑雪' in response.text.lower()
+    result = 'pandas' in response.text.lower() or '熊猫' in response.text.lower()
     file.writelines('result:' + str(result) +
-                    ', reason: Multi-turn example: ski not in ' +
+                    ', reason: multiple images: panda not in ' +
                     response.text + '\n')
 
     messages.append(dict(role='assistant', content=response.text))
     messages.append(dict(role='user', content=DESC))
     response = pipe(messages)
-    result = 'ski' in response.text.lower() or '滑雪' in response.text.lower()
+    result = 'pandas' in response.text.lower() or '熊猫' in response.text.lower()
     file.writelines('result:' + str(result) +
-                    ', reason: Multi-turn example: ski not in ' +
+                    ', reason: multiple images second: panda not in ' +
                     response.text + '\n')
 
     # In-context few-shot learning
+    EXAMPLE1 = 'https://github.com/user-attachments/assets/405d9147-95f6-4f78-8879-606a0aed6707'  # noqa E251,E501
+    EXAMPLE2 = 'https://github.com/user-attachments/assets/9f2c6ed9-2aa5-4189-9c4f-0b9753024ba1'  # noqa E251,E501
+    EXAMPLE3 = 'https://github.com/user-attachments/assets/f335b507-1957-4c22-84ae-ed69ff79df38'  # noqa E251,E501
     question = 'production date'
     messages = [
         dict(role='user',
              content=[
                  dict(type='text', text=question),
-                 dict(type='image_url', image_url=dict(url='example1.jpg')),
+                 dict(type='image_url', image_url=dict(url=EXAMPLE1)),
              ]),
-        dict(role='assistant', content='2023.08.04'),
+        dict(role='assistant', content='2021.08.29'),
         dict(role='user',
              content=[
                  dict(type='text', text=question),
-                 dict(type='image_url', image_url=dict(url='example2.jpg')),
+                 dict(type='image_url', image_url=dict(url=EXAMPLE2)),
              ]),
-        dict(role='assistant', content='2007.04.24'),
+        dict(role='assistant', content='1999.05.15'),
         dict(role='user',
              content=[
                  dict(type='text', text=question),
-                 dict(type='image_url', image_url=dict(url='test.jpg')),
+                 dict(type='image_url', image_url=dict(url=EXAMPLE3)),
              ])
     ]
     response = pipe(messages)
-    result = 'ski' in response.text.lower() or '滑雪' in response.text.lower()
+    result = '2021' in response.text.lower() or '14' in response.text.lower()
     file.writelines('result:' + str(result) +
-                    ', reason: Multi-turn example: ski not in ' +
+                    ', reason: in context learning: 2021 not in ' +
                     response.text + '\n')
 
     # Chat with video
@@ -653,10 +659,11 @@ def MiniCPM_vl_testcase(config, pipe, file):
 
     messages = [dict(role='user', content=content)]
     response = pipe(messages)
-    result = 'ski' in response.text.lower() or '滑雪' in response.text.lower()
+    result = 'red panda' in response.text.lower(
+    ) or '熊猫' in response.text.lower()
     file.writelines('result:' + str(result) +
-                    ', reason: Multi-turn example: ski not in ' +
-                    response.text + '\n')
+                    ', reason: video example: panda not in ' + response.text +
+                    '\n')
 
 
 def Qwen_vl_testcase(config, pipe, file):
@@ -670,17 +677,19 @@ def Qwen_vl_testcase(config, pipe, file):
              ])
     ]
     response = pipe(messages)
-    result = 'ski' in response.text.lower() or '滑雪' in response.text.lower()
+    result = 'buildings' in response.text.lower(
+    ) or '楼' in response.text.lower()
     file.writelines('result:' + str(result) +
-                    ', reason: Multi-turn example: ski not in ' +
+                    ', reason: combined images: buildings not in ' +
                     response.text + '\n')
 
     messages.append(dict(role='assistant', content=response.text))
     messages.append(dict(role='user', content=DESC))
     response = pipe(messages)
-    result = 'ski' in response.text.lower() or '滑雪' in response.text.lower()
+    result = 'buildings' in response.text.lower(
+    ) or '楼' in response.text.lower()
     file.writelines('result:' + str(result) +
-                    ', reason: Multi-turn example: ski not in ' +
+                    ', reason: combined images second: buildings not in ' +
                     response.text + '\n')
 
     # image resolution for performance boost
@@ -702,16 +711,19 @@ def Qwen_vl_testcase(config, pipe, file):
     ]
     response = pipe(messages)
     result = 'ski' in response.text.lower() or '滑雪' in response.text.lower()
+    result = 'buildings' in response.text.lower(
+    ) or '楼' in response.text.lower()
     file.writelines('result:' + str(result) +
-                    ', reason: Multi-turn example: ski not in ' +
+                    ', reason: performance boost: buildings not in ' +
                     response.text + '\n')
 
     messages.append(dict(role='assistant', content=response.text))
     messages.append(dict(role='user', content=DESC))
     response = pipe(messages)
-    result = 'ski' in response.text.lower() or '滑雪' in response.text.lower()
+    result = 'buildings' in response.text.lower(
+    ) or '楼' in response.text.lower()
     file.writelines('result:' + str(result) +
-                    ', reason: Multi-turn example: ski not in ' +
+                    ', reason: performance boost second: buildings not in ' +
                     response.text + '\n')
 
 
