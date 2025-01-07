de2050d6b389e1b46dc3c1f69a1d1c6fcb472724
q yao <streetyao@live.com>
2025-01-07 20:55:28 +0800

    fix internvl2 qk norm (#2987)

--- lmdeploy/pytorch/nn/utils.py
+++ lmdeploy/pytorch/nn/utils.py
@@ -1,4 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import torch
+
+
 def div_up(a: int, b: int):
     """div up."""
     return (a + b - 1) // b
@@ -18,3 +21,19 @@ def get_distribute_size(feature_size: int,
     if rank < aligned_size % world_size:
         updated_aligned_size += 1
     return updated_aligned_size * align
+
+
+def chunk_aligned(weight: torch.Tensor, chunks: int, dim: int, align: int):
+    """chunk aligned."""
+    if align == 1:
+        return weight.chunk(chunks, dim=dim)
+    size = weight.size(dim)
+    assert size % align == 0
+    aligned_size = size // align
+
+    # try best to evenly split chunks
+    align_per_chunk = aligned_size // chunks
+    remain = aligned_size % chunks
+    sections = [align_per_chunk + int(c < remain) for c in range(chunks)]
+    sections = [sec * align for sec in sections]
+    return weight.split(sections, dim=dim)
