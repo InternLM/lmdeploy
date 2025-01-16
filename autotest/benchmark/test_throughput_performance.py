09363052fd8582b7267fae97cf1717cceb979637
zhulinJulia24 <145004780+zhulinJulia24@users.noreply.github.com>
2025-01-16 13:49:41 +0800

    [ci] add internlm3 into testcase (#3038)
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- autotest/benchmark/test_throughput_performance.py
+++ autotest/benchmark/test_throughput_performance.py
@@ -71,3 +71,29 @@ def test_throughput_func_tp2(config, run_id, run_config, worker_id):
                                   is_smoke=True)
 
     assert result, msg
+
+
+@pytest.mark.function
+@pytest.mark.flaky(reruns=0)
+@pytest.mark.gpu_num_1
+@pytest.mark.pr_test
+@pytest.mark.parametrize('run_config', [{
+    'model': 'meta-llama/Meta-Llama-3-1-8B-Instruct',
+    'backend': 'pytorch',
+    'tp_num': 1
+}, {
+    'model': 'meta-llama/Meta-Llama-3-1-8B-Instruct',
+    'backend': 'turbomind',
+    'quant_policy': 0,
+    'tp_num': 1
+}])
+def test_throughput_prtest_tp1(config, run_id, run_config, worker_id):
+    result, msg = throughput_test(config,
+                                  run_id,
+                                  run_config,
+                                  cuda_prefix=get_cuda_prefix_by_workerid(
+                                      worker_id, tp_num=1),
+                                  worker_id=worker_id,
+                                  is_smoke=True)
+
+    assert result, msg
