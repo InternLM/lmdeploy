0c5bc2340a490c99109eae6686b6c0d623106839
JackWeiw <51255903105@stu.ecnu.edu.cn>
2025-01-21 18:54:05 +0800

    sovle conflict

--- lmdeploy/pytorch/kernels/dlinfer/rms_norm.py
+++ lmdeploy/pytorch/kernels/dlinfer/rms_norm.py
@@ -1,24 +1,15 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from typing import Optional
-
 import dlinfer.ops as ext_ops
-from torch import Tensor, dtype
+from torch import Tensor
 
 
-def rms_norm(hidden_states: Tensor,
-             weight: Tensor,
-             epsilon: float = 1e-6,
-             residual: Tensor = None,
-             quant_dtype: Optional[dtype] = None,
-             out: Tensor = None):
+def rms_norm(hidden_states: Tensor, weight: Tensor, epsilon: float = 1e-6, residual: Tensor = None, out: Tensor = None):
     if residual is None:
-        rms_norm_out = ext_ops.rms_norm(hidden_states, weight, epsilon,
-                                        quant_dtype)
+        rms_norm_out = ext_ops.rms_norm(hidden_states, weight, epsilon)
         if out is None:
             out = rms_norm_out
         else:
             out.copy_(rms_norm_out)
         return out
     else:
-        return ext_ops.add_rms_norm(hidden_states, residual, weight, epsilon,
-                                    quant_dtype)
+        return ext_ops.add_rms_norm(hidden_states, residual, weight, epsilon)
