d77f837f3c7d6c340392b088b321f4e1676bc334
zhulinJulia24 <zhulinJulia24@163.com>
2025-01-26 15:36:52 +0800

    update

--- autotest/utils/benchmark_utils.py
+++ autotest/utils/benchmark_utils.py
@@ -116,7 +116,7 @@ def throughput_test(config, run_id, run_config, cuda_prefix: str = None, worker_
     return returncode == 0, stderr
 
 
-def restful_test(config, run_id, run_config, worker_id: str = '', is_smoke: bool = False):
+def restful_test_new(config, run_id, run_config, worker_id: str = '', is_smoke: bool = False):
     model = run_config['model']
     backend = run_config['backend']
     if backend == 'turbomind':
@@ -139,11 +139,11 @@ def restful_test(config, run_id, run_config, worker_id: str = '', is_smoke: bool
     else:
         port = DEFAULT_PORT + worker_num
 
-    http_url = f'http://localhost:{port}'
+    http_url = f'http://localhost:{port}'  # noqa: E231
     if not health_check(http_url):
         return False, 'server not start'
 
-    command = f'python3 /nvme/qa_test_models/offline_pkg/profile_restful_api.py localhost:{port} {model_path} {dataset_path} --stream-output True '  # noqa: F401, E501
+    command = f'python3 /nvme/qa_test_models/offline_pkg/profile_restful_api.py localhost:{port} {model_path} {dataset_path} --stream-output True '  # noqa: F401, E501, E231
     if is_smoke:
         command += ' --num-prompts 200'
     else:
@@ -160,6 +160,54 @@ def restful_test(config, run_id, run_config, worker_id: str = '', is_smoke: bool
             benchmark_res = subprocess.run([cmd], stdout=f, stderr=PIPE, shell=True, text=True, encoding='utf-8')
             f.writelines(benchmark_res.stderr)
         allure.attach.file(benchmark_log, attachment_type=allure.attachment_type.TEXT)
+    if benchmark_res.returncode == 0 and not os.path.isfile(csv_path):
+        return False, 'result is empty'
+    return benchmark_res.returncode == 0, benchmark_res.stderr
+
+
+def restful_test(config, run_id, run_config, worker_id: str = '', is_smoke: bool = False):
+    model = run_config['model']
+    backend = run_config['backend']
+    if backend == 'turbomind':
+        quant_policy = run_config['quant_policy']
+    log_path = config.get('log_path')
+    dataset_path = config.get('dataset_path')
+    benchmark_log = os.path.join(log_path, 'benchmark_restful_' + model.split('/')[1] + worker_id + '.log')
+    if backend == 'turbomind' and quant_policy != 0:
+        benchmark_path = '/'.join(
+            [config.get('benchmark_path'), run_id, model, f'benchmark-restful-{backend}-kvint{quant_policy}'])
+    else:
+        benchmark_path = '/'.join([config.get('benchmark_path'), run_id, model, f'benchmark-restful-{backend}'])
+
+    create_multi_level_directory(benchmark_path)
+
+    worker_num = get_workerid(worker_id)
+    if worker_num is None:
+        port = DEFAULT_PORT
+    else:
+        port = DEFAULT_PORT + worker_num
+
+    http_url = f'http://localhost:{port}'  # noqa: E231
+    if not health_check(http_url):
+        return False, 'server not start'
+
+    command = f'python3 benchmark/profile_restful_api.py --port {port} --backend lmdeploy --dataset-path {dataset_path} --request-rate-range 2,30,2 --multi '  # noqa: F401, E501, E231
+
+    if is_smoke:
+        command += ' --num-prompts 200'
+    else:
+        command += ' --num-prompts 50'
+
+    csv_path = f'{benchmark_path}/restful_batch_1th.csv'
+    cmd = ' '.join([command, '--output-file', csv_path])
+
+    with open(benchmark_log, 'w') as f:
+        f.writelines('reproduce command: ' + cmd + '\n')
+        print('reproduce command: ' + cmd)
+
+        benchmark_res = subprocess.run([cmd], stdout=f, stderr=PIPE, shell=True, text=True, encoding='utf-8')
+        f.writelines(benchmark_res.stderr)
+    allure.attach.file(benchmark_log, attachment_type=allure.attachment_type.TEXT)
     if benchmark_res.returncode == 0 and not os.path.isfile(csv_path):
         return False, 'result is empty'
     return benchmark_res.returncode == 0, benchmark_res.stderr
