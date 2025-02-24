cc2f3647fd9cb12057afccf7bc0c02d3043b98a7
Li Zhang <lzhang329@gmail.com>
2025-02-24 20:21:27 +0800

    handle non-multi-gpu build

--- src/turbomind/comm/comm.cc
+++ src/turbomind/comm/comm.cc
@@ -11,12 +11,14 @@ std::unique_ptr<GroupId> CreateCustomGroupId();
 
 std::unique_ptr<GroupId> CreateGroupId(const std::string& backend)
 {
+#if BUILD_MULTI_GPU
     if (backend == "nccl") {
         return CreateNcclGroupId();
     }
     else if (backend == "custom") {
         return CreateCustomGroupId();
     }
+#endif
     FT_CHECK_WITH_INFO(0, fmtstr("Unknown communication backend: %s", backend.c_str()));
     return nullptr;
 }
