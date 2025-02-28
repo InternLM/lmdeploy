49b0c0f03c3f95c3a27d51fff4655891acf79801
AllentDan <AllentDan@yeah.net>
2025-02-28 19:19:12 +0800

    remove useless

--- lmdeploy/serve/async_engine.py
+++ lmdeploy/serve/async_engine.py
@@ -4,10 +4,8 @@ import asyncio
 import atexit
 import concurrent.futures
 import dataclasses
-import json
 import os
 import random
-import re
 from contextlib import asynccontextmanager, closing
 from copy import deepcopy
 from functools import partial
@@ -807,49 +805,6 @@ class AsyncEngine(LogitsMixin):
                     # rewind the step to the token before the stop token
                     output_len = gen_len
                 self.id2step[session_id] += input_len + output_len
-
-    def parse_tool_response(self, text, tools, **kwargs):
-        """Parse model response containing tool information.
-
-        Args:
-            text(str): model response in string format
-            tools(List): tools from user request
-        """
-        if '<|plugin|>' in text:  # internlm2
-            text, action = text.split('<|action_start|><|plugin|>')
-            action = action.split('<|action_end|>'.strip())[0]
-            action = action[action.find('{'):]
-            action = json.loads(action)
-            name, parameters = action['name'], json.dumps(action.get('parameters', action.get('arguments', {})),
-                                                          ensure_ascii=False)
-            call_info_list = [(name, parameters)]
-        elif '<function=' in text:  # llama3.1
-            action, _ = text.split('</function>')
-            parameters = action[action.find('{'):]
-            name = action.split('<function=')[1].split('>{')[0]
-            call_info_list = [(name, parameters)]
-        elif '<tool_call>' in text and '</tool_call>' in text:  # qwen2.5
-            # get tool_call in text
-            pattern = r'<tool_call>(.*?)</tool_call>'
-            match_result_list = re.findall(pattern, text, re.DOTALL)
-            call_info_list = []
-            for match_result in match_result_list:
-                action = json.loads(match_result)
-                call_info_list.append((action['name'], json.dumps(action['arguments'], ensure_ascii=False)))
-            # get text outside of tags
-            if not text.startswith('<tool_call>'):
-                text = text[:text.find('<tool_call>')]
-            elif not text.endswith('</tool_call>'):
-                text = text[text.rfind('</tool_call>') + len('</tool_call>'):]
-            else:
-                text = ''
-
-        else:
-            raise RuntimeError(f'Unexpected model response: {text}')
-
-        call_info_list = [([tool.function.name for tool in tools].index(call_info[0]), call_info[0], call_info[1])
-                          for call_info in call_info_list]
-        return text, call_info_list
 
     def _run(self, fn=None, coro=None, loop=None):
         assert (fn or coro) and not (fn and coro)
