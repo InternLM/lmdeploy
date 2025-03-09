c64c5f52fa2818db8dff17ab7ddfc656adef6796
jinminxi104 <jinminxi104@hotmail.com>
2025-03-09 15:20:02 +0000

    lint

--- lmdeploy/pytorch/backends/dlinfer/ascend/graph_runner.py
+++ lmdeploy/pytorch/backends/dlinfer/ascend/graph_runner.py
@@ -9,8 +9,8 @@ import torch_npu
 
 from lmdeploy.pytorch.config import BackendConfig, CacheConfig, ModelConfig
 from lmdeploy.pytorch.model_inputs import StepContext
-from lmdeploy.pytorch.models.qwen2_vl import Qwen2VLForConditionalGeneration
 from lmdeploy.pytorch.models.chatglm2 import ChatGLMForConditionalGeneration
+from lmdeploy.pytorch.models.qwen2_vl import Qwen2VLForConditionalGeneration
 from lmdeploy.utils import get_logger
 
 from ...graph_runner import GraphRunner
@@ -41,7 +41,10 @@ class AscendGraphRunner(GraphRunner):
             elif isinstance(self.model, Qwen2VLForConditionalGeneration):
                 self.model.model = torch.compile(self.model.model, fullgraph=True, dynamic=True, backend='atbgraph')
             elif isinstance(self.model, ChatGLMForConditionalGeneration):
-                self.model.transformer.encoder = torch.compile(self.model.transformer.encoder, fullgraph=True, dynamic=True, backend='atbgraph')
+                self.model.transformer.encoder = torch.compile(self.model.transformer.encoder,
+                                                               fullgraph=True,
+                                                               dynamic=True,
+                                                               backend='atbgraph')
             else:
                 self.model = torch.compile(self.model, fullgraph=True, dynamic=True, backend='atbgraph')
 
