8aaa7b5dce62034628309e40e261b3219883bdc8
JimyMa <hit16s105116@gmail.com>
2025-03-12 16:17:43 +0800

    add example

--- migration/examples/initiator.py
+++ migration/examples/initiator.py
@@ -9,11 +9,11 @@ x = torch.ones([1000 * 1000 * 1000 * 8]).cuda().half()
 data_ptr = x.data_ptr()
 print(data_ptr)
 
-a.register_local_memory(data_ptr, 0, x.numel() * x.dtype.itemsize + 2)
+a.register_local_memory(data_ptr, 0, x.numel() * x.dtype.itemsize)
 
 time.sleep(30)
 begin = time.time()
-a.transport_batch("10.130.8.139:8001", data_ptr, [0], [x.numel() * x.dtype.itemsize], [0])
+a.transport_batch("10.130.8.139:8001", data_ptr, [0], 0, [x.numel() * x.dtype.itemsize], [0])
 print(time.time() - begin)
 
 print(x.sum())
