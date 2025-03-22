e37a76dd325f8cebce96222112cebd7fd3c4c02e
AllentDan <41138331+AllentDan@users.noreply.github.com>
2025-03-22 13:32:23 +0800

    Add deep gemm with tma pre allocated (#3287)
    * add deep gemm with tma pre allocated
    
    * add comment
    
    * add comment
    
    * dispatch
    
    * no use_deep_gemm arg
    
    * remove DeepGemmBlockedF8
    
    * missed op type
    
    * latest get_best_config
    
    * add a line of debug

--- lmdeploy/pytorch/backends/cuda/blockedf8_modules.py
+++ lmdeploy/pytorch/backends/cuda/blockedf8_modules.py
@@ -4,9 +4,12 @@ from typing import List, Optional
 import torch
 
 import lmdeploy.pytorch.distributed as dist
-from lmdeploy.pytorch.kernels.cuda.blocked_gemm_fp8 import blocked_gemm_fp8, quant_fp8
+from lmdeploy.pytorch.kernels.cuda.blocked_gemm_fp8 import blocked_gemm_fp8, deep_gemm_fp8, quant_fp8, quant_fp8_tma
+from lmdeploy.utils import get_logger
 
 from ..blockedf8_modules import LinearBlockedF8Builder, LinearBlockedF8Impl
+
+logger = get_logger('lmdeploy')
 
 
 def _reduce_scatter_input(out: torch.Tensor, rank: int, tp_sizes: List[int]):
@@ -60,4 +63,40 @@ class TritonLinearBlockedF8Builder(LinearBlockedF8Builder):
     @staticmethod
     def build(in_features: int, out_features: int, block_size: int = 128, bias: bool = True, dtype: torch.dtype = None):
         """build."""
-        return TritonLinearBlockedF8Impl(in_features, out_features, block_size, dtype)
+        try:
+            import deep_gemm  # noqa
+            logger.debug('build with DeepGemmLinearBlockedF8Impl')
+            return DeepGemmLinearBlockedF8Impl(in_features, out_features, block_size, dtype)
+        except:  # noqa
+            return TritonLinearBlockedF8Impl(in_features, out_features, block_size, dtype)
+
+
+class DeepGemmLinearBlockedF8Impl(LinearBlockedF8Impl):
+    """Deep gemm blocked f8 implementation."""
+
+    def __init__(self, in_features: int, out_features: int, block_size: int, out_dtype: torch.dtype = torch.float16):
+        self.in_features = in_features
+        self.out_features = out_features
+        self.out_dtype = out_dtype
+        self.block_size = block_size
+
+    def forward(self,
+                x,
+                weight: torch.Tensor,
+                scale: torch.Tensor,
+                bias: Optional[torch.Tensor] = None,
+                all_reduce: bool = False):
+        """forward."""
+        x_shape = x.shape
+        x = x.flatten(0, -2)
+        input_quant, input_scale = quant_fp8_tma(x, self.block_size, dtype=weight.dtype)
+
+        out = deep_gemm_fp8(input_quant, input_scale, weight, scale, out_dtype=x.dtype)
+        if bias is not None:
+            out += bias
+
+        if all_reduce:
+            dist.all_reduce(out)
+
+        out = out.unflatten(0, x_shape[:-1])
+        return out
