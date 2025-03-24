f1610ae03a4e91eba436becb348c81496d33453f
JimyMa <hit16s105116@gmail.com>
2025-03-24 22:22:11 +0800

    bugfix

--- src/migration/ops/gather.cu
+++ src/migration/ops/gather.cu
@@ -6,15 +6,20 @@
 
 __global__ void _gather(int8_t* src, int8_t* buffer, int64_t length, int64_t* offset)
 {
-    int64_t buf_idx = blockIdx.x * length + threadIdx.x;
-    int64_t src_idx = offset[blockIdx.x] + threadIdx.x;
+    int64_t iter = (length + blockDim.x - 1) / blockDim.x;
 
-    buffer[buf_idx] = src[src_idx];
+    for (int i = 0; i < iter; i++) {
+        if (blockDim.x * threadIdx.x + i < length) {
+            int64_t buf_idx = blockIdx.x * length + blockDim.x * threadIdx.x + i;
+            int64_t src_idx = offset[blockIdx.x] + blockDim.x * threadIdx.x + i;
+            buffer[buf_idx] = src[src_idx];
+        }
+    }
 }
 
 namespace migration {
 void gather(int64_t src_ptr, int64_t buffer_ptr, int64_t length, int64_t offset_ptr, int64_t num_offset)
 {
-    _gather<<<num_offset, length>>>((int8_t*)src_ptr, (int8_t*)buffer_ptr, length, (int64_t*)offset_ptr);
+    _gather<<<num_offset, 512>>>((int8_t*)src_ptr, (int8_t*)buffer_ptr, length, (int64_t*)offset_ptr);
 }
 }  // namespace migration
--- src/migration/ops/scatter.cu
+++ src/migration/ops/scatter.cu
@@ -6,14 +6,19 @@
 
 __global__ void _scatter(int8_t* des, int8_t* buffer, int64_t length, int64_t* offset)
 {
-    int64_t buf_idx = blockIdx.x * length + threadIdx.x;
-    int64_t src_idx = offset[blockIdx.x] + threadIdx.x;
+    int64_t iter = (length + blockDim.x - 1) / blockDim.x;
 
-    buffer[buf_idx] = des[src_idx];
+    for (int i = 0; i < iter; i++) {
+        if (blockDim.x * threadIdx.x + i < length) {
+            int64_t buf_idx = blockIdx.x * length + blockDim.x * threadIdx.x + i;
+            int64_t des_idx = offset[blockIdx.x] + blockDim.x * threadIdx.x + i;
+            buffer[buf_idx] = des[des_idx];
+        }
+    }
 }
 namespace migration {
 void scatter(int64_t des_ptr, int64_t buffer_ptr, int64_t length, int64_t offset_ptr, int64_t num_offset)
 {
-    _scatter<<<num_offset, length>>>((int8_t*)des_ptr, (int8_t*)buffer_ptr, length, (int64_t*)offset_ptr);
+    _scatter<<<num_offset, 512>>>((int8_t*)des_ptr, (int8_t*)buffer_ptr, length, (int64_t*)offset_ptr);
 }
 }  // namespace migration
