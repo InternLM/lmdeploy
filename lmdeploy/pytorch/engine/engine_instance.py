46cdf7f8a820f6bf47fe169b374d09fdcf6052c4
JimyMa <hit16s105116@gmail.com>
2025-03-27 06:30:27 +0800

    lint

--- lmdeploy/pytorch/engine/engine_instance.py
+++ lmdeploy/pytorch/engine/engine_instance.py
@@ -8,7 +8,7 @@ from ..messages import SamplingParam
 from .engine import Engine
 from .request import RequestSender, RequestType, Response, ResponseType
 
-logger = get_logger('lmdeploy')
+logger = get_logger("lmdeploy")
 
 InputMultiModalType = List[Dict[str, Any]]
 
@@ -34,16 +34,24 @@ async def async_try_add_session(req_sender: RequestSender, session_id: int):
     Args:
         session_id (int): The session id to add.
     """
-    resp = await req_sender.async_send(RequestType.ADD_SESSION, dict(session_id=session_id))
-    _check_resp(resp, [ResponseType.SUCCESS, ResponseType.SESSION_REPEAT], (f'Can not add session {session_id} '
-                                                                            f'with error: {resp.type}'))
+    resp = await req_sender.async_send(
+        RequestType.ADD_SESSION, dict(session_id=session_id)
+    )
+    _check_resp(
+        resp,
+        [ResponseType.SUCCESS, ResponseType.SESSION_REPEAT],
+        (f"Can not add session {session_id} " f"with error: {resp.type}"),
+    )
 
 
 async def async_cancel(req_sender: RequestSender, session_id: int):
     """Stop current streaming inference."""
-    resp = await req_sender.async_send(RequestType.STOP_SESSION, dict(session_id=session_id))
-    _check_resp_success(resp, (f'Failed to cancel session: {session_id}. '
-                               f'Error: {resp.type}.'))
+    resp = await req_sender.async_send(
+        RequestType.STOP_SESSION, dict(session_id=session_id)
+    )
+    _check_resp_success(
+        resp, (f"Failed to cancel session: {session_id}. " f"Error: {resp.type}.")
+    )
 
 
 def try_add_session(req_sender: RequestSender, session_id: int):
@@ -53,20 +61,26 @@ def try_add_session(req_sender: RequestSender, session_id: int):
         session_id (int): The session id to add.
     """
     resp = req_sender.send(RequestType.ADD_SESSION, dict(session_id=session_id))
-    _check_resp(resp, [ResponseType.SUCCESS, ResponseType.SESSION_REPEAT], (f'Can not add session {session_id} '
-                                                                            f'with error: {resp.type}'))
+    _check_resp(
+        resp,
+        [ResponseType.SUCCESS, ResponseType.SESSION_REPEAT],
+        (f"Can not add session {session_id} " f"with error: {resp.type}"),
+    )
 
 
 def end(req_sender: RequestSender, session_id: int):
     """End the given session."""
-    req_sender.send_async(RequestType.END_SESSION, dict(session_id=session_id, response=False))
+    req_sender.send_async(
+        RequestType.END_SESSION, dict(session_id=session_id, response=False)
+    )
 
 
 def cancel(req_sender: RequestSender, session_id: int):
     """Stop current streaming inference."""
     resp = req_sender.send(RequestType.STOP_SESSION, dict(session_id=session_id))
-    _check_resp_success(resp, (f'Failed to cancel session: {session_id}. '
-                               f'Error: {resp.type}.'))
+    _check_resp_success(
+        resp, (f"Failed to cancel session: {session_id}. " f"Error: {resp.type}.")
+    )
 
 
 class EngineInstance:
@@ -102,13 +116,15 @@ class EngineInstance:
         """
         return try_add_session(self.req_sender, session_id)
 
-    async def async_stream_infer(self,
-                                 session_id: int,
-                                 input_ids: List[int],
-                                 gen_config: GenerationConfig = None,
-                                 multimodal: InputMultiModalType = None,
-                                 adapter_name: str = None,
-                                 **kwargs):
+    async def async_stream_infer(
+        self,
+        session_id: int,
+        input_ids: List[int],
+        gen_config: GenerationConfig = None,
+        multimodal: InputMultiModalType = None,
+        adapter_name: str = None,
+        **kwargs,
+    ):
         """Send stream inference request.
 
         Args:
@@ -127,7 +143,9 @@ class EngineInstance:
             return
         gen_config = gen_config or GenerationConfig()
         sampling_param = SamplingParam.from_gen_config(gen_config=gen_config)
-        self.req_sender.send_async(RequestType.ADD_SESSION, dict(session_id=session_id, response=False))
+        self.req_sender.send_async(
+            RequestType.ADD_SESSION, dict(session_id=session_id, response=False)
+        )
         msg = dict(
             token_ids=input_ids,
             session_id=session_id,
@@ -143,31 +161,37 @@ class EngineInstance:
             resp = await self.req_sender.async_recv(resp)
 
             if resp.type == ResponseType.SUCCESS:
-                token_ids = resp.data['token_ids'].tolist()
-                yield EngineOutput(resp.type,
-                                   token_ids,
-                                   len(token_ids),
-                                   cache_block_ids=resp.data.get('cache_block_ids'))
+                token_ids = resp.data["token_ids"].tolist()
+                yield EngineOutput(
+                    resp.type,
+                    token_ids,
+                    len(token_ids),
+                    cache_block_ids=resp.data.get("cache_block_ids"),
+                )
             elif resp.type == ResponseType.FINISH:
                 resp_data = resp.data
-                token_ids = resp_data['token_ids'].tolist()
-                logits = resp_data['logits']
-                yield EngineOutput(resp.type,
-                                   token_ids,
-                                   len(token_ids),
-                                   logits=logits,
-                                   cache_block_ids=resp_data.get('cache_block_ids'))
+                token_ids = resp_data["token_ids"].tolist()
+                logits = resp_data["logits"]
+                yield EngineOutput(
+                    resp.type,
+                    token_ids,
+                    len(token_ids),
+                    logits=logits,
+                    cache_block_ids=resp_data.get("cache_block_ids"),
+                )
                 break
             else:
                 yield EngineOutput(resp.type, [], 0)
                 break
 
-    async def async_infer(self,
-                          session_id: int,
-                          input_ids: List[int] = None,
-                          multimodal: InputMultiModalType = None,
-                          gen_config: GenerationConfig = None,
-                          **kwargs):
+    async def async_infer(
+        self,
+        session_id: int,
+        input_ids: List[int] = None,
+        multimodal: InputMultiModalType = None,
+        gen_config: GenerationConfig = None,
+        **kwargs,
+    ):
         """Send inference request.
 
         Args:
@@ -180,56 +204,28 @@ class EngineInstance:
             List[int]: The streaming output tokens.
             int: The number of the output tokens.
         """
-        async for outputs in self.async_stream_infer(session_id,
-                                                     input_ids,
-                                                     multimodal=multimodal,
-                                                     gen_config=gen_config,
-                                                     **kwargs):
+        async for outputs in self.async_stream_infer(
+            session_id,
+            input_ids,
+            multimodal=multimodal,
+            gen_config=gen_config,
+            **kwargs,
+        ):
             status = outputs.status
             if status not in [ResponseType.SUCCESS, ResponseType.FINISH]:
                 return outputs
 
         return outputs
 
-    async def async_migrate(self,
-                            session_id: int,
-                            input_ids: List[int],
-                            gen_config: GenerationConfig = None,
-                            block_ids=None,
-                            multimodal: InputMultiModalType = None,
-                            adapter_name: str = None,
-                            **kwargs):
-        """Send migration request."""
-        assert block_ids, 'block ids must be set'
-        # if len(input_ids) > self.max_input_len:
-        #     return EngineOutput(ResponseType.INPUT_LENGTH_ERROR, [], 0)
-        gen_config = gen_config or GenerationConfig()
-        sampling_param = SamplingParam.from_gen_config(gen_config=gen_config)
-        self.req_sender.send_async(RequestType.ADD_SESSION, dict(session_id=session_id, response=False, migration=True))
-        msg = dict(
-            token_ids=input_ids,
-            session_id=session_id,
-            sampling_param=sampling_param,
-            adapter_name=adapter_name,
-            input_multimodals=multimodal,
-            block_ids=block_ids,
-            remote_token_ids=gen_config.remote_token_ids,
-            migration=True,
-        )
-
-        logger.info('migration msg: ', msg)
-
-        resp = self.req_sender.send_async(RequestType.ADD_MESSAGE, msg)
-        resp = await self.req_sender.async_recv(resp)
-        return
-
-    def stream_infer(self,
-                     session_id: int,
-                     input_ids: List[int],
-                     multimodal: InputMultiModalType = None,
-                     gen_config: GenerationConfig = None,
-                     adapter_name: str = None,
-                     **kwargs):
+    def stream_infer(
+        self,
+        session_id: int,
+        input_ids: List[int],
+        multimodal: InputMultiModalType = None,
+        gen_config: GenerationConfig = None,
+        adapter_name: str = None,
+        **kwargs,
+    ):
         """Send stream inference request.
 
         Args:
@@ -246,12 +242,14 @@ class EngineInstance:
 
         def __call_async():
             """call async."""
-            coro_gen = self.async_stream_infer(session_id,
-                                               input_ids,
-                                               multimodal=multimodal,
-                                               gen_config=gen_config,
-                                               adapter_name=adapter_name,
-                                               **kwargs)
+            coro_gen = self.async_stream_infer(
+                session_id,
+                input_ids,
+                multimodal=multimodal,
+                gen_config=gen_config,
+                adapter_name=adapter_name,
+                **kwargs,
+            )
             while True:
                 try:
                     yield self.req_sender.run_until_complete(coro_gen.__anext__())
@@ -260,12 +258,14 @@ class EngineInstance:
 
         yield from __call_async()
 
-    def infer(self,
-              session_id: int,
-              input_ids: List[int] = None,
-              multimodal: InputMultiModalType = None,
-              gen_config: GenerationConfig = None,
-              **kwargs):
+    def infer(
+        self,
+        session_id: int,
+        input_ids: List[int] = None,
+        multimodal: InputMultiModalType = None,
+        gen_config: GenerationConfig = None,
+        **kwargs,
+    ):
         """Send inference request.
 
         Args:
@@ -279,7 +279,14 @@ class EngineInstance:
             int: The number of the output tokens.
         """
         return self.req_sender.run_until_complete(
-            self.async_infer(session_id, input_ids, multimodal=multimodal, gen_config=gen_config, **kwargs))
+            self.async_infer(
+                session_id,
+                input_ids,
+                multimodal=multimodal,
+                gen_config=gen_config,
+                **kwargs,
+            )
+        )
 
     async def async_end(self, session_id: int):
         """End the given session."""
