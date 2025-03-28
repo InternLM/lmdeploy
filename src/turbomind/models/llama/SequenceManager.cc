533941d4ea38d20dbccc44d3907ff451854985ba
lvhan028 <lvhan_028@163.com>
2025-03-28 14:07:32 +0800

    INFO log to DEBUG log

--- src/turbomind/models/llama/SequenceManager.cc
+++ src/turbomind/models/llama/SequenceManager.cc
@@ -155,7 +155,7 @@ void SequenceManager::CachePrompt(const Sequences& sequences, int active_size)
         int valid                                    = block_manager_->Verify(block_ids, block_unique_ids);
         if (rank_ == 0) {
             TM_LOG_INFO("[SeqMgr][CachePrompt] ID %llu, cached blocks %d, valid num %d", seq.id, block_ids.size(), valid);
-            TM_LOG_INFO("[SeqMgr][CachePrompt] ID %llu, cached block_ids %s, block_unique_ids %s",
+            TM_LOG_DEBUG("[SeqMgr][CachePrompt] ID %llu, cached block_ids %s, block_unique_ids %s",
                 seq.id,
                 serialize_vector(block_ids).c_str(),
                 serialize_vector(block_unique_ids).c_str());
@@ -179,7 +179,7 @@ void SequenceManager::CacheGeneration(const Sequence& seq)
     int valid                                    = block_manager_->Verify(block_ids, block_unique_ids);
     if (rank_ == 0) {
         TM_LOG_INFO("[SeqMgr][CacheGeneration] ID %llu, cached blocks %d, valid %d", seq.id, block_ids.size(), valid);
-        TM_LOG_INFO("[SeqMgr][CacheGeneration] ID %llu, cached block_ids %s, cached block_unique_ids %s",
+        TM_LOG_DEBUG("[SeqMgr][CacheGeneration] ID %llu, cached block_ids %s, cached block_unique_ids %s",
             seq.id,
             serialize_vector(block_ids).c_str(),
             serialize_vector(block_unique_ids).c_str());
@@ -314,8 +314,6 @@ struct Transaction {
 
     const Sequences& sequences_;
     Schedule&        schedule_;
-
-    std::shared_ptr<BlockTrie> block_trie_;
 
     explicit Transaction(const Sequences& sequences, int index, int block_count, int input_count, Schedule& sched):
         sequences_(sequences), schedule_(sched), index_(index), block_count_(block_count), input_count_(input_count)
@@ -471,8 +469,7 @@ void SequenceManager::PrefixMatch(Sequences& sequences)
         block_manager_->Lock(matched_blocks);
         // block_manager_->Touch(matched_blocks);
         if (!seq.blocks.empty()) {
-            // seq.cache_len = 0 but seq.blocks is not empty. It means this seq is a reused seq
-            // the new seq's ID is reused.
+            // seq.cache_len == 0 but seq.blocks is not empty. It means the new seq reuses an older seq's ID
             seq.blocks.clear();
             seq.block_unique_ids.clear();
         }
@@ -481,7 +478,7 @@ void SequenceManager::PrefixMatch(Sequences& sequences)
         seq.cache_len = valid * block_seq_len_;
         if (rank_ == 0) {
             TM_LOG_INFO("[SeqMgr][match] ID %llu, hit blocks %d, cache_len %d", seq.id, valid, seq.cache_len);
-            TM_LOG_INFO("[SeqMgr][match] ID %llu, hit block_ids %s, unique_ids %s",
+            TM_LOG_DEBUG("[SeqMgr][match] ID %llu, hit block_ids %s, unique_ids %s",
                         seq.id,
                         serialize_vector(block_ids).c_str(),
                         serialize_vector(unique_ids).c_str());
