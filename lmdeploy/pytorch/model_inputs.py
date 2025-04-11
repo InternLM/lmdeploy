2b641b85ee91f47da03c6342fa7e8848c7a7446d
Qing Wang <49198408+wanfengcxz@users.noreply.github.com>
2025-04-11 11:32:20 +0800

    fix tensor dispatch in dynamo (#3417)

--- lmdeploy/pytorch/model_inputs.py
+++ lmdeploy/pytorch/model_inputs.py
@@ -221,6 +221,7 @@ class ModelInputs:
 
         return ret
 
+    @torch.inference_mode()
     def to_device(self, device: str, non_blocking: bool = False):
         """to device."""
         out_dict = dict()
