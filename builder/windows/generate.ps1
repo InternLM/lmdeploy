a243da0ed3ec5e552f2c58c00398fe50f34bfd94
Li Zhang <lzhang329@gmail.com>
2025-04-16 21:33:56 +0800

    fix msvc build

--- builder/windows/generate.ps1
+++ builder/windows/generate.ps1
@@ -3,6 +3,5 @@ cmake .. -A x64 -T "v143,cuda=$env:CUDA_PATH" `
     -DCMAKE_INSTALL_PREFIX=install `
     -DBUILD_PY_FFI=ON `
     -DBUILD_MULTI_GPU=OFF `
-    -DCMAKE_CUDA_FLAGS="-lineinfo" `
-    -DUSE_NVTX=ON `
+    -DUSE_NVTX=OFF `
     -DBUILD_TEST="$env:BUILD_TEST"
