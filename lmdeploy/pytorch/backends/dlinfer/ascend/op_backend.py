3d2aa5cb377dca6f7ca5eba03be90b2eb51aea35
JackWeiw <51255903105@stu.ecnu.edu.cn>
2025-05-09 17:35:32 +0800

    [ascend]set transdata dynamic shape true

--- lmdeploy/pytorch/backends/dlinfer/ascend/op_backend.py
+++ lmdeploy/pytorch/backends/dlinfer/ascend/op_backend.py
@@ -323,10 +323,7 @@ class AscendOpsBackend(DlinferOpsBackend):
             dlinfer.graph.config.enable_graph_mode = True
             if torch.distributed.is_initialized():
                 torch._inductor.config.compile_threads = 1
-            AscendOpsBackend.transdata_func = torch.compile(transdata,
-                                                            fullgraph=True,
-                                                            dynamic=True,
-                                                            backend='atbgraph')
+            AscendOpsBackend.transdata_func = torch.compile(transdata, fullgraph=True, dynamic=True, backend='atbgraph')
         return AscendOpsBackend.transdata_func
 
     @staticmethod
