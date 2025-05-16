0a422d39752517796c33c97552de64a606fd77f0
Li Zhang <lzhang329@gmail.com>
2025-05-16 20:51:31 +0800

    larger tiles

--- src/turbomind/kernels/gemm/gemm_universal_sm90_v2.h
+++ src/turbomind/kernels/gemm/gemm_universal_sm90_v2.h
@@ -215,14 +215,14 @@ struct GemmUniversalSm90_v2 {
     using Arch = Sm90;
 
     // using MMA_Atom = GMMA::MMA_64x128x16_F32BF16BF16_SS<GMMA::Major::K, GMMA::Major::K>;
-    using MMA_Atom = GMMA::MMA_64x192x32_F32E4M3E4M3_SS_TN<>;
+    using MMA_Atom = GMMA::MMA_64x128x32_F32E4M3E4M3_SS_TN<>;
     static constexpr typename cute::MMA_Traits<MMA_Atom>::Shape_MNK MMA_Shape{};
 
     static constexpr int MMA_ATOM_M = cute::get<0>(MMA_Shape);
     static constexpr int MMA_ATOM_N = cute::get<1>(MMA_Shape);
     static constexpr int MMA_ATOM_K = cute::get<2>(MMA_Shape);
 
-    static constexpr int WARPGORUPS = 2;  // kWorkGroupM * kWorkGroupN;
+    static constexpr int WARPGORUPS = 2;
 
     static constexpr int TILE_M = 128;
     static constexpr int TILE_N = MMA_ATOM_N;
@@ -232,12 +232,12 @@ struct GemmUniversalSm90_v2 {
     static constexpr int MMA_ITER_N = TILE_N / MMA_ATOM_N;
     static constexpr int MMA_ITER_K = TILE_K / MMA_ATOM_K;
 
-    static constexpr int kMulticastA = 1;
-    static constexpr int kMulticastB = 2;
+    static constexpr int kMulticastA = 2;
+    static constexpr int kMulticastB = 4;
 
     static constexpr int kClusterSize = kMulticastA * kMulticastB;
 
-    static constexpr int Stages = 4;
+    static constexpr int Stages = 5;
 
     static constexpr bool kSplitK     = false;
     static constexpr int  kChunkSizeK = TILE_K;
@@ -255,21 +255,26 @@ struct GemmUniversalSm90_v2 {
 
     using Cluster = arch::Cluster<kMulticastB, kMulticastA, kRowMajor>;
 
-    using Scheduler = TileScheduler<kRowMajor, Cluster, false, true>;
+    using Scheduler = TileScheduler<kRowMajor, Cluster, false, false>;
 
     using ProducerBar = cutlass::arch::ClusterTransactionBarrier;
     using ConsumerBar = cutlass::arch::ClusterBarrier;
 
+    static constexpr int MAX_K = 32768;
+
     static constexpr int TILE_M_U = cdiv(TILE_M, 1);
     static constexpr int CTA_K_U  = cdiv(TILE_K, 128);
 
-    static constexpr int kTmaTxBytes = sizeof(Ta) * (TILE_M * TILE_K) + sizeof(Tb) * (TILE_N * TILE_K);
+    static constexpr int kTmaTxBytes =
+        sizeof(Ta) * (TILE_M * TILE_K) + sizeof(Tb) * (TILE_N * TILE_K) + sizeof(Tu) * TILE_M_U * CTA_K_U;
 
     // ! Smem addr must be SBO aligned for TMA load/store
     struct SharedStorage {
         struct Source {
             __align__(1024) Array<Ta, Stages * TILE_M * TILE_K> A;
             __align__(1024) Array<Tb, Stages * TILE_N * TILE_K> B;
+            __align__(1024) Tu U[Stages][round_up(TILE_M_U * CTA_K_U, 32)];
+            __align__(1024) Tv V[2][WARPGORUPS][cdiv(MAX_K, 128)];
         };
         Source source;
         __align__(1024) Array<Tc, TILE_M * TILE_N> C;
@@ -336,8 +341,9 @@ struct GemmUniversalSm90_v2 {
                 const int mc_offset_m = cluster.cta_n() * (TILE_M / kMulticastA);
                 const int mc_offset_n = cluster.cta_m() * (TILE_N / kMulticastB);
 
-                auto smem_A = storage.source.A.data() + mc_offset_m * TILE_K;
-                auto smem_B = storage.source.B.data() + mc_offset_n * TILE_K;
+                auto  smem_A = storage.source.A.data() + mc_offset_m * TILE_K;
+                auto  smem_B = storage.source.B.data() + mc_offset_n * TILE_K;
+                auto& smem_U = storage.source.U;
 
                 sched.grid_init();
 
@@ -367,12 +373,16 @@ struct GemmUniversalSm90_v2 {
                     GmemIteratorSm90<kMulticastA> gmem_A{&tm_a, {offset_k, offset_m + mc_offset_m}, {TILE_K, 0}};
                     GmemIteratorSm90<kMulticastB> gmem_B{&tm_b, {offset_k, offset_n + mc_offset_n}, {TILE_K, 0}};
 
+                    // column-major
+                    GmemIteratorSm90<false> gmem_U{&tm_u, {offset_m, offset_k / 128}, {0, 1}};
+
                     for (; k_iter > 0; --k_iter) {
                         int pipe = write_state.index();
                         ConsumerBar::wait(&consumer_bar[pipe], write_state.phase());
                         ProducerBar::arrive_and_expect_tx(&producer_bar[pipe], kTmaTxBytes);
                         gmem_A.Load(&producer_bar[pipe], &smem_A[pipe * TILE_M * TILE_K], mask_A);
                         gmem_B.Load(&producer_bar[pipe], &smem_B[pipe * TILE_N * TILE_K], mask_B);
+                        gmem_U.Load(&producer_bar[pipe], &smem_U[pipe][0], 0);
                         ++write_state;
                     }
                 }
@@ -385,6 +395,7 @@ struct GemmUniversalSm90_v2 {
 
             auto& smem_A = storage.source.A;
             auto& smem_B = storage.source.B;
+            auto& smem_U = storage.source.U;
 
             auto smem_desc_A = make_smem_desc(&smem_A, 1);
             auto smem_desc_B = make_smem_desc(&smem_B, 1);
@@ -428,13 +439,17 @@ struct GemmUniversalSm90_v2 {
                     continue;
                 }
 
-                MMA_Atom::CRegisters frag_C[MMA_ITER_M][MMA_ITER_N];
+                MMA_Atom::CRegisters frag_C[MMA_ITER_N];
+                MMA_Atom::CRegisters accum_C[MMA_ITER_M][MMA_ITER_N]{};
 
                 const auto tile_offset              = sched.tile_offset();
                 const auto [iter_k_beg, iter_k_end] = sched.iter_k_range();
 
+                const auto [M, N, K, L] = sched.gemm_shape();
+
                 const int offset_m = tile_offset.x * TILE_M;
                 const int offset_n = tile_offset.y * TILE_N;
+                const int offset_k = 0;
 
                 int k_iter = iter_k_end - iter_k_beg;
 
@@ -476,27 +491,100 @@ struct GemmUniversalSm90_v2 {
                     }
                 }
 
-                auto gmma = [&](bool clear = false) {
-                    smem_iter_A.Reset(pipe_acquire.index());
-                    smem_iter_B.Reset(pipe_acquire.index());
+                auto Copy = [k = cdiv(K, 128)](Tv* dst, const Tv* src) {
+                    for (int i = threadIdx.x % WARPGROUP_SIZE; i < k; i += WARPGROUP_SIZE) {
+                        dst[i] = __ldg(&src[i]);
+                    }
+                };
+                auto gmem_V = (const Tv*)V_ + (offset_n / 128) * ldV + (offset_k / 128);
+                Copy(storage.source.V[0][warpgroup_id], gmem_V);
+
+                uint32_t pred_V{};
+                int      iter_V{};
+
+                constexpr int OUTER_N = std::gcd(MMA_ATOM_N, 128);
+                if constexpr (OUTER_N != 128) {
+
+                    static_assert(MMA_ATOM_N <= 128 + OUTER_N, "MMA inst is crossing more than 2 scale blocks");
+
+                    constexpr uint32_t mask = (1UL << (TILE_M / OUTER_N)) - 1;
+
+                    int phase = 128 - offset_n % 128;
+                    pred_V    = (mask << (phase / OUTER_N)) & mask;
+
+                    if (pred_V && offset_n / 128 + 1 < cdiv(N, 128)) {
+                        Copy(storage.source.V[1][warpgroup_id], gmem_V + ldV);
+                    }
+
+                    // if constexpr (kWorkGroupN > 1) {
+                    //     constexpr int tiles = MMA_ATOM_N / OUTER_N;
+                    //     pred_V              = (pred_V >> (warp_group_id_n * tiles)) & ((1 << tiles) - 1);
+                    // }
+                }
+
+                float scale_V[2];
+                auto  Load_V = [&] {
+                    scale_V[0] = storage.source.V[0][warpgroup_id][iter_V];
+                    if (pred_V) {
+                        scale_V[1] = storage.source.V[1][warpgroup_id][iter_V];
+                    }
+                    ++iter_V;
+                };
+
+                float     scale_U[MMA_ITER_M][2];
+                const int offset_U = warp_id % 4 * 16 + lane_id / 4;
+                auto      Load_U   = [&] {
+                    for (int m = 0; m < MMA_ITER_M; ++m) {
+                        scale_U[m][0] = smem_U[pipe_acquire.index()][offset_U + m * MMA_ATOM_M];
+                        scale_U[m][1] = smem_U[pipe_acquire.index()][offset_U + m * MMA_ATOM_M + 8];
+                    }
+                };
+
+                auto scale_accum = [&](int m) {  // cta_n = mma_iter_n * wg_n * mma_atom_n
+                    float scales[2][2];
+                    scales[0][0] = scale_U[m][0] * scale_V[0];
+                    scales[1][0] = scale_U[m][1] * scale_V[0];
+                    scales[0][1] = scale_U[m][0] * scale_V[1];
+                    scales[1][1] = scale_U[m][1] * scale_V[1];
+                    PRAGMA_UNROLL
+                    for (int n = 0; n < MMA_ITER_N; ++n) {
+                        PRAGMA_UNROLL
+                        for (int c0 = 0; c0 < MMA_ATOM_N; c0 += OUTER_N) {
+                            bool pred = (pred_V & (1U << (c0 / OUTER_N)));
+                            PRAGMA_UNROLL
+                            for (int cc = 0; cc < OUTER_N; cc += 8) {
+                                int c = c0 + cc;
+                                // clang-format off
+                                accum_C[m][n][c / 2 + 0] += (pred ? scales[0][1] : scales[0][0]) * frag_C[n][c / 2 + 0];
+                                accum_C[m][n][c / 2 + 1] += (pred ? scales[0][1] : scales[0][0]) * frag_C[n][c / 2 + 1];
+                                accum_C[m][n][c / 2 + 2] += (pred ? scales[1][1] : scales[1][0]) * frag_C[n][c / 2 + 2];
+                                accum_C[m][n][c / 2 + 3] += (pred ? scales[1][1] : scales[1][0]) * frag_C[n][c / 2 + 3];
+                                // clang-format on
+                            }
+                        }
+                    }
+
+                };
+
+                auto gmma = [&](int m) {
+                    if (m == 0) {
+                        smem_iter_A.Reset(pipe_acquire.index());
+                        smem_iter_B.Reset(pipe_acquire.index());
+                    }
                     PRAGMA_UNROLL
                     for (int k = 0; k < MMA_ITER_K; ++k) {
                         PRAGMA_UNROLL
-                        for (int m = 0; m < MMA_ITER_M; ++m) {
-                            PRAGMA_UNROLL
-                            for (int n = 0; n < MMA_ITER_N; ++n) {
-                                wgmma<MMA_Atom>(smem_iter_A, smem_iter_B, frag_C[m][n], k == 0 && clear);
-                                smem_iter_B += kStepNB;
-                            }
-                            smem_iter_B -= MMA_ITER_N * kStepNB;
-                            smem_iter_A += kStepMA;
+                        for (int n = 0; n < MMA_ITER_N; ++n) {
+                            wgmma<MMA_Atom>(smem_iter_A, smem_iter_B, frag_C[n], k == 0);
+                            smem_iter_B += kStepNB;
                         }
-                        smem_iter_A -= MMA_ITER_M * kStepMA;
+                        smem_iter_B -= MMA_ITER_N * kStepNB;
                         smem_iter_A += kStepKA;
                         smem_iter_B += kStepKB;
                     }
                     smem_iter_A -= MMA_ITER_K * kStepKA;
                     smem_iter_B -= MMA_ITER_K * kStepKB;
+                    smem_iter_A += kStepMA;
                     cute::warpgroup_commit_batch();
                 };
 
@@ -506,20 +594,34 @@ struct GemmUniversalSm90_v2 {
 
                 pipe_release = pipe_acquire.advance(storage.pipe_count[warpgroup_id ^ 1]);
 
-                constexpr int MMA_PIPE = 1;
-
-                for (int s = 0; s < MMA_PIPE; ++s) {
-                    ProducerBar::wait(&producer_bar[pipe_acquire.index()], pipe_acquire.phase());
-                    cute::warpgroup_arrive();
-                    gmma(s == 0);
-                    ++pipe_acquire;
-                    --k_iter;
-                }
+                Load_V();
+                ProducerBar::wait(&producer_bar[pipe_acquire.index()], pipe_acquire.phase());
+                Load_U();
+                cute::warpgroup_arrive();
+                gmma(0);
+                cute::warpgroup_wait<0>();
+                scale_accum(0);
+                cute::warpgroup_arrive();
+                gmma(1);
+                cute::warpgroup_wait<0>();
+                scale_accum(1);
+                consumer_arrive();
+                ++pipe_acquire;
+                ++pipe_release;
+                --k_iter;
 
                 for (; k_iter > 0; --k_iter) {
+                    Load_V();
                     ProducerBar::wait(&producer_bar[pipe_acquire.index()], pipe_acquire.phase());
-                    gmma();
-                    cute::warpgroup_wait<MMA_PIPE>();
+                    Load_U();
+                    cute::warpgroup_arrive();
+                    gmma(0);
+                    cute::warpgroup_wait<0>();
+                    scale_accum(0);
+                    cute::warpgroup_arrive();
+                    gmma(1);
+                    cute::warpgroup_wait<0>();
+                    scale_accum(1);
                     consumer_arrive();
                     ++pipe_acquire;
                     ++pipe_release;
@@ -530,13 +632,6 @@ struct GemmUniversalSm90_v2 {
                 }
 
                 math_barrier_sync(1);
-
-                cute::warpgroup_wait<0>();
-
-                for (int s = 0; s < MMA_PIPE; ++s) {
-                    consumer_arrive();
-                    ++pipe_release;
-                }
 
                 const int wg_thread_id = threadIdx.x % WARPGROUP_SIZE;
 
@@ -562,7 +657,7 @@ struct GemmUniversalSm90_v2 {
 
                         PRAGMA_UNROLL
                         for (int i = 0; i < MMA_ATOM_N; i += 16) {
-                            __align__(16) Array<Tc, 8> tvec = cast<Tc>(*(Array<float, 8>*)&frag_C[m][n][i / 2]);
+                            __align__(16) Array<Tc, 8> tvec = cast<Tc>(*(Array<float, 8>*)&accum_C[m][n][i / 2]);
 
                             int mm = m0 + warp_id % 4 * 16 + (lane_id & 8);
                             int nn = n0 + i / N * N;
