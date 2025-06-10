dbc901357d844a80e1ca1b2c18bfc53480890c02
Li Zhang <lzhang329@gmail.com>
2025-06-10 14:07:59 +0800

    guard CUDA version

--- src/turbomind/kernels/gemm/kernel/sm90_q64n32.cu
+++ src/turbomind/kernels/gemm/kernel/sm90_q64n32.cu
@@ -1,5 +1,7 @@
 #include <cuda.h>
 
+#if CUDA_VERSION >= 12000
+
 #include "src/turbomind/kernels/gemm/arch.h"
 #include "src/turbomind/kernels/gemm/gemm_universal_sm90_v3.h"
 #include "src/turbomind/kernels/gemm/gemm_universal_sm90_v5.h"
@@ -11,7 +13,6 @@ namespace turbomind::gemm {
 
 void Registry::sm90_s64n32_dynamic()
 {
-#if CUDA_VERSION >= 12000
     Add(std::make_unique<KernelImplSm90<GemmUniversalSm90_v5<kRowMajor, 1, 1, false>>>());
     Add(std::make_unique<KernelImplSm90<GemmUniversalSm90_v5<kRowMajor, 2, 1, false>>>());
     Add(std::make_unique<KernelImplSm90<GemmUniversalSm90_v5<kRowMajor, 1, 2, false>>>());
@@ -27,7 +28,12 @@ void Registry::sm90_s64n32_dynamic()
     Add(std::make_unique<KernelImplSm90<GemmUniversalSm90_v3<kColMajor, 1, 1, true>>>());
     Add(std::make_unique<KernelImplSm90<GemmUniversalSm90_v3<kColMajor, 2, 1, true>>>());
     Add(std::make_unique<KernelImplSm90<GemmUniversalSm90_v3<kColMajor, 1, 2, true>>>());
-#endif
 }
+
+#else
+
+void Registry::sm90_s64n32_dynamic() {}
+
+#endif
 
 }  // namespace turbomind::gemm
