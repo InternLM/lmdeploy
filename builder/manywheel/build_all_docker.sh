274810241ebed69e16f06894827b990c1241f4be
Lyu Han <lvhan_028@163.com>
2025-06-12 17:13:40 +0800

    check in lmdeploy-builder on cuda 12.4 and 12.8 platform (#3630)
    * check in lmdeploy-builder on cuda 12.4 platform
    
    * remove openmpi
    
    * build cuda12.8 image
    
    * update pypi

--- builder/manywheel/build_all_docker.sh
+++ builder/manywheel/build_all_docker.sh
@@ -4,6 +4,6 @@ set -eou pipefail
 
 TOPDIR=$(git rev-parse --show-toplevel)/builder
 
-for cuda_version in 11.8; do
+for cuda_version in 12.4; do
     MANY_LINUX_VERSION=2014 GPU_ARCH_VERSION="${cuda_version}" "${TOPDIR}/manywheel/build_docker.sh"
 done
