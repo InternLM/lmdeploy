832e72349a4fd4bf70d51a978c8428ff4140f0e7
zhulinJulia24 <zhulinJulia24@163.com>
2025-06-16 15:42:57 +0800

    update

--- .github/scripts/action_tools.py
+++ .github/scripts/action_tools.py
@@ -224,10 +224,14 @@ def generate_benchmark_report(report_path: str):
                     for f in csv_files:
                         df = pd.read_csv(f)
                         merged_df = pd.concat([merged_df, df], ignore_index=True)
+                    if 'throughput' in backend_subfolder:
+                        merged_df = merged_df.sort_values(by=merged_df.columns[1])
 
-                    merged_df = merged_df.sort_values(by=merged_df.columns[0])
+                        grouped_df = merged_df.groupby(merged_df.columns[1])
+                    else:
+                        merged_df = merged_df.sort_values(by=merged_df.columns[0])
 
-                    grouped_df = merged_df.groupby(merged_df.columns[0])
+                        grouped_df = merged_df.groupby(merged_df.columns[0])
                     if 'generation' not in backend_subfolder:
                         average_values = grouped_df.pipe((lambda group: {
                             'mean': group.mean(numeric_only=True).round(decimals=3)
