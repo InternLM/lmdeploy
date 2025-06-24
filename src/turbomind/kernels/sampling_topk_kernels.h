aede642025d92b6e2243a707d560f47121c65cd1
Li Zhang <lzhang329@gmail.com>
2025-06-24 20:42:58 +0800

    fix lint

--- src/turbomind/kernels/sampling_topk_kernels.h
+++ src/turbomind/kernels/sampling_topk_kernels.h
@@ -54,15 +54,15 @@ void invokeCurandBatchInitialize(curandState_t*  states,
                                  cudaStream_t    stream);
 
 struct TopKSortFilterParams {
-    void*  logits;
-    void*  sorted_logits;
-    int*   sorted_indices;
-    int*   kept;
-    int*   top_ks;
-    int    max_top_k;
-    int    batch_size;
-    int    vocab_size;
-    int    vocab_size_padded;
+    void* logits;
+    void* sorted_logits;
+    int*  sorted_indices;
+    int*  kept;
+    int*  top_ks;
+    int   max_top_k;
+    int   batch_size;
+    int   vocab_size;
+    int   vocab_size_padded;
 };
 
 template<typename T>
