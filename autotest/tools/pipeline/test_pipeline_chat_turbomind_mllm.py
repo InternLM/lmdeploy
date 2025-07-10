411694ac8a42b77167dc68751db9984a9f7e72d6
zhulinJulia24 <zhulinJulia24@163.com>
2025-07-10 18:04:14 +0800

    update

--- autotest/tools/pipeline/test_pipeline_chat_turbomind_mllm.py
+++ autotest/tools/pipeline/test_pipeline_chat_turbomind_mllm.py
@@ -12,6 +12,7 @@ BACKEND_KVINT = 'turbomind-kvint'
 @pytest.mark.pipeline_chat
 @pytest.mark.flaky(reruns=0)
 @pytest.mark.gpu_num_1
+@pytest.mark.test_3090
 @pytest.mark.parametrize('model', get_turbomind_model_list(tp_num=1, model_type='vl_model'))
 @pytest.mark.parametrize('communicator', get_communicator_list())
 def test_pipeline_chat_tp1(config, model, communicator, worker_id):
@@ -30,7 +31,9 @@ def test_pipeline_chat_tp2(config, model, communicator, worker_id):
     if 'gw' in worker_id:
         os.environ['CUDA_VISIBLE_DEVICES'] = get_cuda_id_by_workerid(worker_id, tp_num=2)
         os.environ['MASTER_PORT'] = str(int(worker_id.replace('gw', '')) + 29500)
-
+    if ('MiniCPM-V-2_6' in model or 'InternVL2_5-26B' in model or 'InternVL2-26B' in model
+            or 'InternVL3-38B' in model) and communicator == 'native':
+        return
     run_pipeline_vl_chat_test(config, model, BACKEND, worker_id, {'communicator': communicator})
 
 
@@ -51,6 +54,7 @@ def test_pipeline_chat_tp4(config, model, communicator, worker_id):
 @pytest.mark.pipeline_chat
 @pytest.mark.flaky(reruns=0)
 @pytest.mark.gpu_num_1
+@pytest.mark.test_3090
 @pytest.mark.parametrize('model', get_turbomind_model_list(tp_num=1, quant_policy=4, model_type='vl_model'))
 @pytest.mark.parametrize('communicator', get_communicator_list())
 def test_pipeline_chat_kvint4_tp1(config, model, communicator, worker_id):
@@ -98,6 +102,7 @@ def test_pipeline_chat_kvint4_tp4(config, model, communicator, worker_id):
 @pytest.mark.pipeline_chat
 @pytest.mark.flaky(reruns=0)
 @pytest.mark.gpu_num_1
+@pytest.mark.test_3090
 @pytest.mark.parametrize('model', get_turbomind_model_list(tp_num=1, quant_policy=8, model_type='vl_model'))
 @pytest.mark.parametrize('communicator', get_communicator_list())
 def test_pipeline_chat_kvint8_tp1(config, model, communicator, worker_id):
