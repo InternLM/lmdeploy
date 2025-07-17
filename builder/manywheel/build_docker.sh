46151c18fb23d2e0a15c0a8c5fa46499b0008974
lvhan028 <lvhan_028@163.com>
2025-07-17 10:47:22 +0800

    fix py313 env creation failed when building lmdeploy-builder image

--- builder/manywheel/build_docker.sh
+++ builder/manywheel/build_docker.sh
@@ -10,23 +10,36 @@ TARGET=cuda_final
 DOCKER_TAG=cuda${GPU_ARCH_VERSION}
 
 DOCKER_IMAGE=openmmlab/lmdeploy-builder:${DOCKER_TAG}
-if [[ -n ${MANY_LINUX_VERSION} ]]; then
-    DOCKERFILE_SUFFIX=_${MANY_LINUX_VERSION}
-else
-    DOCKERFILE_SUFFIX=''
-fi
+DOCKERFILE_SUFFIX=$([[ -n ${MANY_LINUX_VERSION} ]] && echo "_${MANY_LINUX_VERSION}" || echo "")
+
+# List of all build arguments (format: KEY=VALUE)
+# Empty values will be automatically filtered out later
+BUILD_ARGS=(
+    "BASE_CUDA_VERSION=${GPU_ARCH_VERSION}"
+    "DEVTOOLSET_VERSION=9"
+    "HTTPS_PROXY=${HTTPS_PROXY:-}"
+    "HTTP_PROXY=${HTTP_PROXY:-}"
+    # Add more parameters here if needed
+)
+
+# Base Docker build command arguments
+docker_build_args=(
+    -t "${DOCKER_IMAGE}"
+    --target "${TARGET}"
+    -f "${TOPDIR}/manywheel/Dockerfile${DOCKERFILE_SUFFIX}"
+)
+
+# Process build arguments: filter empty values and format as --build-arg
+for arg in "${BUILD_ARGS[@]}"; do
+    IFS='=' read -r key value <<< "$arg"  # Split KEY=VALUE
+    if [[ -n "$value" ]]; then  # Only add non-empty values
+        docker_build_args+=(--build-arg "$arg")
+    fi
+done
 
 (
     set -x
-    DOCKER_BUILDKIT=1 docker build \
-        -t "${DOCKER_IMAGE}" \
-        --build-arg BASE_CUDA_VERSION=${GPU_ARCH_VERSION} \
-        --build-arg DEVTOOLSET_VERSION=9 \
-        --build-arg HTTPS_PROXY=${HTTPS_PROXY} \
-        --build-arg HTTP_PROXY=${HTTP_PROXY} \
-        --target "${TARGET}" \
-        -f "${TOPDIR}/manywheel/Dockerfile${DOCKERFILE_SUFFIX}" \
-        "${TOPDIR}"
+    DOCKER_BUILDKIT=1 docker build "${docker_build_args[@]}" "${TOPDIR}"
 )
 
 if [[ "${WITH_PUSH}" == true ]]; then
