e83093984d9dde380e736885f1eb53f9d9188a42
grimoire <yaoqian@pjlab.org.cn>
2025-07-23 11:56:16 +0800

    fix ut

--- tests/pytorch/config/test_hf_overrides.py
+++ tests/pytorch/config/test_hf_overrides.py
@@ -2,7 +2,7 @@ import pytest
 
 
 class TestHFOverrides:
-    
+
     @pytest.fixture
     def hf_config(self):
         from transformers.models.llava import LlavaConfig
@@ -13,35 +13,20 @@ class TestHFOverrides:
 
         # update root
         assert hf_config.model_type == 'llava'
-        overrides_dict = dict(
-            model_type='llava_custom',
-        )
+        overrides_dict = dict(model_type='llava_custom', )
         override_hf_config(hf_config, overrides_dict)
         assert hf_config.model_type == 'llava_custom'
 
         # update rope
         assert hf_config.text_config.model_type == 'llama'
         assert hf_config.text_config.rope_scaling is None
-        overrides_dict = dict(
-            text_config=dict(
-                rope_scaling=dict(
-                    rope_type='yarn',
-                )
-            )
-        )
+        overrides_dict = dict(text_config=dict(rope_scaling=dict(rope_type='yarn', )))
         override_hf_config(hf_config, overrides_dict)
         assert hf_config.text_config.model_type == 'llama'
         assert hf_config.text_config.rope_scaling['rope_type'] == 'yarn'
 
         # update both
-        overrides_dict = dict(
-            model_type='llava_custom2',
-            text_config=dict(
-                rope_scaling=dict(
-                    rope_type='yarn2',
-                )
-            )
-        )
+        overrides_dict = dict(model_type='llava_custom2', text_config=dict(rope_scaling=dict(rope_type='yarn2', )))
         override_hf_config(hf_config, overrides_dict)
         assert hf_config.model_type == 'llava_custom2'
         assert hf_config.text_config.model_type == 'llama'
