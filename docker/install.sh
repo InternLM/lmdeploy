bc9693eb4bf144106cc1bf084628c00f335a44b0
Zhongbo Tian <windreamer@gmail.com>
2025-07-26 23:44:39 +0800

    build(docker): merge hopper image

--- docker/install.sh
+++ docker/install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash -ex
 
+export DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC
 apt-get update -y
 apt-get install -y --no-install-recommends \
-    software-properties-common wget curl ssh sudo \
-    git-core libibverbs1 ibverbs-providers ibverbs-utils librdmacm1 libibverbs-dev rdma-core
-add-apt-repository -y ppa:deadsnakes/ppa
+    tzdata wget curl ssh sudo git-core libibverbs1 ibverbs-providers ibverbs-utils librdmacm1 libibverbs-dev rdma-core libmlx5-1
+
+if [[ ${PYTHON_VERSION} != "3.10" ]]; then
+    apt-get install -y --no-install-recommends software-properties-common
+    add-apt-repository -y ppa:deadsnakes/ppa
+    apt-get update -y
+fi
+
 apt-get install -y --no-install-recommends \
-    python${PYTHON_VERSION}-minimal python${PYTHON_VERSION}-dev python${PYTHON_VERSION}-venv
+    python${PYTHON_VERSION} python${PYTHON_VERSION}-dev python${PYTHON_VERSION}-venv
 
 pushd /opt >/dev/null
     python${PYTHON_VERSION} -m venv py3
@@ -26,3 +32,8 @@ rm -rf /var/lib/apt/lists/*
 export PATH=/opt/py3/bin:$PATH
 
 pip install /wheels/*.whl --extra-index-url https://download.pytorch.org/whl/${CUDA_VERSION_SHORT}
+
+if [[ "${CUDA_VERSION_SHORT}" != "cu118" ]] && [[ "${PYTHON_VERSION}" != "3.9" ]]; then
+    pip install -U pip wheel setuptools
+    pip install cuda-python dlblas
+fi
---
+++ docker/prepare_wheel.sh
@@ -0,0 +1,65 @@
+#!/bin/bash -ex
+
+export PATH=/opt/py3/bin:$PATH
+
+if [[ ${PYTHON_VERSION} = "3.13" ]]; then
+    #curl https://sh.rustup.rs -sSf | sh -s -- -y
+    pip install "cmake<4.0" wheel ninja setuptools
+    pip wheel -v --no-build-isolation -w /wheels "git+https://github.com/google/sentencepiece.git@v0.2.0#subdirectory=python"
+fi
+
+if [[ "${CUDA_VERSION_SHORT}" != "cu118" ]]; then
+    GDRCOPY_VERSION=2.4.4
+    NVSHMEM_VERSION=3.2.5-1
+    DEEP_EP_VERSION=bdd119f
+    FLASH_MLA_VERSION=9edee0c
+
+    if [[ "${CUDA_VERSION_SHORT}" = "cu124" ]]; then
+        DEEP_GEMM_VERSION=03d0be3
+    else
+        DEEP_GEMM_VERSION=1876566
+    fi
+
+    pip install torch "cmake<4.0" wheel ninja setuptools
+
+    pushd /tmp >/dev/null
+        curl -sSL "https://github.com/NVIDIA/gdrcopy/archive/refs/tags/v${GDRCOPY_VERSION}.tar.gz" | tar xz
+        pushd gdrcopy-${GDRCOPY_VERSION} >/dev/null
+            make prefix=/usr/local/gdrcopy -j $(proc) install
+        popd >/dev/null
+        rm -rf gdrcopy-${GDRCOPY_VERSION}
+
+        git clone --depth=1 https://github.com/deepseek-ai/DeepEP.git
+        pushd DeepEP >/dev/null
+            git checkout ${DEEP_EP_VERSION}
+        popd >/dev/null
+
+
+        # NVSHMEM
+        curl -sSL "https://developer.nvidia.com/downloads/assets/secure/nvshmem/nvshmem_src_${NVSHMEM_VERSION}.txz" | tar xz
+        pushd nvshmem_src >/dev/null
+            git apply /tmp/DeepEP/third-party/nvshmem.patch
+            NVSHMEM_SHMEM_SUPPORT=0 \
+            NVSHMEM_UCX_SUPPORT=0 \
+            NVSHMEM_USE_NCCL=0 \
+            NVSHMEM_MPI_SUPPORT=0 \
+            NVSHMEM_IBGDA_SUPPORT=1 \
+            NVSHMEM_PMIX_SUPPORT=0 \
+            NVSHMEM_TIMEOUT_DEVICE_POLLING=0 \
+            NVSHMEM_USE_GDRCOPY=1 \
+            cmake -S . -B build/ -DCMAKE_INSTALL_PREFIX=/usr/local/nvshmem -DMLX5_lib=/lib/x86_64-linux-gnu/libmlx5.so.1
+            cmake --build build --target install --parallel $(nproc)
+        popd >/dev/null
+        rm -rf nvshmem_src
+
+        NVSHMEM_DIR=/usr/local/nvshmem pip wheel -v --no-build-isolation -w /wheels ./DeepEP
+        rm -rf DeepEP
+
+        pip wheel -v --no-build-isolation -w /wheels "git+https://github.com/deepseek-ai/FlashMLA.git@${FLASH_MLA_VERSION}"
+        pip wheel -v --no-build-isolation -w /wheels "git+https://github.com/deepseek-ai/DeepGEMM.git@${DEEP_GEMM_VERSION}"
+
+    popd >/dev/null
+else
+    mkdir -p /usr/local/gdrcopy
+    mkdir -p /usr/local/nvshmem
+fi
