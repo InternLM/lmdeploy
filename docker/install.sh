4a0cc7817659bb3166baaaca1dc4259eaded0e3a
Zhongbo Tian <windreamer@gmail.com>
2025-07-28 10:46:22 +0800

    build(docker): support python3.13 and also install flash-attention for better performance

--- docker/install.sh
+++ docker/install.sh
@@ -31,9 +31,36 @@ rm -rf /var/lib/apt/lists/*
 
 export PATH=/opt/py3/bin:$PATH
 
-pip install /wheels/*.whl --extra-index-url https://download.pytorch.org/whl/${CUDA_VERSION_SHORT}
+if [[ "${CUDA_VERSION_SHORT}" = "cu118" ]]; then
+    FA_VERSION=2.7.3
+    TORCH_VERSION="<2.7"
+else
+    FA_VERSION=2.8.2
+    TORCH_VERSION=""
+fi
+
+pip install -U pip wheel setuptools
+pip install /wheels/*.whl torch${TORCH_VERSION} --extra-index-url https://download.pytorch.org/whl/${CUDA_VERSION_SHORT}
 
 if [[ "${CUDA_VERSION_SHORT}" != "cu118" ]] && [[ "${PYTHON_VERSION}" != "3.9" ]]; then
-    pip install -U pip wheel setuptools
     pip install cuda-python dlblas
 fi
+
+PLATFORM="linux_x86_64"
+PY_VERSION=$(python3 - <<'PY'
+import torch, sys
+torch_ver = '.'.join(torch.__version__.split('.')[:2])
+cuda_ver  = torch.version.cuda.split('.')[0]
+cxx11abi  = str(torch.compiled_with_cxx11_abi()).upper()
+py_tag    = f"cp{sys.version_info.major}{sys.version_info.minor}"
+print(f"{torch_ver} {cuda_ver} {cxx11abi} {py_tag}")
+PY
+)
+
+read TORCH_VER CUDA_VER CXX11ABI PY_TAG <<< "$PY_VERSION"
+
+WHEEL="flash_attn-${FA_VERSION}+cu${CUDA_VER}torch${TORCH_VER}cxx11abi${CXX11ABI}-${PY_TAG}-${PY_TAG}-${PLATFORM}.whl"
+BASE_URL="https://github.com/Dao-AILab/flash-attention/releases/download/v${FA_VERSION}"
+FULL_URL="${BASE_URL}/${WHEEL}"
+
+pip install "$FULL_URL"
