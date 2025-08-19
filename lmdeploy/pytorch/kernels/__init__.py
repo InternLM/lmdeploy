198a92a0c26d19138cf7ecde43ce29b52dc7edd2
grimoire <yaoqian@pjlab.org.cn>
2025-08-19 14:20:01 +0800

    remove dispatch kernel

--- lmdeploy/pytorch/kernels/__init__.py
+++ lmdeploy/pytorch/kernels/__init__.py
@@ -1,23 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 
-from .alibi_pagedattention import alibi_paged_attention_fwd
-from .apply_rotary_pos_emb import apply_rotary_pos_emb
-from .fill_kv_cache import fill_kv_cache
-from .fused_moe import fused_moe
-from .multinomial_sampling import multinomial_sampling
-from .pagedattention import paged_attention_fwd
-from .rms_norm import rms_norm
 from .w8a8_triton_kernels import (matmul_kernel_dynamic_quant, per_channel_quant, per_token_quant_int8,
                                   rms_norm_dynamic_quant)
 
 __all__ = [
-    'apply_rotary_pos_emb',
-    'fused_moe',
-    'paged_attention_fwd',
-    'alibi_paged_attention_fwd',
-    'fill_kv_cache',
-    'multinomial_sampling',
-    'rms_norm',
     'matmul_kernel_dynamic_quant',
     'per_channel_quant',
     'per_token_quant_int8',
