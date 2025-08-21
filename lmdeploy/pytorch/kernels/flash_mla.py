c8e53d504c707860854bc08c8bb9d67da233f30d
q yao <streetyao@live.com>
2025-08-21 13:52:09 +0800

    Remove unused code in PT Engine (#3858)
    * remove unused code in pt engine
    
    * remove dispatch kernel

--- lmdeploy/pytorch/kernels/flash_mla.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .dispatcher import FunctionDispatcher
-
-flash_mla_fwd = FunctionDispatcher('flash_mla_fwd').make_caller()
--- lmdeploy/pytorch/kernels/rms_norm.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .dispatcher import FunctionDispatcher
-
-rms_norm = FunctionDispatcher('rms_norm').make_caller()
