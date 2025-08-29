f86964f4b16891f97b6b45d612d868b62fe4091e
yaofengchen <67218893+yao-fengchen@users.noreply.github.com>
2025-08-29 11:49:50 +0800

    dlinfer backend support ray (#3903)
    * dlinfer backend support ray
    
    * remove ppu change

--- lmdeploy/pytorch/backends/dlinfer/maca/op_backend.py
+++ lmdeploy/pytorch/backends/dlinfer/maca/op_backend.py
@@ -109,3 +109,8 @@ class MacaOpsBackend(DlinferOpsBackend):
         """Build graph runner."""
         from lmdeploy.pytorch.backends.cuda.graph_runner import CUDAGraphRunner
         return CUDAGraphRunner(model, model_config, cache_config, backend_config, device)
+
+    @staticmethod
+    def support_ray():
+        """Support ray."""
+        return True
