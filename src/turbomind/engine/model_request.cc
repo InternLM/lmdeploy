4b9854651efac0cabc52a47a27884dbe018d1e48
Zhongbo Tian <windreamer@gmail.com>
2025-09-11 17:48:04 +0800

    add ModelRequest support for xgrammar

--- src/turbomind/engine/model_request.cc
+++ src/turbomind/engine/model_request.cc
@@ -127,6 +127,10 @@ auto ModelRequest::Forward(InputParam param, std::function<void()> cb) -> Output
     r->output_ids      = outputs_->at("output_ids");
     r->sequence_length = outputs_->at("sequence_length");
 
+    if (compiled_grammar_) {
+        r->matcher = std::make_shared<xgrammar::GrammarMatcher>(*compiled_grammar_);
+    }
+
     // Keep a weak reference for canceling the request
     request_ = r;
 
