d72ef372388fe551a6ad86b943a8f66057235848
zhulinJulia24 <145004780+zhulinJulia24@users.noreply.github.com>
2025-09-24 23:26:07 +0800

    Update eval_base_config.py

--- .github/scripts/eval_base_config.py
+++ .github/scripts/eval_base_config.py
@@ -132,7 +132,7 @@ base_model = dict(
     gen_config=dict(top_k=1, temperature=1e-6, top_p=0.9, max_new_tokens=1024),
     max_seq_len=7168,
     max_out_len=1024,
-    batch_size=64,
+    batch_size=32,
     run_cfg=dict(num_gpus=1),
 )
 
