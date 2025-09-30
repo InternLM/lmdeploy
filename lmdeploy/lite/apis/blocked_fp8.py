d4e151bd5cdebb5bd9c0f7ace75a1b5cdc418c07
zxy <zhou0493@e.ntu.edu.sg>
2025-09-30 11:59:05 +0800

    align config formats, optimize

--- lmdeploy/lite/apis/blocked_fp8.py
+++ lmdeploy/lite/apis/blocked_fp8.py
@@ -7,6 +7,7 @@ from typing import Literal
 import fire
 import torch
 from torch import nn
+from tqdm import tqdm
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 from lmdeploy.lite.quantization.weight.quant_utils import quant_blocked_fp8
@@ -16,7 +17,7 @@ from lmdeploy.pytorch.models import QLinear
 
 def blocked_fp8(model: str,
                 work_dir: str = './work_dir',
-                quant_dtype: Literal['float8_e4m3fn', 'float8_e5m2'] = 'float8_e4m3fn',
+                quant_dtype: Literal['fp8', 'float8_e4m3fn', 'float8_e5m2'] = 'float8_e4m3fn',
                 block_size: int = 128,
                 revision: str = None,
                 download_dir: str = None):
@@ -30,33 +31,31 @@ def blocked_fp8(model: str,
         print(f'can\'t find model from local_path {model}, '
               'try to download from remote')
         from lmdeploy.utils import get_model
-        model = get_model(model, revision=revision, download_dir=download_dir)
+        model_path = get_model(model, revision=revision, download_dir=download_dir)
+    else:
+        model_path = model
 
-    model_path = model
     tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
     model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True, torch_dtype=torch.float16)
     model = model.eval().cuda()
 
     # collect all linear layers
     fcs = collect_target_modules(model, nn.Linear)
-    modules_to_not_convert = [
-        'language_model.lm_head',
-        'language_model.model.embed_tokens',
-        'vision_model',  # don't quantize vision part in internvl3.5
-        'mlp1'  # don't quantize internvl3.5 mlp1
-    ]
+
+    # NOTE: don't quantize 'mlp1' in InternVL3
+    skip_patterns = ['lm_head', 'embed_tokens', 'vision_model', 'mlp1']
+    modules_to_not_convert = []
 
     # quantize and replace linear layers
-    for name, linear in fcs.items():
+    for name, linear in tqdm(fcs.items(), desc='Quantizing'):
         # skip not to convert modules
-        if any([x in name for x in modules_to_not_convert]):
-            print(f'skip: {name}')
+        if any([x in name for x in skip_patterns]):
+            modules_to_not_convert.append(name)
             continue
 
-        print(f'quantize: {name}')
         linear.to('cuda')
         # quantize weight
-        q_weight, scales = quant_blocked_fp8(linear.weight, q_dtype, block_size=block_size)
+        q_weight, scales = quant_blocked_fp8(weight=linear.weight, fp8_dtype=q_dtype, block_size=block_size)
 
         # create and replace with QLinear
         q_linear = QLinear.from_float(linear, quant_dtype=q_dtype, initialization=False)
@@ -75,9 +74,13 @@ def blocked_fp8(model: str,
     model.to('cpu')
 
     # update model config
+    if quant_dtype == 'float8_e4m3fn':
+        fmt = 'e4m3'
+    elif quant_dtype == 'float8_e5m2':
+        fmt = 'e5m2'
     quant_config = dict(activation_scheme='dynamic',
                         modules_to_not_convert=modules_to_not_convert,
-                        fmt='e4m3',
+                        fmt=fmt,
                         quant_method='fp8',
                         weight_block_size=[block_size, block_size])
     model.config.update(dict(quantization_config=quant_config))
