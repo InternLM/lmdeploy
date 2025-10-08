14414936df837ab03a393f01f91ae79cece89f03
grimoire <yaoqian@pjlab.org.cn>
2025-10-08 16:16:27 +0800

    broadcast extra inputs

--- lmdeploy/pytorch/strategies/ar/model_agent.py
+++ lmdeploy/pytorch/strategies/ar/model_agent.py
@@ -1,10 +1,13 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from contextlib import contextmanager
 from dataclasses import dataclass
 from typing import Any, List, Optional
 
 import torch
 from torch.profiler import record_function
 
+import lmdeploy.pytorch.distributed as dist
+from lmdeploy.pytorch.distributed import DistContext
 from lmdeploy.pytorch.engine.logits_process import SamplingInputs
 from lmdeploy.pytorch.messages import SchedulerSequence
 from lmdeploy.pytorch.model_inputs import ModelInputs
@@ -106,3 +109,11 @@ class ARModelAgentStrategy(ModelAgentStrategy):
                       extra_inputs: ARExtraInputs):
         """Post sampling."""
         return next_token_ids, extra_inputs
+
+    @contextmanager
+    def broadcast_next_token(self, next_token_ids: torch.Tensor, extra_inputs: ExtraInputs, dist_ctx: DistContext):
+        """Broadcast next token ids and extra inputs."""
+        tp_gpu_group = dist_ctx.tp_gpu_group
+        handle = dist.broadcast(next_token_ids, src=0, group=tp_gpu_group, async_op=True)
+        yield
+        handle.wait()
