1d201604b159234669073ed0c517a2bce301647d
windreamer <windreamer@gmail.com>
2025-10-15 22:05:32 +0800

    Reimplement guided decoding with xgrammar for PyTorch Engine (#4028)
    * feat: replace outlines with xgrammar in pytorch engine
    
    * fix: add a session status synchronization to help model_agent manage guided processors
    
    * fix: use cpu apply kernel for npu and others
    
    * fix: speedup with batched allocate and apply
    
    * fix: fix potential processor leakage, move session related fields to SamplingInputs and refater the code

--- lmdeploy/pytorch/strategies/ar/sampling.py
+++ lmdeploy/pytorch/strategies/ar/sampling.py
@@ -27,22 +27,6 @@ def _gather_all_ids(pad_id: int, seqs: SeqList, sampling_inputs: SamplingInputs)
     return output
 
 
-def _gather_guided_input_ids(pad_id: int, seqs: SeqList, sampling_inputs: 'SamplingInputs'):
-    """Gather input ids for guided decode."""
-    if not any(sampling_inputs.response_formats or ()):
-        return None
-    batch = len(seqs)
-    max_len = max(seq.num_new_tokens for seq in seqs)
-    output = torch.full((batch, max_len), pad_id, dtype=torch.int64)
-    for idx, seq in enumerate(seqs):
-        h_len = seq.num_new_tokens
-        if h_len == 0:
-            continue
-        h_ids = torch.from_numpy(seq.generated_ids)
-        output[idx, -h_len:] = h_ids
-    return output
-
-
 def _get_num_ignore_eos(seqs: SeqList):
     """Get num ignore eos."""
     ret = [seq.sampling_param.min_new_tokens - seq.num_new_tokens for seq in seqs]
@@ -55,6 +39,7 @@ class ARSamplingStrategy(SamplingStrategy):
     def __init__(self, pad_token_id: int) -> None:
         pad_token_id = 0 if pad_token_id is None else pad_token_id
         self.pad_token_id = pad_token_id
+        self.session_to_cleanup = []
 
     def make_sampling_inputs(self, seqs: SeqList) -> SamplingInputs:
         """Create sampling inputs from the sequences."""
@@ -71,6 +56,8 @@ class ARSamplingStrategy(SamplingStrategy):
         response_formats = [None] * batch_size
         logits_processors = [None] * batch_size
         num_logprobs = [None] * batch_size
+        session_to_cleanup = self.session_to_cleanup
+        self.session_to_cleanup = []
 
         def __gather_params():
             """Gather params."""
@@ -164,6 +151,11 @@ class ARSamplingStrategy(SamplingStrategy):
 
         max_num_logprobs = max(num_logprobs)
 
+        session_ctx = [{
+            'session_id': seq.session.session_id,
+            'seq_id': seq.seq_id,
+        } for seq in seqs]
+
         sampling_input = SamplingInputs(
             temperature=temperature,
             bad_words=bad_words,
@@ -182,10 +174,14 @@ class ARSamplingStrategy(SamplingStrategy):
             logits_processors=logits_processors,
             max_num_logprobs=max_num_logprobs,
             batch_size=batch_size,
+            session_ctx=session_ctx,
+            session_to_cleanup=session_to_cleanup,
         )
 
         pad_token_id = self.pad_token_id
         sampling_input.all_ids = _gather_all_ids(pad_token_id, seqs, sampling_input)
-        sampling_input.guided_input_ids = _gather_guided_input_ids(pad_token_id, seqs, sampling_input)
         sampling_input.num_ignore_eos = _get_num_ignore_eos(seqs)
         return sampling_input
+
+    def on_session_end(self, session_id: int):
+        self.session_to_cleanup.append(session_id)
