87877091b0abac70eb5098ff320429f69af54ce3
zxy <zhou0493@e.ntu.edu.sg>
2025-11-28 20:36:25 +0800

    fix moe

--- lmdeploy/pytorch/configurations/qwen3_vl.py
+++ lmdeploy/pytorch/configurations/qwen3_vl.py
@@ -8,7 +8,7 @@ class Qwen3VLModelConfigBuilder(AutoModelConfigBuilder):
     @classmethod
     def condition(cls, hf_config):
         """config."""
-        return hf_config.model_type == 'qwen3_vl'
+        return hf_config.model_type == 'qwen3_vl' or hf_config.model_type == 'qwen3_vl_moe'
 
     @classmethod
     def build(cls, hf_config, model_path: str = None, **kwargs):
