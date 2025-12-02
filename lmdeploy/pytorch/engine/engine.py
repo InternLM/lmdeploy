3385d04a39e193bcda4c213d77eafc387e223fe4
q.yao <yaoqian@pjlab.org.cn>
2025-12-02 14:09:34 +0800

    add config builder

--- lmdeploy/pytorch/engine/engine.py
+++ lmdeploy/pytorch/engine/engine.py
@@ -1,6 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import asyncio
-import copy
 import gc
 import logging
 import os
@@ -18,15 +17,16 @@ from lmdeploy.pytorch.disagg.conn.engine_conn import EngineP2PConnection
 from lmdeploy.pytorch.disagg.conn.protocol import (DistServeConnectionRequest, DistServeDropConnectionRequest,
                                                    DistServeInitRequest)
 from lmdeploy.pytorch.disagg.messages import MigrationExecutionBatch
-from lmdeploy.utils import get_logger, get_max_batch_size, get_model
+from lmdeploy.utils import get_logger, get_model
 
 from ..adapter.adapter import AdapterManager
-from ..config import BackendConfig, CacheConfig, DistConfig, MiscConfig, ModelConfig, SchedulerConfig, SpecDecodeConfig
+from ..config import CacheConfig, ModelConfig
 from ..messages import MessageStatus, SchedulerSequence, UpdateTokenMode
 from ..model_inputs import ModelInputs, VisionModelInputs
 from ..paging import Scheduler
 from ..strategies import build_strategy_factory
 from .base import EngineBase
+from .config_builder import ConfigBuilder
 from .engine_checker import EngineChecker
 from .executor import build_executor
 from .model_agent import BatchedOutputs
@@ -75,99 +75,6 @@ def _tensorlize_block_offsets(block_offsets, dtype=torch.int32):
     return torch.as_tensor(out, dtype=dtype)
 
 
-def _update_engine_config(engine_config: PytorchEngineConfig):
-    """Update pytorch engine config."""
-    # make sure engine exits
-    if engine_config is None:
-        engine_config = PytorchEngineConfig()
-    else:
-        engine_config = copy.deepcopy(engine_config)
-
-    if engine_config.max_batch_size is None:
-        engine_config.max_batch_size = get_max_batch_size(engine_config.device_type)
-
-    if engine_config.dllm_block_length is not None:
-        max_prefill_token_num = engine_config.max_prefill_token_num
-        max_batch_size = engine_config.max_batch_size
-        if max_batch_size * engine_config.dllm_block_length > max_prefill_token_num:
-            engine_config.max_batch_size = max_prefill_token_num // engine_config.dllm_block_length
-            logger.warning(f'Update max_batch_size to {engine_config.max_batch_size} '
-                           f'since dllm_block_length({engine_config.dllm_block_length}) * max_batch_size '
-                           f'({max_batch_size}) > max_prefill_token_num ({max_prefill_token_num}).')
-
-    if engine_config.dp != 1:
-        if engine_config.tp == 1 and engine_config.ep == 1:
-            engine_config.dp = 1
-            engine_config.dp_rank = 0
-
-    return engine_config
-
-
-def _build_scheduler_config(engine_config: PytorchEngineConfig):
-    """Build scheduler config."""
-    scheduler_config = SchedulerConfig(max_batches=engine_config.max_batch_size,
-                                       max_session_len=engine_config.session_len,
-                                       prefill_interval=engine_config.prefill_interval)
-    return scheduler_config
-
-
-def _build_cache_config(engine_config: PytorchEngineConfig):
-    """Build cache config."""
-    cache_config = CacheConfig(max_batches=engine_config.max_batch_size,
-                               block_size=engine_config.block_size,
-                               num_cpu_blocks=engine_config.num_cpu_blocks,
-                               num_gpu_blocks=engine_config.num_gpu_blocks,
-                               cache_max_entry_count=engine_config.cache_max_entry_count,
-                               max_prefill_token_num=engine_config.max_prefill_token_num,
-                               enable_prefix_caching=engine_config.enable_prefix_caching,
-                               quant_policy=engine_config.quant_policy,
-                               device_type=engine_config.device_type,
-                               migration_backend=engine_config.migration_backend,
-                               role=engine_config.role)
-    return cache_config
-
-
-def _build_backend_config(engine_config: PytorchEngineConfig):
-    """Build backend config."""
-    backend_config = BackendConfig(
-        eager_mode=engine_config.eager_mode,
-        device_type=engine_config.device_type,
-    )
-    return backend_config
-
-
-def _build_dist_config(engine_config: PytorchEngineConfig):
-    """Build dist config."""
-    dist_config = DistConfig.from_engine_config(engine_config=engine_config)
-    return dist_config
-
-
-def _build_misc_config(engine_config: PytorchEngineConfig):
-    """Build misc config."""
-    misc_config = MiscConfig.from_engine_config(engine_config)
-    return misc_config
-
-
-def _build_specdecode_config(target_model, speculative_config: SpeculativeConfig, engine_config: PytorchEngineConfig,
-                             cache_config: CacheConfig):
-    """Build spec decode config."""
-    specdecode_config = None
-    if speculative_config is not None:
-        draft_model = speculative_config.model
-        if draft_model and not os.path.exists(speculative_config.model):
-            draft_model = get_model(draft_model, engine_config.download_dir, engine_config.revision)
-
-        specdecode_config = SpecDecodeConfig.from_config(
-            method=speculative_config.method,
-            num_speculative_tokens=speculative_config.num_speculative_tokens,
-            model=draft_model,
-            target_model=target_model,
-            target_cache_cfg=cache_config,
-            dtype=engine_config.dtype,
-        )
-    return specdecode_config
-
-
 def _build_seq_meta(cache_config: CacheConfig, strategy: Any):
     from lmdeploy.pytorch.messages import SequenceMeta
 
@@ -202,11 +109,11 @@ class CounterEvent:
 class RunableEventBase:
     """Runable event base."""
 
-    async def wait(self, idx: int):
+    async def wait(self):
         """Wait event."""
         raise NotImplementedError('Not implemented.')
 
-    def set(self, idx: int = None):
+    def set(self):
         """Set event."""
         raise NotImplementedError('Not implemented.')
 
@@ -365,7 +272,7 @@ class Engine(EngineBase):
         speculative_config: SpeculativeConfig = None,
     ) -> None:
         # make sure engine config exist
-        engine_config = _update_engine_config(engine_config)
+        engine_config = ConfigBuilder.update_engine_config(engine_config)
 
         # frequently gc would cause latency spike
         # default threshold (700, 10, 10)
@@ -393,14 +300,14 @@ class Engine(EngineBase):
         checker.handle()
 
         # build configs
-        scheduler_config = _build_scheduler_config(engine_config)
-        cache_config = _build_cache_config(engine_config)
-        backend_config = _build_backend_config(engine_config)
-        dist_config = _build_dist_config(engine_config)
-        misc_config = _build_misc_config(engine_config)
-
+        scheduler_config = ConfigBuilder.build_scheduler_config(engine_config)
+        cache_config = ConfigBuilder.build_cache_config(engine_config)
+        backend_config = ConfigBuilder.build_backend_config(engine_config)
+        dist_config = ConfigBuilder.build_dist_config(engine_config)
+        misc_config = ConfigBuilder.build_misc_config(engine_config)
         # spec decode
-        self.specdecode_config = _build_specdecode_config(model_path, speculative_config, engine_config, cache_config)
+        self.specdecode_config = ConfigBuilder.build_specdecode_config(model_path, speculative_config, engine_config,
+                                                                       cache_config)
 
         # build model agent
         self.executor = build_executor(
