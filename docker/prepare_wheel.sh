ecb6cceabd28fed6a47b0a3cadf506e2044010ac
zxy <46674730+CUHKSZzxy@users.noreply.github.com>
2025-12-04 14:22:10 +0800

    Add FA3 (#4166)
    * add fa3
    
    * separate hopper
    
    * use pre-built FA3
    
    * FA3 for cu128 only
    
    * simplify
    
    * remove hopper image, add dlslime
    
    * FA3 for cu128, cu130
    
    * skip Dive for cu13

--- docker/prepare_wheel.sh
+++ docker/prepare_wheel.sh
@@ -23,7 +23,6 @@ if [[ ${PYTHON_VERSION} = "3.13" ]]; then
 fi
 
 if [[ "${CUDA_VERSION_SHORT}" != "cu118" ]]; then
-
     GDRCOPY_VERSION=2.5.1
     DEEP_EP_VERSION=9af0e0d  # v1.2.1
     DEEP_GEMM_VERSION=c9f8b34  # v2.1.1.post3
