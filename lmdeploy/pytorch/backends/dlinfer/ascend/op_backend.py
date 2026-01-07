f896caa003d92d4f16927b343891f39937c9fff3
tangzhiyi <tangzhiyi@pjlab.org.cn>
2026-01-07 02:56:30 +0000

    update

--- lmdeploy/pytorch/backends/dlinfer/ascend/op_backend.py
+++ lmdeploy/pytorch/backends/dlinfer/ascend/op_backend.py
@@ -148,25 +148,35 @@ class AscendOpsBackend(DlinferOpsBackend):
             return cls.total_slots
 
         def get_cpu_seqlens(is_decoding, is_unpaged_prefill):
+            """Get sequence lengths on CPU.
+
+            Returns:
+                q_seqlens_cpu: query sequence lengths (per sequence).
+                kv_seqlens_cpu: kv sequence lengths (per sequence), used for
+                    list/max seqlens calculation.
+                kv_seqlens_expanded: kv sequence lengths expanded per token via
+                    repeat_interleave, used for attention metadata.
+            """
             if is_decoding:
                 q_seqlens_cpu = None
-                kv_seqlens_cpu_raw = kv_seqlens_cpu = step_context.kv_seqlens.cpu()
+                kv_seqlens_cpu = kv_seqlens_expanded = step_context.kv_seqlens.cpu()
             elif is_unpaged_prefill:
                 q_seqlens_cpu = step_context.q_seqlens.cpu()
-                kv_seqlens_cpu_raw = kv_seqlens_cpu = q_seqlens_cpu
+                kv_seqlens_cpu = kv_seqlens_expanded = q_seqlens_cpu
             else:
                 q_seqlens_cpu = step_context.q_seqlens.cpu()
-                kv_seqlens_cpu_raw = step_context.kv_seqlens.cpu()
-                kv_seqlens_cpu = kv_seqlens_cpu_raw.repeat_interleave(q_seqlens_cpu, 0)
-            return q_seqlens_cpu, kv_seqlens_cpu_raw, kv_seqlens_cpu
+                kv_seqlens_cpu = step_context.kv_seqlens.cpu()
+                # Expand kv_seqlens to per-token for paged prefill attention
+                kv_seqlens_expanded = kv_seqlens_cpu.repeat_interleave(q_seqlens_cpu, 0)
+            return q_seqlens_cpu, kv_seqlens_cpu, kv_seqlens_expanded
 
-        def get_list_seqlens(is_decoding, is_unpaged_prefill, q_seqlens_cpu=None, kv_seqlens_cpu_raw=None):
+        def get_list_seqlens(is_decoding, is_unpaged_prefill, q_seqlens_cpu=None, kv_seqlens_cpu=None):
             if is_decoding:
                 q_seqlens_list, kv_seqlens_list = None, None
             elif is_unpaged_prefill:
                 q_seqlens_list = kv_seqlens_list = q_seqlens_cpu.tolist()
             else:
-                q_seqlens_list, kv_seqlens_list = q_seqlens_cpu.tolist(), kv_seqlens_cpu_raw.tolist()
+                q_seqlens_list, kv_seqlens_list = q_seqlens_cpu.tolist(), kv_seqlens_cpu.tolist()
             return q_seqlens_list, kv_seqlens_list
 
         def get_max_seqlens(is_decoding, is_unpaged_prefill, q_seqlens_list=None, kv_seqlens_list=None):
@@ -222,10 +232,10 @@ class AscendOpsBackend(DlinferOpsBackend):
 
             return kv_start_indices, attention_mask
 
-        q_seqlens_cpu, kv_seqlens_cpu_raw, kv_seqlens_cpu = get_cpu_seqlens(step_context.is_decoding,
-                                                                            is_unpaged_prefill)
+        q_seqlens_cpu, kv_seqlens_cpu, kv_seqlens_expanded = get_cpu_seqlens(step_context.is_decoding,
+                                                                             is_unpaged_prefill)
         q_seqlens_list, kv_seqlens_list = get_list_seqlens(step_context.is_decoding, is_unpaged_prefill, q_seqlens_cpu,
-                                                           kv_seqlens_cpu_raw)
+                                                           kv_seqlens_cpu)
         max_q_seq_len, max_kv_seq_len = get_max_seqlens(step_context.is_decoding, is_unpaged_prefill, q_seqlens_list,
                                                         kv_seqlens_list)
         kv_start_indices, attention_mask = get_kv_start_indices_and_attention_mask(step_context.is_decoding,
@@ -252,7 +262,9 @@ class AscendOpsBackend(DlinferOpsBackend):
             step_context.block_offsets,
             q_start_loc=None,
             q_seqlens=q_seqlens_cpu,
-            kv_seqlens=kv_seqlens_cpu,
+            # kv_seqlens_expanded is only expanded in paged prefill,
+            # otherwise it equals kv_seqlens_cpu
+            kv_seqlens=kv_seqlens_expanded,
             kv_start_indices=kv_start_indices,
             block_size=block_size,
             attention_mask=attention_mask,
