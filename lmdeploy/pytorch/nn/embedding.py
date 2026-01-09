5b7e9d6696c64f0e5cf7e783dacb03eb04389fa6
maoruihan <maoruihan@stonewise.cn>
2026-01-09 16:28:28 +0800

    fix embedding

--- lmdeploy/pytorch/nn/embedding.py
+++ lmdeploy/pytorch/nn/embedding.py
@@ -33,13 +33,13 @@ class ParallelEmbedding(nn.Module):
         self.padding_size = padding_size
         if padding_idx is not None:
             if padding_idx < 0:
-                padding_idx = vocab_size - padding_idx
-            assert padding_idx >= 0
+                padding_idx = vocab_size + padding_idx
+            assert padding_idx >= 0 and padding_idx < vocab_size
         self.padding_idx = padding_idx
 
         dist_cfg = get_dist_manager().current_config()
         _, self.rank = get_tp_world_rank(layer_type)
-        self.tp, tp_mode = dist_cfg.get_tp_by_layer(layer_type)
+        self.tp, _ = dist_cfg.get_tp_by_layer(layer_type)
 
         dist_group = get_dist_group(layer_type=layer_type)
         self.tp_group = dist_group.gpu_group
@@ -87,12 +87,15 @@ class ParallelEmbedding(nn.Module):
 
     def weight_loader(self, param: torch.nn.Parameter, loaded_weight: torch.Tensor):
         """Weight loader."""
-        if not self.is_tp:
+        if not self.all_reduce:
             default_weight_loader(param, loaded_weight)
+            if self.padding_idx is not None:
+                self.weight[self.padding_idx] = 0
         else:
             self._weight_loader_tp_rowwise(param, loaded_weight)
-        if self.padding_idx is not None and self.padding_idx >= self.start_index and self.padding_idx < self.end_index:
-            self.weight[self.padding_idx - self.start_index] = 0
+            if (self.padding_idx is not None and self.padding_idx >= self.start_index
+                    and self.padding_idx < self.end_index):
+                self.weight[self.padding_idx - self.start_index] = 0
 
     def forward(self, x: torch.Tensor):
         return self.impl.forward(x, self.weight, all_reduce=self.all_reduce, group=self.tp_group)
