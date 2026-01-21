f984093bd34fac3958914ad1ba041b2c30738991
zhulin1 <zhulinJulia24@163.com>
2026-01-21 10:31:02 +0800

    update

--- autotest/utils/run_restful_chat.py
+++ autotest/utils/run_restful_chat.py
@@ -37,10 +37,11 @@ def start_openai_service(config, run_config, worker_id):
         run_config['extra_params'] = {}
     run_config['extra_params']['server-port'] = str(port)
     run_config['extra_params']['allow-terminate-by-client'] = None
-    run_config['extra_params'].pop('model-name', None)
+    model_name = case_name if run_config['extra_params'].get(
+        'model-name', None) is None else run_config['extra_params'].pop('model-name')
     cmd = ' '.join([
         cuda_prefix, 'lmdeploy serve api_server', model_path,
-        get_cli_common_param(run_config), f'--model-name {case_name}'
+        get_cli_common_param(run_config), f'--model-name {model_name}'
     ]).strip()
 
     env = os.environ.copy()
