09071c7979fef7d83cdcb7e1e4cafa819d32ecb5
yaofengchen <67218893+yao-fengchen@users.noreply.github.com>
2026-02-11 11:20:01 +0800

    [ascend]suppot ep (#3696)
    * [ascend]suppot deepseek eager_mode
    
    * fix flash_mla_available on ascend
    
    * modify for dp_ep
    
    * backup code
    
    * run tp ep
    
    * format code
    
    * add dp tp
    
    * move DlinferDistContext into dlinfer
    
    * fix get_max_tokens_across_dp in tp case
    
    * format code
    
    * fix grpah_mode dp
    
    * add mlpmetada
    
    * good eager mode
    
    * good graph mode
    
    * good dp*tp+ep feature
    
    * fix tp err
    
    * update pad_size
    
    * optimize ep moe
    
    * opt ep moe
    
    * fix ascend dptp
    
    * refactor code
    
    * refactor code
    
    * remove useless code
    
    * update code
    
    * update code

--- lmdeploy/pytorch/kernels/dlinfer/fused_moe.py
+++ lmdeploy/pytorch/kernels/dlinfer/fused_moe.py
@@ -1,5 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import dlinfer.ops as ext_ops
+from dlinfer.utils.type_annotation import MoECommType as DlinferMoECommType  # noqa: F401
+from dlinfer.utils.type_annotation import MoeMetadata as DlinferMoeMetadata
 from torch import Tensor
 
 
@@ -11,6 +13,8 @@ def fused_moe(
     topk_ids: Tensor,
     topk: int,
     renormalize: bool,
+    moe_metadata: DlinferMoeMetadata,
 ):
     """Dlinfer fused moe."""
-    return ext_ops.fused_moe(hidden_states, gate_up_weights, down_weights, topk_weights, topk_ids, topk, renormalize)
+    return ext_ops.fused_moe(hidden_states, gate_up_weights, down_weights, topk_weights, topk_ids, topk, renormalize,
+                             moe_metadata)
