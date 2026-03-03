35d7434b66ab8efb79ec86dd4dba7248091fb92a
lvhan028 <lvhan_028@163.com>
2026-03-03 12:08:05 +0800

    fix

--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -ex
 
-mkdir -p /wheels /nccl
+mkdir -p /wheels
 
 if [[ "${CUDA_VERSION_SHORT}" = "cu130" ]]; then
     pip install nvidia-nccl-cu13
 else
     pip install nvidia-nccl-cu12
+fi
 pip install --upgrade pip build
 python3 -m build -w -o /wheels -v .
