3573506a1d772a7ad5f599f4e24d68f3cb6bde54
yaofengchen <fengchenyao@foxmail.com>
2026-04-13 01:43:43 +0000

    update code

--- lmdeploy/pytorch/backends/dlinfer/moe.py
+++ lmdeploy/pytorch/backends/dlinfer/moe.py
@@ -65,8 +65,6 @@ class DlinferFusedMoEImpl(FusedMoEImpl):
 
     def update_weights(self, gate_up_weights: torch.Tensor, down_weights: torch.Tensor):
         """Update weights."""
-        if os.getenv('DLINFER_RESET_MOE_UPDATE_WEIGHTS', '0') == '1':
-            return gate_up_weights, down_weights
         return gate_up_weights, down_weights
 
     def ep_expert_list(self, world_size: int, rank: int):
