41bea9a9efa27bd2b8ad4c0ade404182829feffc
lvhan028 <lvhan_028@163.com>
2026-04-16 04:40:40 +0000

    fix ut

--- tests/test_lmdeploy/test_vl/test_hf_chat_template.py
+++ tests/test_lmdeploy/test_vl/test_hf_chat_template.py
@@ -6,7 +6,7 @@ from lmdeploy.model import MODELS
 from lmdeploy.vl.model.builder import load_vl_model
 
 
-def get_model_and_chat_template(model_path):
+def get_model_and_chat_template(model_path, trust_remote_code=False):
     if os.getenv('LMDEPLOY_USE_MODELSCOPE', 'False').lower() == 'true':
         from modelscope import snapshot_download
     elif os.getenv('LMDEPLOY_USE_OPENMIND_HUB', 'False').lower() == 'true':
@@ -15,7 +15,7 @@ def get_model_and_chat_template(model_path):
         from huggingface_hub import snapshot_download
     model_path = snapshot_download(model_path, allow_patterns=['*.json', '*.py', '*.txt', '*.model', '*.jinja'])
     model = load_vl_model(model_path=model_path, with_llm=False, backend='pytorch')
-    chat_template = MODELS.module_dict['hf'](model_path=model_path)
+    chat_template = MODELS.module_dict['hf'](model_path=model_path, trust_remote_code=trust_remote_code)
     return model, chat_template
 
 
@@ -67,7 +67,7 @@ class TestInternVLHFChatTemplate:
             'OpenGVLab/InternVL3_5-30B-A3B-HF',
             'OpenGVLab/InternVL3_5-241B-A28B-HF',
         ]
-        models = [get_model_and_chat_template(model_path) for model_path in model_list]
+        models = [get_model_and_chat_template(model_path, trust_remote_code=True) for model_path in model_list]
         return models
 
     def test_proc_messages(self, models, mock_messages):
