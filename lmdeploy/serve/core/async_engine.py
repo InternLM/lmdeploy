7fb47c4eec1577117f71773e5dd56370eeda071b
lvhan028 <lvhan_028@163.com>
2026-05-08 04:10:41 +0000

    fix ppl oom

--- lmdeploy/serve/core/async_engine.py
+++ lmdeploy/serve/core/async_engine.py
@@ -713,6 +713,8 @@ class AsyncEngine:
                     async for outputs in gen:
                         pass
                     logits[i] = outputs.logits[:input_len, :]
+            if sequence_end and self.backend == 'pytorch':
+                await handle.async_end(session.session_id)
 
         create_sessions = False
         if sessions is None:
@@ -720,9 +722,6 @@ class AsyncEngine:
             sessions = [self.session_mgr.get() for _ in range(len(input_ids))]
         tasks = [_proc(session, i) for i, session in enumerate(sessions)]
         await asyncio.gather(*tasks)
-        if sequence_end and self.backend == 'pytorch':
-            for session in sessions:
-                await session.async_close()
         if sequence_end and create_sessions:
             for session in sessions:
                 self.session_mgr.remove(session)
