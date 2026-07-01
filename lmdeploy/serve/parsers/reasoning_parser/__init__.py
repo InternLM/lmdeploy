f16c79ce9e329b6772fa31ac1e8a9bb41d4ddfaf
zxy <zhou0493@e.ntu.edu.sg>
2026-07-01 21:16:00 +0800

    refactor: reuse deepseek v32 tool parser for v4

--- lmdeploy/serve/parsers/reasoning_parser/__init__.py
+++ lmdeploy/serve/parsers/reasoning_parser/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .deepseek_v3_reasoning_parser import DeepSeekV3ReasoningParser
-from .deepseek_v32_reasoning_parser import DeepSeekV32ReasoningParser
 from .deepseek_v4_reasoning_parser import DeepSeekV4ReasoningParser
+from .deepseek_v32_reasoning_parser import DeepSeekV32ReasoningParser
 from .reasoning_parser import LEGACY_REASONING_PARSER_NAMES, ReasoningParser, ReasoningParserManager
 
 __all__ = [
