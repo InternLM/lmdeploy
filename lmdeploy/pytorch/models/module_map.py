fe2f0699bf1ba4326aa0718cc28732d1e949744a
zxy <zhou0493@e.ntu.edu.sg>
2026-07-28 03:09:51 +0000

    chore: reorder interns2 preview module map

--- lmdeploy/pytorch/models/module_map.py
+++ lmdeploy/pytorch/models/module_map.py
@@ -184,14 +184,6 @@ MODULE_MAP.update({
     f'{LMDEPLOY_PYTORCH_MODEL_PATH}.qwen3_5_moe.Qwen3_5MoeForConditionalGeneration',
 })
 
-# interns2-preview
-MODULE_MAP.update({
-    'InternS2PreviewForConditionalGeneration':
-    f'{LMDEPLOY_PYTORCH_MODEL_PATH}.interns2_preview.InternS2PreviewForConditionalGeneration',
-    'InternS2PreviewForCausalLM':
-    f'{LMDEPLOY_PYTORCH_MODEL_PATH}.interns2_preview.InternS2PreviewForConditionalGeneration',
-})
-
 MODULE_MAP.update({
     'Qwen3_5MTPModel': f'{LMDEPLOY_PYTORCH_MODEL_PATH}.qwen3_5_mtp.Qwen3_5MTPModel',
 })
@@ -201,6 +193,14 @@ MODULE_MAP.update({
 MODULE_MAP.update({
     'Qwen3OmniMoeForConditionalGeneration':
     f'{LMDEPLOY_PYTORCH_MODEL_PATH}.qwen3_omni_moe_thinker.Qwen3OmniMoeThinkerForConditionalGeneration',
+})
+
+# interns2-preview
+MODULE_MAP.update({
+    'InternS2PreviewForConditionalGeneration':
+    f'{LMDEPLOY_PYTORCH_MODEL_PATH}.interns2_preview.InternS2PreviewForConditionalGeneration',
+    'InternS2PreviewForCausalLM':
+    f'{LMDEPLOY_PYTORCH_MODEL_PATH}.interns2_preview.InternS2PreviewForConditionalGeneration',
 })
 
 # phi-3
