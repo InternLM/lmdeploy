0455cae3e8e03d75139868ec45103b9ce994176c
RunningLeon <mnsheng@yeah.net>
2026-08-06 13:05:06 +0000

    remove ut

--- tests/pytorch/engine/test_ray_mp_engine.py
+++ tests/pytorch/engine/test_ray_mp_engine.py
@@ -1,8 +1,6 @@
 import asyncio
 from contextlib import suppress
 
-import pytest
-
 from lmdeploy.messages import EngineOutput, ResponseType
 from lmdeploy.pytorch.engine.mp_engine.base_worker import (
     EngineOutputGather,
@@ -10,6 +8,17 @@ from lmdeploy.pytorch.engine.mp_engine.base_worker import (
     StreamPollResult,
 )
 from lmdeploy.pytorch.engine.mp_engine.ray_engine import RayEngineWorker, RayMPEngine
+
+
+def _make_worker(stream=None):
+    worker = RayEngineWorker.__new__(RayEngineWorker)
+    worker._stream_id = 0
+    worker._stream_aiter = {}
+    worker._stream_task = {}
+    worker._engine_output_gather = EngineOutputGather()
+    if stream is not None:
+        worker.instance_async_stream_infer = stream
+    return worker
 
 
 class _FakeRayMPEngine(RayMPEngine):
@@ -44,6 +53,15 @@ class _FakeRayMPEngine(RayMPEngine):
             self.drop_called.set()
             return None
         raise AssertionError(f'Unexpected fake Ray RPC: {func}')
+
+
+class _LocalRayMPEngine(RayMPEngine):
+
+    def __init__(self, worker):
+        self.worker = worker
+
+    async def _collective_rpc_async(self, func, *args, **kwargs):
+        return await getattr(self.worker, func)(*args, **kwargs)
 
 
 async def _async_test_ray_stream_startup_cancel_drops_remote_stream():
@@ -90,10 +108,7 @@ def test_ray_stream_cancel_after_start_drops_remote_stream():
 
 
 async def _async_test_ray_get_stream_task_result_after_drop_is_idempotent():
-    worker = RayEngineWorker.__new__(RayEngineWorker)
-    worker._stream_aiter = {}
-    worker._stream_task = {}
-    worker._engine_output_gather = EngineOutputGather()
+    worker = _make_worker()
 
     poll_result = await worker.get_stream_task_result(404)
     assert poll_result == StreamPollResult(done=True)
@@ -106,126 +121,57 @@ async def _async_test_ray_get_stream_task_result_after_drop_is_idempotent():
     await asyncio.sleep(0)
     worker._stream_aiter.pop(stream_id)
 
-    stream_out.publish(EngineOutput(ResponseType.FINISH, [7]))
+    stream_out.publish(None)
     stream_out.finish()
     poll_result = await asyncio.wait_for(get_task, timeout=1)
 
-    assert poll_result.done is True
-    assert poll_result.has_output is True
-    assert poll_result.output.status == ResponseType.FINISH
-    assert poll_result.output.token_ids == [7]
+    assert poll_result == StreamPollResult(output=None, has_output=True, done=True)
 
 
 def test_ray_get_stream_task_result_after_drop_is_idempotent():
     asyncio.run(_async_test_ray_get_stream_task_result_after_drop_is_idempotent())
 
 
-async def _async_test_ray_stream_does_not_repeat_consumed_final_output():
-    worker = RayEngineWorker.__new__(RayEngineWorker)
-    worker._stream_id = 0
-    worker._stream_aiter = {}
-    worker._stream_task = {}
-    worker._engine_output_gather = EngineOutputGather()
-    allow_final = asyncio.Event()
+async def _async_test_ray_stream_does_not_repeat_final_output():
     allow_close = asyncio.Event()
 
     async def fake_stream(notify_add_msg_func=None):
         notify_add_msg_func()
-        yield EngineOutput(ResponseType.SUCCESS, [1, 2])
-        await allow_final.wait()
         yield EngineOutput(ResponseType.FINISH, [3, 4, 5])
         # Keep the generator alive after its final yield, matching async
         # session cleanup in EngineInstance.async_stream_infer.
         await allow_close.wait()
 
-    worker.instance_async_stream_infer = fake_stream
-    stream_id = await worker.create_stream_task('instance_async_stream_infer')
-
-    poll_result = await worker.get_stream_task_result(stream_id)
-    assert poll_result.output.token_ids == [1, 2]
-    assert poll_result.has_output is True
-    assert poll_result.done is False
-
-    allow_final.set()
-    poll_result = await worker.get_stream_task_result(stream_id)
-    assert poll_result.output.token_ids == [3, 4, 5]
-    assert poll_result.has_output is True
-    assert poll_result.done is False
+    engine = _LocalRayMPEngine(_make_worker(fake_stream))
+    stream = engine._collective_rpc_streaming_async('instance_async_stream_infer', asyncio.Event())
+    outputs_task = asyncio.create_task(anext(stream))
+    output = await asyncio.wait_for(outputs_task, timeout=1)
+    assert output.token_ids == [3, 4, 5]
 
     allow_close.set()
-    poll_result = await worker.get_stream_task_result(stream_id)
-    assert poll_result.has_output is False
-    assert poll_result.done is True
-    assert poll_result.error is None
+    with suppress(StopAsyncIteration):
+        await asyncio.wait_for(anext(stream), timeout=1)
+        raise AssertionError('final output was repeated')
 
 
-def test_ray_stream_does_not_repeat_consumed_final_output():
-    asyncio.run(_async_test_ray_stream_does_not_repeat_consumed_final_output())
+def test_ray_stream_does_not_repeat_final_output():
+    asyncio.run(_async_test_ray_stream_does_not_repeat_final_output())
 
 
-async def _async_test_ray_stream_returns_unconsumed_final_output_once():
-    worker = RayEngineWorker.__new__(RayEngineWorker)
-    worker._stream_id = 0
-    worker._stream_aiter = {}
-    worker._stream_task = {}
-    worker._engine_output_gather = EngineOutputGather()
+async def _async_test_ray_stream_error_before_first_output():
 
-    async def fake_stream(notify_add_msg_func=None):
+    async def failing_stream(notify_add_msg_func=None):
         notify_add_msg_func()
-        yield EngineOutput(ResponseType.FINISH, [6, 7, 8])
+        raise RuntimeError('injected stream failure')
+        yield
 
-    worker.instance_async_stream_infer = fake_stream
-    stream_id = await worker.create_stream_task('instance_async_stream_infer')
-    await asyncio.sleep(0)
-
-    poll_result = await worker.get_stream_task_result(stream_id)
-    assert poll_result.output.token_ids == [6, 7, 8]
-    assert poll_result.has_output is True
-    assert poll_result.done is True
-    assert poll_result.error is None
+    engine = _LocalRayMPEngine(_make_worker(failing_stream))
+    outputs = [
+        output async for output in engine._collective_rpc_streaming_async(
+            'instance_async_stream_infer', asyncio.Event())
+    ]
+    assert [output.status for output in outputs] == [ResponseType.INTERNAL_ENGINE_ERROR]
 
 
-def test_ray_stream_returns_unconsumed_final_output_once():
-    asyncio.run(_async_test_ray_stream_returns_unconsumed_final_output_once())
-
-
-class _LocalRayMPEngine(RayMPEngine):
-
-    def __init__(self, worker):
-        self.worker = worker
-
-    async def _collective_rpc_async(self, func, *args, **kwargs):
-        return await getattr(self.worker, func)(*args, **kwargs)
-
-
-@pytest.mark.parametrize('fail_after_output', [False, True])
-def test_ray_stream_error_delivery(fail_after_output):
-
-    async def _test():
-        worker = RayEngineWorker.__new__(RayEngineWorker)
-        worker._stream_id = 0
-        worker._stream_aiter = {}
-        worker._stream_task = {}
-        worker._engine_output_gather = EngineOutputGather()
-
-        async def fake_stream(notify_add_msg_func=None):
-            notify_add_msg_func()
-            if fail_after_output:
-                yield EngineOutput(ResponseType.SUCCESS, [8, 9])
-            raise RuntimeError('injected stream failure')
-            yield
-
-        worker.instance_async_stream_infer = fake_stream
-        engine = _LocalRayMPEngine(worker)
-        outputs = [
-            output async for output in engine._collective_rpc_streaming_async(
-                'instance_async_stream_infer', asyncio.Event())
-        ]
-        expected = ([ResponseType.SUCCESS, ResponseType.INTERNAL_ENGINE_ERROR]
-                    if fail_after_output else [ResponseType.INTERNAL_ENGINE_ERROR])
-        assert [output.status for output in outputs] == expected
-        assert outputs[-1].token_ids == []
-        if fail_after_output:
-            assert outputs[0].token_ids == [8, 9]
-
-    asyncio.run(_test())
+def test_ray_stream_error_before_first_output():
+    asyncio.run(_async_test_ray_stream_error_before_first_output())
